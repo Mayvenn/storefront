3680a9564a286e009794a67c9c13b1ded1a45c89
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-05 16:55:43 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject storefront "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  
+            
+  :dependencies [[org.clojure/clojure "1.6.0"]])
