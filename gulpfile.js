5304de2da9f929e0966450a17c4f1d46b2b56840
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-07 11:02:35 -0700

    Update resources from diva, add task to automate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
+var gulpMerge = require('gulp-merge');
+var replace = require('gulp-replace');
 
 gulp.task('sass', function () {
   gulp.src('./resources/scss/*.scss')
@@ -16,3 +18,23 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['sass']);
+
+
+gulp.task('copy-assets', function () {
+  gulp.src('../diva/app/assets/images/**/*.{png,jpg}')
+    .pipe(gulp.dest('./resources/public/images'));
+  var pipe1 = gulp.src('../diva/vendor/assets/stylesheets/spree/frontend/mayvenn/**/*.scss')
+      .pipe(gulp.dest('./resources/scss'));
+  var pipe2 = gulp.src('../diva/vendor/assets/styleshess/spree/frontend/all.css.scss')
+      .pipe(gulp.dest('./resources/scss/all.scss'));
+  var pipe3 = gulp.src('../diva/vendor/assets/styleshess/spree/frontend/_free_shipping_banner.scss')
+      .pipe(gulp.dest('./resources/scss/_free_shipping_banner.scss'));
+
+  gulpMerge(pipe1, pipe2, pipe3)
+    .pipe(replace('image-url\("', 'url("/images/'))
+    .pipe(replace("image-url\('", "url('/images/"))
+    .pipe(replace("image-url\('", "url('/images/"))
+    .pipe(replace("spree\/frontend\/mayvenn\/", ""))
+    .pipe(replace("spree\/frontend\/", ""))
+    .pipe(gulp.dest('./resources/scss'))
+});
