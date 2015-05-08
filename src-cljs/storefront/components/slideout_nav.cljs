f259de9541c3f387fc2bdfd0df4864c549360319
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-08 10:56:44 -0700

    Adding template for mobile slideout nav

--- src-cljs/storefront/components/slideout_nav.cljs
+++ src-cljs/storefront/components/slideout_nav.cljs
@@ -5,6 +5,13 @@
             [storefront.events :as events]
             [storefront.state :as state]))
 
+(defn slideout-nav-link [data {:keys [path icon-class image label full-width?]}]
+  [:a.slideout-nav-link
+   {:href "FIXME: slideout-nav-links" :class (if full-width? "full-width" "half-width")}
+   [:div.slideout-nav-link-inner
+    [:img.slideout-nav-link-icon {:class (str "icon-" icon-class)} image]
+    label]])
+
 (defn slideout-nav-component [data owner]
   (om/component
    (html
@@ -63,4 +70,111 @@
         (when false ;; FIXME: if current user is stylist
           [:ul.ship-menu-expanded.closed
            [:li [:a {:href "FIXME: path to shop hair extensions"} "Hair Extensions"]]
-           [:li [:a {:href "FIXME: path to stylist only products"} "Stylist Only Products"]]])]]))))
+           [:li [:a {:href "FIXME: path to stylist only products"} "Stylist Only Products"]]])]
+       [:ul.slideout-nav-list
+        (when false ;; FIXME: own store
+          [:li.slideout-nav-section.stylist
+           [:h3.slideout-nav-section-header.highlight "Manage Store"]
+           (slideout-nav-link
+            data
+            {:path "FIXME path"
+             :icon-class "commissions-and-payouts"
+             :image "/images/slideout_nav/commissions_and_payouts.png"
+             :label "Commissions & Payouts"
+             :full-width? false})
+           (slideout-nav-link
+            data
+            {:path "FIXME path"
+             :icon-class "sales-bonuses"
+             :image "/images/slideout_nav/sales_bonuses.png"
+             :label "Stylist Bonuses"
+             :full-width? false})
+           (slideout-nav-link
+            data
+            {:path "FIXME path"
+             :icon-class "stylist-referrals"
+             :image "/images/slideout_nav/stylist_referrals.png"
+             :label "Stylist Referrals"
+             :full-width? false})
+           (slideout-nav-link
+            data
+            {:path "FIXME path"
+             :icon-class "edit-profile"
+             :image "/images/slideout_nav/edit_profile.png"
+             :label "Edit Profile"
+             :full-width? false})
+           ])
+        [:li.slideout-nav-section
+         [:h3.slideout-nav-section-header "Shop"]
+         (slideout-nav-link
+            data
+            {:path "FIXME path"
+             :icon-class "hair-extensions"
+             :image "/images/slideout_nav/hair_extensions.png"
+             :label "Hair Extensions"
+             :full-width? true})
+         (when false ;; FIXME is own store
+           (slideout-nav-link
+            data
+            {:path "FIXME path"
+             :icon-class "stylist-products"
+             :image "/images/slideout_nav/stylist-products.png"
+             :label "Stylist Products"
+             :full-width? true}))]
+        [:li.slideout-nav-section
+         [:h3.slideout-nav-section-header "My Account"]
+         (if false ;; FIXME logged in user
+           [:div
+            (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "my-orders"
+              :image "/images/slideout_nav/my_orders.png"
+              :label "My Orders"
+              :full-width? true})
+            (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "manage-account"
+              :image "/images/slideout_nav/manage-account.png"
+              :label "Manage Account"
+              :full-width? false})
+            (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "logout"
+              :image "/images/slideout_nav/logout.png"
+              :label "Logout"
+              :full-width? false})]
+           [:div
+            (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "sign-in"
+              :image "/images/slideout_nav/sign_in.png"
+              :label "Sign In"
+              :full-width? false})
+            (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "join"
+              :image "/images/slideout_nav/join.png"
+              :label "Join"
+              :full-width? false})]
+           )]
+        [:li.slideout-nav-section
+         [:h3.slideout-nav-section-header "Help"]
+         (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "customer-service"
+              :image "/images/slideout_nav/customer_service.png"
+              :label "Customer Service"
+              :full-width? false})
+         (slideout-nav-link
+             data
+             {:path "FIXME path"
+              :icon-class "30-day-guarantee"
+              :image "/images/slideout_nav/30_day_guarantee.png"
+              :label "30 Day Guarantee"
+              :full-width? false})]]]))))
