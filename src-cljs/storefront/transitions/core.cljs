60e6755eaad61a3e998ffff38a660806bd40a1b5
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-08 09:41:48 -0700

    Event processing can dispatch off event partials.

--- src-cljs/storefront/transitions/core.cljs
+++ src-cljs/storefront/transitions/core.cljs
@@ -5,25 +5,22 @@
             [storefront.taxons :refer [taxon-path-for]]))
 
 (defmulti transition-state identity)
-(defmethod transition-state :default [event arg app-state]
-  (js/console.error "Transitioned via default, probably shouldn't. " (prn-str event))
+(defmethod transition-state :default [dispatch event arg app-state]
   app-state)
 
-(defmethod transition-state events/navigate-home [event args app-state]
-  (assoc-in app-state state/navigation-event-path events/navigate-home))
+(defmethod transition-state events/navigate [_ event args app-state]
+  (assoc-in app-state state/navigation-event-path event))
 
-(defmethod transition-state events/navigate-category [event {:keys [taxon-path]} app-state]
+(defmethod transition-state events/navigate-category [_ event {:keys [taxon-path]} app-state]
   (let [taxons (get-in app-state state/taxons-path)
         taxon (first (filter #(= (taxon-path-for %) taxon-path) taxons))]
-    (-> app-state
-        (assoc-in state/browse-taxon-path taxon)
-        (assoc-in state/navigation-event-path events/navigate-category))))
+    (assoc-in app-state state/browse-taxon-path taxon)))
 
-(defmethod transition-state events/api-success-taxons [event args app-state]
+(defmethod transition-state events/api-success-taxons [_ event args app-state]
   (assoc-in app-state state/taxons-path (:taxons args)))
 
-(defmethod transition-state events/api-success-store [event args app-state]
+(defmethod transition-state events/api-success-store [_ event args app-state]
   (assoc-in app-state state/store-path args))
 
-(defmethod transition-state events/api-success-products [event {:keys [taxon-id products]} app-state]
+(defmethod transition-state events/api-success-products [_ event {:keys [taxon-id products]} app-state]
   (update-in app-state state/products-for-taxons-path assoc taxon-id products))
