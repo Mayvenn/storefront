e2b1db20f3ccd789044edcadec635f55059b2c8b
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-09 20:37:05 -0700

    Remove autogenerated file accidentally committed

--- resources/public/js/main.js
+++
@@ -1,5 +0,0 @@
-if(typeof goog == "undefined") document.write('<script src="/js/out/goog/base.js"></script>');
-document.write('<script src="/js/out/cljs_deps.js"></script>');
-document.write('<script>if (typeof goog != "undefined") { goog.require("storefront.core"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
-
-document.write("<script>if (typeof goog != \"undefined\") { goog.require(\"figwheel.connect\"); }</script>");
