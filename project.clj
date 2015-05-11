fbb8880b3cc763b4c8cee6aa75d995fb28242afd
Jeff Hui <jeff@mayvenn.com>
2015-05-11 16:47:34 -0700

    Updated Bidi to 1.18.11.

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
                  [sablono "0.3.4"]
                  [cljs-ajax "0.3.11"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [bidi "1.18.10"]]
+                 [bidi "1.18.11"]]
   
                              
                              
