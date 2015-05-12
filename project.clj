326766600be468a44481f33bbbc4ae8d11eeffe4
Jeff Hui <jeff@mayvenn.com>
2015-05-12 10:14:02 -0700

    Added cemerick.url library.

--- project.clj
+++ project.clj
@@ -25,7 +25,8 @@
                  [sablono "0.3.4"]
                  [cljs-ajax "0.3.11"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [bidi "1.18.11"]]
+                 [bidi "1.18.11"]
+                 [com.cemerick/url "0.1.1"]]
   :repositories [["private" {:url "s3p://mayvenn-dependencies/releases/"
                              :username :env
                              :passphrase :env}]]
