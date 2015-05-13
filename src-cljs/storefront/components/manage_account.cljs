2f7073dc7074fb92666b5f292303f24db83b392f
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-13 14:14:04 -0700

    Filling in template for manage account

--- src-cljs/storefront/components/manage_account.cljs
+++ src-cljs/storefront/components/manage_account.cljs
@@ -8,4 +8,20 @@
 (defn manage-account-component [data owner]
   (om/component
    (html
-    [:h1 "Manage"])))
+    [:div#edit-account
+     [:form
+      [:label {:for "user-email"} "Email"]
+      [:br]
+      [:input.title#user-email {:type "email" :value ""}]
+      [:div#password-credentials
+       [:p
+        [:label {:for "user-password"} "Password"]
+        [:br]
+        [:input.title#user-password {:type "password" :value ""}]]
+       [:p
+        [:label {:for "user-password-confirmation"} "Enter the same password"]
+        [:br]
+        [:input.title#user-password-confirmation {:type "password" :value ""}]]]
+      [:p.user-password-instructions "Leave blank to keep the same password."]
+      [:p
+       [:input.button.primary {:type "submit" :value "Update"}]]]])))
