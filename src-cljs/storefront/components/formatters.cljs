547d1ed3f1de14085c6fbca2298539f7935baed0
Corey Reece <corey@mayvenn.com>
2015-05-13 17:01:34 -0700

    Add format utils for locale-date and formatting money

---
+++ src-cljs/storefront/components/formatters.cljs
@@ -0,0 +1,13 @@
+(ns storefront.components.formatters
+  (:require [goog.string :as gstring]
+            [goog.string.format]))
+
+(defn locale-date [iso8601-formatted-string]
+  (-> iso8601-formatted-string
+      (js/Date.parse)
+      (js/Date.)
+      (.toLocaleDateString)))
+
+
+(defn float-as-money [amount]
+  (apply gstring/format "$%1.2f" [amount]))
