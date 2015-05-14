7b6bf64fdc53f3dfd2b4424e433e586d1c6027aa
Corey Reece <corey@mayvenn.com>
2015-05-14 14:09:11 -0700

    Extract shared stylist dashboard navigation

---
+++ src-cljs/storefront/components/stylist/nav.cljs
@@ -0,0 +1,13 @@
+(ns storefront.components.stylist.nav
+  (:require [om.core :as om]
+            [sablono.core :refer-macros [html]]
+            [storefront.events :as events]
+            [storefront.components.utils :as utils]))
+
+(defn stylist-dashboard-nav-component [data]
+  (om/component
+   (html
+    [:nav.stylist-dashboard-nav
+     (utils/link-with-selected data events/navigate-stylist-commissions "Commissions")
+     (utils/link-with-selected data events/navigate-stylist-commissions "Bonus Credit")
+     (utils/link-with-selected data events/navigate-stylist-commissions "Referrals")])))
