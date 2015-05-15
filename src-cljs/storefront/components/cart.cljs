88fdddedb237551d90dbc1d11f4ff943cde2e836
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-15 10:28:25 -0700

    Add in cart page skeleton

---
+++ src-cljs/storefront/components/cart.cljs
@@ -0,0 +1,11 @@
+(ns storefront.components.cart
+  (:require [storefront.components.utils :as utils]
+            [om.core :as om]
+            [sablono.core :refer-macros [html]]
+            [storefront.events :as events]
+            [storefront.state :as state]))
+
+(defn cart-component [data owner]
+  (om/component
+   (html
+    [:h1 "Cart"])))
