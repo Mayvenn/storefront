026f8112bf8adb96322bfd57e40dff170dce425e
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-15 10:40:51 -0700

    Add in static empty cart

--- src-cljs/storefront/components/cart.cljs
+++ src-cljs/storefront/components/cart.cljs
@@ -3,9 +3,19 @@
             [om.core :as om]
             [sablono.core :refer-macros [html]]
             [storefront.events :as events]
+            [storefront.taxons :refer [default-taxon-path]]
             [storefront.state :as state]))
 
 (defn cart-component [data owner]
   (om/component
    (html
-    [:h1 "Cart"])))
+    [:div.cart-container
+     [:p.empty-cart-message "OH NO!"]
+     [:figure.empty-bag]
+     [:p
+      [:a.button.primary.continue.empty-cart
+       (when-let [path (default-taxon-path data)]
+         (utils/route-to data
+                         events/navigate-category
+                         {:taxon-path path}))
+       "Let's Fix That"]]])))
