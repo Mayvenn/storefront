6ad1b95a9eb916606e4ac635905ecdf367e62731
Jeff Hui <jeff@mayvenn.com>
2015-05-20 16:39:34 -0700

    Navigate to /checkout/delivery on form submission
    Add fetching of states from the API

--- src-cljs/storefront/components/checkout_address.cljs
+++ src-cljs/storefront/components/checkout_address.cljs
@@ -4,105 +4,8 @@
             [storefront.state :as state]
             [storefront.events :as events]
             [storefront.components.utils :as utils]
+            [storefront.components.checkout-steps :refer [checkout-step-bar]]
             [clojure.string :as string]))
-
-(def us-states
-  (vec
-   (map (fn [[value name]] {:name name :value value})
-        [[39 "Alabama"]
-         [50 "Alaska"]
-         [5 "Arizona"]
-         [11 "Arkansas"]
-         [32 "California"]
-         [45 "Colorado"]
-         [47 "Connecticut"]
-         [20 "Delaware"]
-         [13 "District of Columbia"]
-         [27 "Florida"]
-         [44 "Georgia"]
-         [17 "Hawaii"]
-         [14 "Idaho"]
-         [6 "Illinois"]
-         [35 "Indiana"]
-         [40 "Iowa"]
-         [9 "Kansas"]
-         [42 "Kentucky"]
-         [23 "Louisiana"]
-         [34 "Maine"]
-         [26 "Maryland"]
-         [46 "Massachusetts"]
-         [1 "Michigan"]
-         [29 "Minnesota"]
-         [41 "Mississippi"]
-         [10 "Missouri"]
-         [24 "Montana"]
-         [15 "Nebraska"]
-         [12 "Nevada"]
-         [7 "New Hampshire"]
-         [30 "New Jersey"]
-         [43 "New Mexico"]
-         [48 "New York"]
-         [8 "North Carolina"]
-         [33 "North Dakota"]
-         [31 "Ohio"]
-         [22 "Oklahoma"]
-         [37 "Oregon"]
-         [16 "Pennsylvania"]
-         [21 "Rhode Island"]
-         [49 "South Carolina"]
-         [2 "South Dakota"]
-         [25 "Tennessee"]
-         [36 "Texas"]
-         [18 "Utah"]
-         [19 "Vermont"]
-         [28 "Virginia"]
-         [3 "Washington"]
-         [51 "West Virginia"]
-         [4 "Wisconsin"]
-         [38 "Wyoming"]])))
-
-(def checkout-steps ["address" "delivery" "payment" "confirm"])
-
-(defn display-progress-step [current-index index state]
-  [:li.progress-step
-   {:class (->> [state
-                 (if (< index current-index) "completed")
-                 (if (= index (inc current-index)) "next")
-                 (if (= index current-index) "current")
-                 (if (zero? index) "first")
-                 (if (= index (dec (count state))) "last")]
-                (filter seq)
-                (string/join " "))}
-   [:span
-    (let [text [:div.progress-step-index
-                (str (inc index) " ")
-                (string/capitalize state)]]
-      (if (< index current-index)
-        [:a {:href "TODO:/checkout_state_path(state)"} text]
-        text))]])
-
-(defn checkout-progress [app-state current-step]
-  (let [current-step-index (->> checkout-steps
-                                (map-indexed vector)
-                                (filter #(= (second %) current-step))
-                                ffirst)]
-    [:ol {:class (str "progress-steps checkout-step-" current-step)}
-     (map-indexed (partial display-progress-step current-step-index)
-                  checkout-steps)]))
-
-
-(defn order-details [app-state checkout-current-step]
-  (let [checkout-step (get-in app-state checkout-current-step)]
-    [:div#checkout
-     [:div.row
-      [:div.columns.thirteen.omega (checkout-progress app-state checkout-current-step)]]
-     [:div.row
-      [:div.checkout-form-wrapper
-       [:form
-        {:id (str "checkout_form_" checkout-step)
-         :action "#TODO:"
-         :method "POST"}
-        ]]]]))
 
 (defn textfield [name & [{:keys [id value required? type on-change] :or {type "text"}}]]
   [:p.field
@@ -125,7 +28,7 @@
               :on-change on-change
               :value value}
      [:option ""]
-     (map (fn [{name :name val :value}]
+     (map (fn [{name :name val :id}]
             [:option {:value val} (str name)])
           options)]]])
 
@@ -178,7 +81,7 @@
                                                            10))})}
                          {:id :state
                           :required? true
-                          :options us-states
+                          :options (get-in data state/states-path)
                           :value (get-in data state/checkout-billing-address-state-path)}))
      (textfield "Zip"
                 (merge (utils/update-text data events/control-checkout-change state/checkout-billing-address-zip-path)
@@ -246,7 +149,7 @@
                                                            10))})}
                          {:id :state
                           :required? true
-                          :options us-states
+                          :options (get-in data state/states-path)
                           :value (get-in data state/checkout-shipping-address-state-path)}))
      (textfield "Zip"
                 (merge (utils/update-text data events/control-checkout-change state/checkout-shipping-address-zip-path)
@@ -263,21 +166,18 @@
 (defn checkout-address-component [data owner]
   (om/component
    (html
-    (let [checkout-current-step (get-in data state/checkout-current-step-path)]
-      [:div#checkout
-       [:div.row
-        [:div.columns.thirteen.omega (checkout-progress data checkout-current-step)]]
-       [:div.row
-        [:div.checkout-form-wrapper
-         [:form.edit_order
-          {:id (str "checkout_form_" checkout-current-step)
-           :action "TODO: /checkout/update/address"
-           :method "POST"
-           :on-submit (utils/enqueue-event data events/control-checkout-update-addresses-submit)}
+    [:div#checkout
+     (checkout-step-bar data)
+     [:div.row
+      [:div.checkout-form-wrapper
+       [:form.edit_order
+        {:action "TODO: /checkout/update/address"
+         :method "POST"
+         :on-submit (utils/enqueue-event data events/control-checkout-update-addresses-submit)}
 
-          (billing-address-form data)
+        (billing-address-form data)
 
-          (shipping-address-form data)
+        (shipping-address-form data)
 
-          [:div.form-buttons.checkout.save-and-continue
-           [:input.continue.button.primary {:type "submit" :name "Commit" :value "Save and Continue"}]]]]]]))))
+        [:div.form-buttons.checkout.save-and-continue
+         [:input.continue.button.primary {:type "submit" :name "Commit" :value "Save and Continue"}]]]]]])))
