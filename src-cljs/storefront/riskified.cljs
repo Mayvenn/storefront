1dd307c12ffdc3be4dbc7550e6b74776ff938c91
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-04 11:32:45 -0700

    Load riskified beacon with protocol relative src

--- src-cljs/storefront/riskified.cljs
+++ src-cljs/storefront/riskified.cljs
@@ -3,10 +3,7 @@
 (def store-domain "mayvenn.com")
 
 (defn insert-beacon [session-id]
-  (let [protocol (if (= "https:" (.. js/document -location -protocol))
-                   "https://"
-                   "http://")
-        src(str protocol "beacon.riskified.com?shop=" store-domain "&sid=" session-id)
+  (let [src (str "//beacon.riskified.com?shop=" store-domain "&sid=" session-id)
         script-tag (.createElement js/document "script")
         first-script-tag (aget (.getElementsByTagName js/document "script") 0)]
     (set! (.-type script-tag) "text/javascript")
