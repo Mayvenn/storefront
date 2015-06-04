7205e48033070357c47b0974681fba67e96c1c6e
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-04 15:11:32 -0700

    Use utils/route-to in checkout complete link home

--- src-cljs/storefront/components/checkout_complete.cljs
+++ src-cljs/storefront/components/checkout_complete.cljs
@@ -2,6 +2,7 @@
   (:require [om.core :as om]
             [sablono.core :refer-macros [html]]
             [storefront.keypaths :as keypaths]
+            [storefront.components.utils :as utils]
             [storefront.events :as events]))
 
 (defn checkout-complete-component [data owner]
@@ -18,4 +19,4 @@
       "We've received your order and will being processing it right away. Once your order ships we will send you another e-mail confirmation."
       [:a {:href "TODO: spree.order_path(@order)"} "View Order >"]]
 
-     [:a.big-button.left-half {:href "/"} "Return Home"]])))
+     [:a.big-button.left-half (utils/route-to data events/navigate-home) "Return Home"]])))
