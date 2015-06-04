5add9ab3ac5dc42e4f951f6ac4777c2492d059aa
Jeff Hui <jeff@mayvenn.com>
2015-06-04 13:19:57 -0700

    Add completed checkout page.
    Convert api/update-order :navigate key to accept a message instead of
    an event. It's now [event args] instead. Args is optional.

---
+++ src-cljs/storefront/components/checkout_complete.cljs
@@ -0,0 +1,21 @@
+(ns storefront.components.checkout-complete
+  (:require [om.core :as om]
+            [sablono.core :refer-macros [html]]
+            [storefront.keypaths :as keypaths]
+            [storefront.events :as events]))
+
+(defn checkout-complete-component [data owner]
+  (om/component
+   (html
+    [:div.checkout-container
+     [:div.order-thank-you
+      [:figure.shopping-bag]
+      [:p.message "Thank you for your order!"]]
+
+     [:div.solid-line-divider]
+
+     [:p.order-thanks-detail
+      "We've received your order and will being processing it right away. Once your order ships we will send you another e-mail confirmation."
+      [:a {:href "TODO: spree.order_path(@order)"} "View Order >"]]
+
+     [:a.big-button.left-half {:href "/"} "Return Home"]])))
