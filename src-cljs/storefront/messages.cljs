80a7d2a67323dda6e4a524b5b94c06641dc406e6
Corey Reece <corey@mayvenn.com>
2015-06-08 14:14:25 -0700

    Remove comment

--- src-cljs/storefront/messages.cljs
+++ src-cljs/storefront/messages.cljs
@@ -2,5 +2,5 @@
   (:require [cljs.core.async :refer [put!]]))
 
 (defn enqueue-message [ch [event args]]
-  ;; (js/console.trace (clj->js event) "\n" (clj->js args)) ;; uncomment to trace message enqueues
+  (js/console.trace (clj->js event) "\n" (clj->js args)) ;; uncomment to trace message enqueues
   (put! ch [event args]))
