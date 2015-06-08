f5f7f9f1fa0cff7584fb26d7b5d30acb00083aea
Corey Reece <corey@mayvenn.com>
2015-06-08 16:46:56 -0700

    Remove NaNs
    - pass data
    - trying to be more idiomatic

--- src-cljs/storefront/components/stylist/commissions.cljs
+++ src-cljs/storefront/components/stylist/commissions.cljs
@@ -7,7 +7,7 @@
             [storefront.events :as events]
             [storefront.keypaths :as keypaths]))
 
-(defn display-new-order [data new-order]
+(defn show-new-order [data new-order]
   [:.loose-table-row
    [:.left-content
     [:p (new-order :fullname)]
@@ -31,31 +31,35 @@
       :else
        [:p.commission-label.refunded-label "Refunded"])]])
 
-(defn display-new-orders [data new-orders]
-  (html
-   [:.new-order-commissions
-    [:h4.dashboard-details-header "New Orders"]
-    [:p.commission-explanation "Commission is earned when order ships."]
-    [:.solid-line-divider]
-    [:.loose-table-header
-     [:.left-header "Sale"]
-     [:.right-header "Commission"]]
-    (map (partial display-new-order data) new-orders)]))
+(defn list-new-orders [data]
+  (when-let [new-orders (not-empty (get-in data
+                                           keypaths/stylist-commissions-new-orders))]
+    (html
+     [:.new-order-commissions
+      [:h4.dashboard-details-header "New Orders"]
+      [:p.commission-explanation "Commission is earned when order ships."]
+      [:.solid-line-divider]
+      [:.loose-table-header
+       [:.left-header "Sale"]
+       [:.right-header "Commission"]]
+      (map (partial show-new-order data) new-orders)])))
 
-(defn display-payout [payout]
+(defn show-payout [data payout]
   [:.loose-table-row.short-row
    [:.left-content [:span (f/locale-date (payout :paid_at))]]
     [:.right-content
      [:span.payout-amount (f/as-money (payout :amount))]]])
 
-(defn display-payouts [paid-total payouts]
-  [:.commission-payment-history
-   [:h4.dashboard-details-header "Commission Payment History"]
-   [:.solid-line-ider]
-   [:.emphasized-banner
-    [:span.emphasized-banner-header "Commissions Paid"]
-    [:span.emphasized-banner-value (f/as-money paid-total)]]
-   (map display-payout payouts)])
+(defn list-payouts [data]
+  (when-let [paid-total (get-in data keypaths/stylist-commissions-paid-total)] 
+    [:.commission-payment-history
+     [:h4.dashboard-details-header "Commission Payment History"]
+     [:.solid-line-ider]
+     [:.emphasized-banner
+      [:span.emphasized-banner-header "Commissions Paid"]
+      [:span.emphasized-banner-value (f/as-money paid-total)]]
+     (map (partial show-payout data)
+          (get-in data keypaths/stylist-commissions-payouts))]))
 
 (defn stylist-commissions-component [data owner]
   (om/component
@@ -67,26 +71,24 @@
       (om/build stylist-dashboard-nav-component data)
 
       [:.dashboard-content
-       [:#next-commission-summary.dashboard-summary
+       (when-let [next-commission-amount
+                  (get-in data keypaths/stylist-commissions-next-amount)]
+         [:#next-commission-summary.dashboard-summary
 
-        [:.next-payout-description
-         [:p "As of today, your next commission payment is:"]
-         [:p.next-commissions-amount
-          (f/as-money (get-in data keypaths/stylist-commissions-next-amount))]]
+          [:.next-payout-description
+           [:p "As of today, your next commission payment is:"]
+           [:p.next-commissions-amount (f/as-money next-commission-amount)]]
 
-        [:.next-payout-date-container
-         [:p.accented-next-pay "W"]
-         [:p.small-payout-description "Commission paid on Wednesdays"]]]
+          [:.next-payout-date-container
+           [:p.accented-next-pay "W"]
+           [:p.small-payout-description "Commission paid on Wednesdays"]]])
 
-       [:#money-rules
-        [:.gold-money-box]
-        [:.money-rule-details
-         [:p
-          "You earn "
-          (get-in data keypaths/stylist-commissions-rate)
-          "% commission on each new order shipped from your store excluding tax and shipping."]]]
+       (when-let [commission-rate (get-in data keypaths/stylist-commissions-rate)]
+         [:#money-rules
+          [:.gold-money-box]
+          [:.money-rule-details
+           [:p "You earn " commission-rate "% commission on each new order "
+            "shipped from your store excluding tax and shipping."]]])
 
-       (display-new-orders data
-                           (get-in data keypaths/stylist-commissions-new-orders))
-       (display-payouts (get-in data keypaths/stylist-commissions-paid-total)
-                        (get-in data keypaths/stylist-commissions-payouts))]]])))
+       (list-new-orders data)
+       (list-payouts data)]]])))
