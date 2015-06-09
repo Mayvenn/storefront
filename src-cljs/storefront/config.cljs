d60bb959ab76042feac68bd160de9f3c9169b005
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-09 14:16:15 -0700

    Get API endpoint from config
    Changes API endpoint based on application environment

---
+++ src-cljs/storefront/config.cljs
@@ -0,0 +1,11 @@
+(ns storefront.config)
+
+(def development? (= js/environment "development"))
+
+(def api-base-url js/apiUrl)
+                    
+                    
+                    
+
+(def send-sonar-base-url "https://www.sendsonar.com/api/v1")
+(def send-sonar-publishable-key "d7d8f2d0-9f91-4507-bc82-137586d41ab8")
