b6130a7a1aa54c1636baafe2b3f890f2fe18e8a2
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-15 09:14:44 -0700

    Fix dependency name typo

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [ring-jetty-component "0.2.2"]
                  [ring-logging "0.1.0"]
                  [hiccup "1.0.5"]
-                 [cheshire.core "5.5.0"]
+                 [cheshire "5.5.0"]
 
                  [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.cache "0.6.3"]
