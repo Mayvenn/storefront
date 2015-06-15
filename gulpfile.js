fb50a6ca3d81eb9571482aac149b0386376ac835
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-14 18:57:38 -0700

    Remove copy-assets task; the styles have diverged

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
-var gulpMerge = require('gulp-merge');
-var replace = require('gulp-replace');
 var RevAll = require('gulp-rev-all');
 var path = require('path');
 var argv = require('yargs').argv;
@@ -22,29 +20,6 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['sass']);
-
-gulp.task('copy-assets', function () {
-  gulp.src('../diva/app/assets/fonts/**/*')
-    .pipe(gulp.dest('resources/public/fonts'));
-
-  gulp.src('../diva/app/assets/images/**/*.{png,jpg}')
-    .pipe(gulp.dest('./resources/public/images'));
-  var pipe1 = gulp.src('../diva/vendor/assets/stylesheets/spree/frontend/mayvenn/**/*.scss')
-      .pipe(gulp.dest('./resources/scss'));
-  var pipe2 = gulp.src('../diva/vendor/assets/styleshess/spree/frontend/all.css.scss')
-      .pipe(gulp.dest('./resources/scss/all.scss'));
-  var pipe3 = gulp.src('../diva/vendor/assets/styleshess/spree/frontend/_free_shipping_banner.scss')
-      .pipe(gulp.dest('./resources/scss/_free_shipping_banner.scss'));
-
-  gulpMerge(pipe1, pipe2, pipe3)
-    .pipe(replace("font-url\('", "url('/fonts/"))
-    .pipe(replace('image-url\("', 'url("/images/'))
-    .pipe(replace("image-url\('", "url('/images/"))
-    .pipe(replace("image-url\('", "url('/images/"))
-    .pipe(replace("spree\/frontend\/mayvenn\/", ""))
-    .pipe(replace("spree\/frontend\/", ""))
-    .pipe(gulp.dest('./resources/scss'))
-});
 
 gulp.task('cdn', function () {
   if (!argv.host) {
