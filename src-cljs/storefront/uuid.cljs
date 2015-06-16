f67ab5610a9780a60b9adc9e1e4fcda01d2a693c
Sheel Choksi <sheelchoksi@gmail.com>
2015-06-16 15:35:58 -0700

    Remove fixme around uuid, can't be fixed right now

--- src-cljs/storefront/uuid.cljs
+++ src-cljs/storefront/uuid.cljs
@@ -1,6 +1,6 @@
 (ns storefront.uuid)
 
-;; FIXME use cljs.core directly when we upgrade to 0.0-3308 or beyond
+;; use cljs.core directly when we upgrade to 0.0-3308 or beyond
 (defn random-uuid []
   (letfn [(hex [] (.toString (rand-int 15) 16))]
     (let [rhex (.toString (bit-or 0x8 (bit-and 0x3 (rand-int 14))) 16)]
