13256d7c621ec7b1b8cbb0ad6e02345dd6d1e78e
Jeff Hui <jeff@mayvenn.com>
2015-06-18 16:57:05 -0700

    Switch to use om refs instead of atoms

--- src-cljs/storefront/sync_messages.cljs
+++ src-cljs/storefront/sync_messages.cljs
@@ -1,6 +1,7 @@
 (ns storefront.sync-messages
   (:require [storefront.effects :refer [perform-effects]]
-            [storefront.transitions :refer [transition-state]]))
+            [storefront.transitions :refer [transition-state]]
+            [om.core :as om]))
 
 (defn- transition [app-state [event args]]
   (reduce #(transition-state %2 event args %1) app-state (reductions conj [] event)))
@@ -11,5 +12,6 @@
 
 (defn send-message [app-state-ref message]
   ;; (js/console.trace (clj->js event) "\n" (clj->js args)) ;; uncomment to trace messages
-  (swap! app-state-ref transition message)
+  ;;(swap! app-state-ref transition message)
+  (om/transact! app-state-ref #(transition % message))
   (effects @app-state-ref message))
