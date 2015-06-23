2f15a428ed0f52a9e9bb4fe6018f1902e46c6dce
Jeff Hui <jeff@mayvenn.com>
2015-06-23 16:11:37 -0700

    Enhance logging capabilities for messages
    Logging messages now diffs changes to the app state. This makes it
    easier to determine what parts of the app state change from what messages.

--- src-cljs/storefront/sync_messages.cljs
+++ src-cljs/storefront/sync_messages.cljs
@@ -1,17 +1,33 @@
 (ns storefront.sync-messages
   (:require [storefront.effects :refer [perform-effects]]
             [storefront.transitions :refer [transition-state]]
-            [om.core :as om]))
+            [om.core :as om]
+            [clojure.data :refer [diff]]))
 
 (defn- transition [app-state [event args]]
-  (reduce #(transition-state %2 event args %1) app-state (reductions conj [] event)))
+  (reduce (fn [app-state dispatch]
+            (or (transition-state dispatch event args app-state)
+                app-state))
+          app-state
+          (reductions conj [] event)))
 
 (defn- effects [app-state [event args]]
   (doseq [event-fragment (rest (reductions conj [] event))]
     (perform-effects event-fragment event args app-state)))
 
+(defn- log-deltas [old-app-state new-app-state [event args]]
+  (let [[deleted added unchanged] (diff old-app-state new-app-state)]
+    (when (or (seq deleted) (seq added))
+      (js/console.trace (clj->js event)
+                        (clj->js args)
+                        (clj->js {:deleted deleted
+                                  :added added}))))
+  new-app-state)
+
+(defn- transition-log [app-state message]
+  (log-deltas app-state (transition app-state message) message))
+
 (defn send-message [app-state-ref message]
-  ;; (js/console.trace (clj->js event) "\n" (clj->js args)) ;; uncomment to trace messages
-  ;;(swap! app-state-ref transition message)
-  (om/transact! app-state-ref #(transition % message))
+  ;; rename transition to transition-log to log messages
+  (om/transact! app-state-ref #(transition-log % message))
   (effects @app-state-ref message))
