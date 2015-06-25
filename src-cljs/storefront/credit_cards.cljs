fd4c4941f2611317745a637f1f978ab1758b4a4b
Jeff Hui <jeff@mayvenn.com>
2015-06-25 08:59:40 -0700

    Add missing 6 to cc number

--- src-cljs/storefront/credit_cards.cljs
+++ src-cljs/storefront/credit_cards.cljs
@@ -2,7 +2,7 @@
   (:require [clojure.string :as string]
             [goog.string :as gstring]))
 
-(def digits #{\0 \1 \2 \3 \4 \5 \7 \8 \9})
+(def digits #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
 
 (defn filter-cc-number-format [s]
   (->> s
