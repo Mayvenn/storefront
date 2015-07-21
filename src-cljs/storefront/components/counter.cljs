949cc8fda10a0834fe9e621e800202f3d3cbadd9
Greg Winn <gregw@mayvenn.com>
2015-07-21 15:55:30 -0700

    Add spinner styling on counter

--- src-cljs/storefront/components/counter.cljs
+++ src-cljs/storefront/components/counter.cljs
@@ -8,32 +8,32 @@
             [om.core :as om]))
 
 (defn counter-component [data owner {:keys [path set-event inc-event dec-event spinner-path]}]
-  (om/component
-   (html
-    [:div.quantity
-     [:div.quantity-selector
-      [:div.minus [:a.pm-link
-                   {:href "#"
-                    :on-click (utils/send-event-callback data
-                                                         dec-event
-                                                         {:path path})}
-                   "-"]]
-      (let [updating-cart (get-in data spinner-path)]
+  (let [spinning (when spinner-path (get-in data spinner-path))]
+    (om/component
+     (html
+      [:div.quantity
+       [:div.quantity-selector
+        [:div.minus
+         [:a.pm-link
+          {:href "#"
+           :on-click (utils/send-event-callback data
+                                                dec-event
+                                                {:path path})}
+          "-"]]
         [:input#quantity.quantity-selector-input
          {:min 1
           :name "quantity"
           :type "text"
+          :class (when spinning "saving")
           :value (str (get-in data path))
-          :class (when updating-cart
-                   "saving")
           :on-change #(send data
                             set-event
                             {:value-str (.. % -target -value)
-                             :path path})}])
-      [:div.plus [:a.pm-link
-                  {:href "#"
-                   :on-click (utils/send-event-callback data
-                                                        inc-event
-                                                        {:path path})}
-                  "+"]]]])))
+                             :path path})}]
+        [:div.plus [:a.pm-link
+                    {:href "#"
+                     :on-click (utils/send-event-callback data
+                                                          inc-event
+                                                          {:path path})}
+                    "+"]]]]))))
  
