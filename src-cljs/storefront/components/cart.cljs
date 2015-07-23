bbd52e2a4623689e6f3119afa5f4392a91a24ab7
Greg Winn <gregw@mayvenn.com>
2015-07-23 11:36:34 -0700

    Correct add coupon behavior when pressing enter

--- src-cljs/storefront/components/cart.cljs
+++ src-cljs/storefront/components/cart.cljs
@@ -24,36 +24,41 @@
 (defn display-full-cart [data owner]
   (let [cart (get-in data keypaths/order)]
     [:div
-     [:form#update-cart
+     [:div#update-cart
       [:div.inside-cart-form
        [:div.cart-items
         [:div.cart-line-items
          (display-line-items data cart true)]
         [:div.cart-bottom
-         [:div.coupon-cart
-          [:h4 "Have a Coupon Code?"]
-          [:div.coupon-container
-           [:label "Enter a coupon code:"]
-           [:input.coupon-code-input
-            (merge
-             (utils/change-text data owner keypaths/cart-coupon-code)
-             {:type "text"
-              :name "coupon-code"})]]
-          [:div.primary.button#update-button
-           (let [spinning (get-in data (concat keypaths/api-requests request-keys/update-coupon))]
-             {:type "submit" :name "update" 
-              :class (when spinning "saving")
-              :disabled spinning
-              :on-click (utils/send-event-callback data events/control-cart-update-coupon)})
-           "Update"]]
-         [:div.order-summary-cart
-          (display-order-summary cart)
-          [:input.button.checkout.primary#checkout-link
-           {:type "submit"
-            :value "Checkout"
-            :name "checkout"
-            :disabled (block-on-spin data)
-            :on-click (utils/send-event-callback data events/control-checkout-cart-submit)}]]]]]]
+         [:form
+          {:on-submit (utils/send-event-callback data events/control-cart-update-coupon)}
+          [:div.coupon-cart
+           [:h4 "Have a Coupon Code?"]
+           [:div.coupon-container
+            [:label "Enter a coupon code:"]
+            [:input.coupon-code-input
+             (merge
+              (utils/change-text data owner keypaths/cart-coupon-code)
+              {:type "text"
+               :name "coupon-code"})]]
+           [:div.primary.button#update-button
+            (let [spinning (get-in data (concat keypaths/api-requests request-keys/update-coupon))]
+              {:type "submit"
+               :name "update"
+               :class (when spinning "saving")
+               :disabled spinning
+               :on-click (utils/send-event-callback data events/control-cart-update-coupon)})
+            "Update"]]]
+         [:form
+          {:on-submit (utils/send-event-callback data events/control-checkout-cart-submit)}
+          [:div.order-summary-cart
+           (display-order-summary cart)
+           [:input.button.checkout.primary#checkout-link
+            {:type "submit"
+             :value "Checkout"
+             :name "checkout"
+             :disabled (block-on-spin data)
+             :on-click (utils/send-event-callback data events/control-checkout-cart-submit)}]]]]]]]
      [:a.cart-continue.continue.button.gray
       (shopping-link-attrs data)
       "Continue shopping"]]))
