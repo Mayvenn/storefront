d974166824c191408bb424161f363047c1b13e96
Corey Reece <corey@mayvenn.com>
2015-07-27 13:40:25 -0700

    Make checkout options on payment to be human-readible

--- src-cljs/storefront/effects.cljs
+++ src-cljs/storefront/effects.cljs
@@ -324,7 +324,9 @@
 (defmethod perform-effects events/control-checkout-payment-method-submit [_ event args app-state]
   (let [use-store-credit (get-in app-state keypaths/checkout-use-store-credits)
         covered-by-store-credit (get-in app-state keypaths/order-covered-by-store-credit)]
-    (analytics/track-checkout-option 4 (str use-store-credit "/" covered-by-store-credit))
+    (analytics/track-checkout-option 4 (str (if use-store-credit "creditYes" "creditNo")
+                                            "/"
+                                            (if covered-by-store-credit "creditCovers" "partiatCovers")))
     (api/update-order (get-in app-state keypaths/handle-message)
                       (get-in app-state keypaths/user-token)
                       (let [order (get-in app-state keypaths/order)]
