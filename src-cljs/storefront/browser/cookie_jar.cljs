32f52efb585b348bb55761e4315477a2cb7e010a
Sheel Choksi <sheelchoksi@gmail.com>
2015-10-01 17:11:26 -0700

    Enforce required attributes are present on cookie
    Corrupted or bad cookies will not put is in irrepresentable state

--- src-cljs/storefront/browser/cookie_jar.cljs
+++ src-cljs/storefront/browser/cookie_jar.cljs
@@ -7,18 +7,39 @@
   (Cookies. js/document))
 
 (def user-attrs [:email :user-token :store-slug :id])
+(def user-req-attrs [:email :user-token :id])
+
 (def order-attrs [:token :number])
+(def order-req-attrs order-attrs)
+
 (def remember-me-age (* 60 60 24 7 4))
 (def session-age (* 60 60 24 7 52))
 (def secure? (not config/development?))
 
+(defn clear-attrs [attrs cookie]
+  (doseq [attr attrs]
+    (.remove cookie attr)))
+
+(def clear-user (partial clear-attrs user-attrs))
+(def clear-order (partial clear-attrs order-attrs))
+(def clear (juxt clear-user clear-order))
+
+(defn has-required-attrs? [m req-attrs]
+  (every? seq (vals (select-keys m req-attrs))))
+
 (defn retrieve-login [cookie]
-  (zipmap user-attrs
-          (map #(.get cookie %) user-attrs)))
+  (let [attrs (zipmap user-attrs
+                      (map #(.get cookie %) user-attrs))]
+    (if (has-required-attrs? attrs user-req-attrs)
+      attrs
+      (clear-user cookie))))
 
 (defn retrieve-current-order [cookie]
-  (zipmap order-attrs
-          (map #(.get cookie %) order-attrs)))
+  (let [attrs (zipmap order-attrs
+                      (map #(.get cookie %) order-attrs))]
+    (if (has-required-attrs? attrs order-req-attrs)
+      attrs
+      (clear-order cookie))))
 
 (defn force-session-id [cookie]
   (if-let [session-id (.get cookie :session-id)]
@@ -40,11 +61,3 @@
       (if-let [val (attr attrs)]
         (.set cookie attr val age "/" nil secure?)
         (.remove cookie attr)))))
-
-(defn clear-order [cookie]
-  (doseq [attr order-attrs]
-    (.remove cookie attr)))
-
-(defn clear [cookie]
-  (doseq [attr (concat user-attrs order-attrs)]
-    (.remove cookie attr)))
