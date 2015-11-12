a68de6cb7e46b78f81a380a85fe71761b4d1d42d
Sheel Choksi <sheelchoksi@gmail.com>
2015-11-12 10:43:54 -0800

    Reorganize top level component's divs
    - Move the slideout-nav closer to the header, since it's more semantic
      there
    - Position the page-wrap to be relative so all the headers/menus are
      under it
    - Allows easier repositioning of elements to be on the top of the
      page (getting ready for experiment where promotion bar is moved up to
      the top of the page)

--- src-cljs/storefront/components/top_level.cljs
+++ src-cljs/storefront/components/top_level.cljs
@@ -35,64 +35,63 @@
 (defn top-level-component [data owner]
   (om/component
    (html
-    [:div
-     (om/build slideout-nav-component data)
-     [:div (cond
-             (get-in data keypaths/menu-expanded)
-             {:on-click (utils/send-event-callback
-                         data
-                         events/control-menu-collapse
-                         {:keypath keypaths/menu-expanded})}
+    [:div (cond
+            (get-in data keypaths/menu-expanded)
+            {:on-click (utils/send-event-callback
+                        data
+                        events/control-menu-collapse
+                        {:keypath keypaths/menu-expanded})}
 
-             (get-in data keypaths/account-menu-expanded)
-             {:on-click (utils/send-event-callback
-                         data
-                         events/control-menu-collapse
-                         {:keypath keypaths/account-menu-expanded})}
+            (get-in data keypaths/account-menu-expanded)
+            {:on-click (utils/send-event-callback
+                        data
+                        events/control-menu-collapse
+                        {:keypath keypaths/account-menu-expanded})}
 
-             (get-in data keypaths/shop-menu-expanded)
-             {:on-click (utils/send-event-callback
-                         data
-                         events/control-menu-collapse
-                         {:keypath keypaths/shop-menu-expanded})}
+            (get-in data keypaths/shop-menu-expanded)
+            {:on-click (utils/send-event-callback
+                        data
+                        events/control-menu-collapse
+                        {:keypath keypaths/shop-menu-expanded})}
 
-             :else {})
-      [:div.page-wrap
-       (om/build header-component data)
-       (when-let [msg (get-in data keypaths/flash-success-message)]
-         [:div.flash.success msg])
-       (when-let [msg (get-in data keypaths/flash-failure-message)]
-         [:div.flash.error msg])
-       (om/build promotion-banner-component data)
+            :else {})
+     [:div.page-wrap
+      (om/build header-component data)
+      (om/build slideout-nav-component data)
+      (when-let [msg (get-in data keypaths/flash-success-message)]
+        [:div.flash.success msg])
+      (when-let [msg (get-in data keypaths/flash-failure-message)]
+        [:div.flash.error msg])
+      (om/build promotion-banner-component data)
 
-       [:main {:role "main"}
-        [:div.container
+      [:main {:role "main"}
+       [:div.container
 
-         (om/build
-          (condp = (get-in data keypaths/navigation-event)
-            events/navigate-home home-component
-            events/navigate-cart cart-component
-            events/navigate-categories categories-component
-            events/navigate-category category-component
-            events/navigate-product product-component
-            events/navigate-guarantee thirty-day-guarantee-component
-            events/navigate-help help-component
-            events/navigate-sign-in sign-in-component
-            events/navigate-sign-up sign-up-component
-            events/navigate-forgot-password forgot-password-component
-            events/navigate-reset-password reset-password-component
-            events/navigate-stylist-commissions stylist-commissions-component
-            events/navigate-stylist-bonus-credit stylist-bonus-credit-component
-            events/navigate-stylist-referrals stylist-referrals-component
-            events/navigate-stylist-manage-account stylist-manage-account-component
-            events/navigate-manage-account (requires-sign-in data manage-account-component)
-            events/navigate-checkout-address (requires-sign-in data checkout-address-component)
-            events/navigate-checkout-delivery (requires-sign-in data checkout-delivery-component)
-            events/navigate-checkout-payment (requires-sign-in data checkout-payment-component)
-            events/navigate-checkout-confirmation (requires-sign-in data checkout-confirmation-component)
-            events/navigate-order-complete (requires-sign-in data checkout-complete-component)
-            events/navigate-order (requires-sign-in data order-component)
-            events/navigate-my-orders (requires-sign-in data my-orders-component)
-            home-component)
-          data)]]]
-      (om/build footer-component data)]])))
+        (om/build
+         (condp = (get-in data keypaths/navigation-event)
+           events/navigate-home home-component
+           events/navigate-cart cart-component
+           events/navigate-categories categories-component
+           events/navigate-category category-component
+           events/navigate-product product-component
+           events/navigate-guarantee thirty-day-guarantee-component
+           events/navigate-help help-component
+           events/navigate-sign-in sign-in-component
+           events/navigate-sign-up sign-up-component
+           events/navigate-forgot-password forgot-password-component
+           events/navigate-reset-password reset-password-component
+           events/navigate-stylist-commissions stylist-commissions-component
+           events/navigate-stylist-bonus-credit stylist-bonus-credit-component
+           events/navigate-stylist-referrals stylist-referrals-component
+           events/navigate-stylist-manage-account stylist-manage-account-component
+           events/navigate-manage-account (requires-sign-in data manage-account-component)
+           events/navigate-checkout-address (requires-sign-in data checkout-address-component)
+           events/navigate-checkout-delivery (requires-sign-in data checkout-delivery-component)
+           events/navigate-checkout-payment (requires-sign-in data checkout-payment-component)
+           events/navigate-checkout-confirmation (requires-sign-in data checkout-confirmation-component)
+           events/navigate-order-complete (requires-sign-in data checkout-complete-component)
+           events/navigate-order (requires-sign-in data order-component)
+           events/navigate-my-orders (requires-sign-in data my-orders-component)
+           home-component)
+         data)]]]
+     (om/build footer-component data)])))
