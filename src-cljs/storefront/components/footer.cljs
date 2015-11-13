f4c407097dbb70e1054e60ba1a51b5c8de751f8b
Sheel Choksi <sheelchoksi@gmail.com>
2015-11-12 17:22:08 -0800

    Minimal footer on auth/cart/checkout pages

--- src-cljs/storefront/components/footer.cljs
+++ src-cljs/storefront/components/footer.cljs
@@ -4,44 +4,60 @@
             [sablono.core :refer-macros [html]]
             [storefront.events :as events]
             [storefront.components.utils :as utils]
+            [storefront.hooks.experiments :as experiments]
             [storefront.keypaths :as keypaths]))
 
+(def minimal-footer-events
+  #{events/navigate-sign-in
+    events/navigate-sign-up
+    events/navigate-forgot-password
+    events/navigate-reset-password
+    events/navigate-cart
+    events/navigate-checkout-address
+    events/navigate-checkout-delivery
+    events/navigate-checkout-payment
+    events/navigate-checkout-confirmation})
+
 (defn footer-component [data owner]
-  (om/component
-   (html
-    [:footer#footer
-     [:div.footer-logo-container [:figure.footer-logo]]
-     [:div.contact-us
-      [:div.footer-container
-       [:ul.contact-us-menu
-        [:li.cu-item
-         [:a.cu-link.send-sonar-dynamic-number-link
-          (when-let [number (get-in data keypaths/sms-number)]
-            {:href (str "sms://+1" number)})
-          [:i.icon-chat]
-          "text"]]
-        [:li.cu-item
-         [:a.cu-link
-          {:href "mailto:help@mayvenn.com"}
-          [:i.icon-email]
-          "email"]]
-        [:li.cu-item
-         [:a.cu-link
-          {:href "tel://+18885627952"}
-          [:i.icon-phone]
-          "phone"]]]]]
-     [:div.sm-icons
-      [:div.footer-container
-       [:ul.sm-list
-        [:li.sm-icon.icon-facebook
-         [:a.full-link {:href "https://www.facebook.com/MayvennHair"}]]
-        [:li.sm-icon.icon-instagram
-         [:a.full-link {:href "http://instagram.com/mayvennhair"}]]
-        [:li.sm-icon.icon-pinterest
-         [:a.full-link {:href "http://www.pinterest.com/mayvennhair/"}]]
-        [:li.sm-icon.icon-twitter
-         [:a.full-link {:href "https://twitter.com/MayvennHair"}]]]]]
-     [:div.legal
-      [:div.copyright {:dangerouslySetInnerHTML {:__html (str "&copy; Mayvenn " (.getFullYear (js/Date.)))}}]
-      [:a.terms {:target "_blank" :href "/tos.html"} "Terms of Use"]
-      [:a.privacy {:target "_blank" :href "/privacy.html"} "Privacy Policy"]]])))
+  (let [minimal-footer? (and (experiments/simplify-funnel? data)
+                             (minimal-footer-events (get-in data keypaths/navigation-event)))]
+    (om/component
+     (html
+      [:footer#footer
+       (when-not minimal-footer? [:div.footer-logo-container [:figure.footer-logo]])
+       [:div.contact-us
+        [:div.footer-container
+         [:ul.contact-us-menu
+          [:li.cu-item
+           [:a.cu-link.send-sonar-dynamic-number-link
+            (when-let [number (get-in data keypaths/sms-number)]
+              {:href (str "sms://+1" number)})
+            [:i.icon-chat]
+            "text"]]
+          [:li.cu-item
+           [:a.cu-link
+            {:href "mailto:help@mayvenn.com"}
+            [:i.icon-email]
+            "email"]]
+          [:li.cu-item
+           [:a.cu-link
+            {:href "tel://+18885627952"}
+            [:i.icon-phone]
+            "phone"]]]]]
+       (when-not minimal-footer?
+         [:div.sm-icons
+          [:div.footer-container
+           [:ul.sm-list
+            [:li.sm-icon.icon-facebook
+             [:a.full-link {:href "https://www.facebook.com/MayvennHair"}]]
+            [:li.sm-icon.icon-instagram
+             [:a.full-link {:href "http://instagram.com/mayvennhair"}]]
+            [:li.sm-icon.icon-pinterest
+             [:a.full-link {:href "http://www.pinterest.com/mayvennhair/"}]]
+            [:li.sm-icon.icon-twitter
+             [:a.full-link {:href "https://twitter.com/MayvennHair"}]]]]])
+       (when-not minimal-footer?
+         [:div.legal
+          [:div.copyright {:dangerouslySetInnerHTML {:__html (str "&copy; Mayvenn " (.getFullYear (js/Date.)))}}]
+          [:a.terms {:target "_blank" :href "/tos.html"} "Terms of Use"]
+          [:a.privacy {:target "_blank" :href "/privacy.html"} "Privacy Policy"]])]))))
