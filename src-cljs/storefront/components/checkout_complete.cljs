92939b842fb0c64584823899034c5bad4a92da43
Sheel Choksi <sheelchoksi@gmail.com>
2015-11-26 00:05:34 -0500

    Add shipping delay message for Black Friday
    On a timer, shows up around when the sale starts

--- src-cljs/storefront/components/checkout_complete.cljs
+++ src-cljs/storefront/components/checkout_complete.cljs
@@ -4,6 +4,7 @@
             [storefront.keypaths :as keypaths]
             [storefront.hooks.experiments :as experiments]
             [storefront.components.utils :as utils]
+            [storefront.accessors.black-friday :as bf]
             [storefront.events :as events]))
 
 (defn checkout-complete-component [data owner]
@@ -19,6 +20,9 @@
      [:p.order-thanks-detail
       "We've received your order and will being processing it right away. Once your order ships we will send you another e-mail confirmation."]
 
+     (when (bf/after-black-friday-start?)
+       [:p.order-thanks-detail.emphasis
+        "Due to the high volume of orders, shipping may be delayed. We are working hard to deliver as quickly as possible. Happy Holidays!"])
      [:a.big-button.left-half.button.primary
       (merge (utils/route-to data events/navigate-home)
              (when (experiments/simplify-funnel? data)
