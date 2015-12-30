08a03bb26b34e137502e2b76944655d3507f46f5
Jacob Maine <jacob@mayvenn.com>
2015-12-30 10:05:09 -0800

    Ugh: vertical alignment

--- src-cljs/storefront/components/facebook.cljs
+++ src-cljs/storefront/components/facebook.cljs
@@ -1,6 +1,7 @@
 (ns storefront.components.facebook)
 
 (defn button [data]
-  [:button.fb-login-button {:on-click "/* TODO: run FB.login() on a <button> click, with public_profile,email scope */"}
-   [:img {:src "/images/FacebookWhite.png"}]
-   "Sign in with Facebook"])
+  [:button.fb-login-button #_{:on-click "/* TODO: run FB.login() on a <button> click, with public_profile,email scope */"}
+   [:div.fb-login-wrapper
+    [:img {:src "/images/FacebookWhite.png" :width 29 :height 29}]
+    [:div.fb-login-content "Sign in with Facebook"]]])
