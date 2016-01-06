543ecfb47b3f69ddb258384255462db2610f7b7d
Jacob Maine <jacob@mayvenn.com>
2016-01-06 14:32:27 -0800

    Use a different FB dev app

--- src-cljs/storefront/config.cljs
+++ src-cljs/storefront/config.cljs
@@ -34,4 +34,4 @@
 (def facebook-app-id (case js/environment
                        "production" 1536288310021691
                        "acceptance" 1537350883248767
-                       1536578859992636))
+                       1539908182993037))
