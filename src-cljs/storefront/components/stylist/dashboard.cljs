86c59b3864d547da6d4e2415777c7ad70010b6c7
Jacob Maine <jacob@mayvenn.com>
2016-03-15 19:53:51 -0700

    Autoprefix transitions
    So the tab slider works in more browsers.  Also rename/move a few
    helpers.

--- src-cljs/storefront/components/stylist/dashboard.cljs
+++ src-cljs/storefront/components/stylist/dashboard.cljs
@@ -10,51 +10,52 @@
             [storefront.events :as events]
             [storefront.keypaths :as keypaths]))
 
-(defn link-with-selected [data event ref label]
+(defn tab-link [data event ref label]
   (let [navigation-state (get-in data keypaths/navigation-event)]
     [:a.black.py1.mx3.lg-mt2
      (merge {:ref ref
              :data-test (str "nav-" ref)}
             (utils/route-to data event)) label]))
 
+(defn sliding-indicator [{:keys [left width]}]
+  [:div.border-teal.border.absolute.transition-all.transition-ease-in.transition-1
+   {:style {:margin-top "-2px"
+            :left       (str left "px")
+            :width      (str width "px")}}])
+
 (def tab-refs ["bonuses" "commissions" "referrals"])
 (def nav-events [events/navigate-stylist-dashboard-bonus-credit events/navigate-stylist-dashboard-commissions events/navigate-stylist-dashboard-referrals])
 (def labels ["Bonuses" "Commissions" "Referrals"])
 
-(defn save-tab-bounds [owner]
-  (om/set-state! owner
-                 {:bounds (vec (for [tab-ref tab-refs]
-                                 (let [parent-rect (.getBoundingClientRect (om/get-node owner "tabs"))
-                                       rect (.getBoundingClientRect (om/get-node owner tab-ref))]
-                                   {:left (- (.-left rect) (.-left parent-rect))
-                                    :width (.-width rect)})))}))
-
-(defn stylist-dashboard-nav-component [data owner]
-  (let [handle-resize-event (fn [e] (save-tab-bounds owner))]
+(defn nav-component [data owner]
+  (letfn [(tab-bounds []
+            (let [parent-rect (.getBoundingClientRect (om/get-node owner "tabs"))]
+              (vec (for [tab-ref tab-refs]
+                     (let [rect (.getBoundingClientRect (om/get-node owner tab-ref))]
+                       {:left  (- (.-left rect) (.-left parent-rect))
+                        :width (.-width rect)})))))
+          (cache-tab-bounds [] (om/set-state! owner {:tab-bounds (tab-bounds)}))
+          (handle-resize-event [e] (cache-tab-bounds))]
     (reify
       om/IDidMount
       (did-mount [this]
         (js/window.addEventListener "resize" handle-resize-event)
-        (save-tab-bounds owner))
+        (cache-tab-bounds))
       om/IWillUnmount
       (will-unmount [this]
         (js/window.removeEventListener "resize" handle-resize-event))
       om/IRenderState
-      (render-state [this {:keys [bounds]}]
+      (render-state [this {:keys [tab-bounds]}]
         (let [navigation-state (get-in data keypaths/navigation-event)
               tab-position     (utils/position #(= % navigation-state) nav-events)
-              tab-underline    (get bounds tab-position)]
+              bounds           (get tab-bounds tab-position)]
           (html
            [:nav.bg-silver.h5 {:ref "tabs"}
             [:div.bg-lighten-4
              [:div.flex.justify-center
               (for [[event ref label] (map vector nav-events tab-refs labels)]
-                (link-with-selected data event ref label))]
-             [:div.border-teal.border.absolute
-              {:style {:margin-top "-2px"
-                       :transition "all 0.25s ease-in"
-                       :left       (str (:left tab-underline) "px")
-                       :width      (str (:width tab-underline) "px")}}]]]))))))
+                (tab-link data event ref label))]
+             (sliding-indicator bounds)]]))))))
 
 (defn stylist-dashboard-component [data owner]
   (om/component
@@ -63,7 +64,7 @@
      [:.legacy-container
       (om/build stylist-dashboard-stats-component data)
 
-      (om/build stylist-dashboard-nav-component data)
+      (om/build nav-component data)
       (om/build
        (condp = (get-in data keypaths/navigation-event)
          events/navigate-stylist-dashboard-commissions  stylist-commissions-component
