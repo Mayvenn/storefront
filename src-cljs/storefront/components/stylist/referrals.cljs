b5150217e331736e2346e777a0f940043a0f88d6
Jacob Maine <jacob@mayvenn.com>
2016-03-21 21:17:29 -0700

    Refactor some inline CSS into helpers; break down referrals component

--- src-cljs/storefront/components/stylist/referrals.cljs
+++ src-cljs/storefront/components/stylist/referrals.cljs
@@ -11,43 +11,52 @@
         circumference   (* 2 js/Math.PI inner-radius)
         arc-length      (* circumference (- 1 percent-filled))
         svg-circle-size {:r inner-radius :cy radius :cx radius :stroke-width stroke-width :fill "none"}]
-    [:svg {:width diameter :height diameter :xmlns "http://www.w3.org/2000/svg"
-           :style {:transform         "rotate(-90deg)"
-                   :-webkit-transform "rotate(-90deg)"}}
+    [:svg.rotate-270 {:width diameter :height diameter :xmlns "http://www.w3.org/2000/svg"}
      [:circle.stroke-silver svg-circle-size]
      [:circle.stroke-teal (merge svg-circle-size {:style {:stroke-dasharray circumference :stroke-dashoffset arc-length}})]]))
 
-(defn stylist-referral-component [earning-amount {:keys [stylist-name paid-at commissioned-revenue bonus-due]}]
+(defn profile-picture-circle [profile-picture-url]
+  (let [width "4em"]
+    [:.circle.bg-silver.overflow-hidden {:style {:width width :height width}}
+     [:img {:src profile-picture-url :style {:width width}}]]))
+
+(defn state-icon [state earning-amount commissioned-revenue]
+  (let [revenue     (min earning-amount commissioned-revenue) ;; BUG: backend should never show commissioned-revenue past earning-amount, though it does
+        radius      36
+        diameter    (* 2 radius)
+        circle-size {:style {:width (str diameter "px") :height (str diameter "px")}}]
+    (case state
+      :referred    [:div.relative
+                    ;; Absolute centering: https://www.smashingmagazine.com/2013/08/absolute-horizontal-vertical-centering-css/
+                    [:.h6.gray.muted.center.absolute.overlay.m-auto {:style {:height "10%"}} "No Sales"]
+                    [:.border-dashed.border-gray.circle circle-size]]
+      :paid        [:.img-earned-icon circle-size]
+      :in-progress [:div.relative
+                    ;; Absolute centering: https://www.smashingmagazine.com/2013/08/absolute-horizontal-vertical-centering-css/
+                    [:.center.absolute.overlay.m-auto {:style {:height "38%"}}
+                     ;; Explicit font size because font-scaling breaks the circular progress
+                     [:.h2.teal {:style {:font-size "18px"}} (f/as-money-without-cents (js/Math.floor revenue))]
+                     [:.h6.gray.line-height-4 {:style {:font-size "9px"}} "of " (f/as-money-without-cents earning-amount)]]
+                    (circular-progress {:radius         radius
+                                        :stroke-width   5
+                                        :percent-filled (/ revenue earning-amount)})])))
+
+(defn show-referral [earning-amount {:keys [referred-stylist paid-at commissioned-revenue bonus-due]}]
   (html
-   (let [state (cond
+   (let [{:keys [name join-date profile-picture-url]} referred-stylist
+         state (cond
                  paid-at                      :paid
                  (zero? commissioned-revenue) :referred
                  :else                        :in-progress)]
-     [:.flex.items-center.justify-between.border.border-right.border-white.p2.sm-pr4
-      [:.circle.bg-silver.mr1 {:style {:width "4em" :height "4em"}}]
-      [:.flex-auto.overflow-hidden
-       [:.h2 stylist-name]
+     [:.flex.items-center.justify-between.border.border-right.border-white.p2.sm-pr3
+      [:.mr1 (profile-picture-circle profile-picture-url)]
+      [:.flex-auto
+       [:.h2 name]
        [:.h6.gray.line-height-4
-        [:div "Joined " "FIXME"]
+        [:div "Joined " (f/long-date join-date)]
         (when (= state :paid)
           [:div "Credit Earned: " [:span.black (f/as-money-without-cents bonus-due) " on " (f/locale-date paid-at)]])]]
-      (let [revenue     (min earning-amount commissioned-revenue) ;; BUG: backend should never show commissioned-revenue past earning-amount, though it does
-            radius      36
-            diameter    (* 2 radius)
-            circle-size {:style {:width (str diameter "px") :height (str diameter "px")}}]
-        [:.ml1.relative
-         (case state
-           :referred    [:div
-                         [:.h6.gray.muted.center.absolute.left-0.right-0 {:style {:top "50%" :margin-top "-0.3rem"}} "No Sales"]
-                         [:.border-dashed.border-gray.circle circle-size]]
-           :paid        [:.img-earned-icon circle-size]
-           :in-progress [:div
-                         [:div.absolute.left-0.right-0.center {:style {:top "50%" :margin-top "-15px"}}
-                          [:.teal.h2 {:style {:font-size "18px"}} (f/as-money-without-cents (js/Math.floor revenue))]
-                          [:.h6.gray.line-height-4 {:style {:font-size "9px"}} "of " (f/as-money-without-cents earning-amount)]]
-                         (circular-progress {:radius         radius
-                                             :stroke-width   5
-                                             :percent-filled (/ revenue earning-amount)})])])])))
+      [:.ml1 (state-icon state earning-amount commissioned-revenue)]])))
 
 (defn show-lifetime-total [lifetime-total]
   (let [message (str "You have earned " (f/as-money-without-cents lifetime-total) " in referrals since you joined Mayvenn.")]
@@ -59,36 +68,35 @@
       [:.mr1 svg/micro-dollar-sign]
       [:.center message]]]))
 
-(defn refer-ad [bonus-amount earning-amount sales-rep-email]
-  (let [message (str "Earn " (f/as-money-without-cents bonus-amount) " in credit when each stylist sells their first " (f/as-money-without-cents earning-amount))]
+(defn show-refer-ad [sales-rep-email bonus-amount earning-amount]
+  (let [mailto (str "mailto:" sales-rep-email "?Subject=Referral&body=name:%0D%0Aemail:%0D%0Aphone:")
+        message (str "Earn " (f/as-money-without-cents bonus-amount) " in credit when each stylist sells their first " (f/as-money-without-cents earning-amount))]
     [:.border-bottom.border-white
      [:.py2.px3.to-sm-hide.border-bottom.border-white
       [:.img-mail-icon.bg-no-repeat.bg-center {:style {:height "4em"}}]
       [:p.py1.h5.muted.overflow-hidden.line-height-3 message]
-      [:.h3.col-8.mx-auto.mb3 [:a.col-12.btn.btn-primary.border-teal {:href sales-rep-email :target "_top"} "Refer"]]]
+      [:.h3.col-8.mx-auto.mb3 [:a.col-12.btn.btn-primary.border-teal {:href mailto :target "_top"} "Refer"]]]
 
      [:div.p2.clearfix.sm-up-hide
       [:.left.mx1.img-mail-icon.bg-no-repeat {:style {:height "4em" :width "4em"}}]
-      [:.right.ml2.m1.h2.col-4 [:a.col-12.btn.btn-primary.btn-big.border-teal {:href sales-rep-email :target "_top"} "Refer"]]
+      [:.right.ml2.m1.h2.col-4 [:a.col-12.btn.btn-primary.btn-big.border-teal {:href mailto :target "_top"} "Refer"]]
       [:p.py1.h5.muted.overflow-hidden.line-height-3 message]] ]))
 
 (defn stylist-referrals-component [data owner]
   (om/component
    (html
-    (let [sales-rep-email (str "mailto:"
-                               (get-in data keypaths/stylist-sales-rep-email)
-                               "?Subject=Referral&body=name:%0D%0Aemail:%0D%0Aphone:")
-          bonus-amount (get-in data keypaths/stylist-referral-program-bonus-amount)
-          earning-amount (get-in data keypaths/stylist-referral-program-earning-amount)
-          referrals (get-in data keypaths/stylist-referral-program-referrals)]
-      [:.mx-auto.container.border.border-white {:data-test "referrals-panel"}
-       [:.clearfix
-        [:.sm-col-right.sm-col-4
-         (refer-ad bonus-amount earning-amount sales-rep-email)]
+    [:.mx-auto.container.border.border-white {:data-test "referrals-panel"}
+     [:.clearfix
+      [:.sm-col-right.sm-col-4
+       (show-refer-ad
+        (get-in data keypaths/stylist-sales-rep-email)
+        (get-in data keypaths/stylist-referral-program-bonus-amount)
+        (get-in data keypaths/stylist-referral-program-earning-amount))]
 
-        [:.sm-col.sm-col-8
-         (for [referral referrals]
-           (stylist-referral-component earning-amount referral))]
-        [:.sm-col-right.sm-col-4 {:style {:clear "right"}}
-         (when-let [lifetime-total (get-in data keypaths/stylist-referral-program-lifetime-total)]
-           (show-lifetime-total lifetime-total))]]]))))
+      [:.sm-col.sm-col-8
+       (let [earning-amount (get-in data keypaths/stylist-referral-program-earning-amount)]
+         (for [referral (get-in data keypaths/stylist-referral-program-referrals)]
+           (show-referral earning-amount referral)))]
+      [:.sm-col-right.sm-col-4.clearfix
+       (when-let [lifetime-total (get-in data keypaths/stylist-referral-program-lifetime-total)]
+         (show-lifetime-total lifetime-total))]]])))
