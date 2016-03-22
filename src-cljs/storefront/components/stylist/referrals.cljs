fb648b0f3dbdbeebc7dbb76b99a6604473370b97
Jacob Maine <jacob@mayvenn.com>
2016-03-21 23:31:45 -0700

    Small cleanups of referrals dashboard

--- src-cljs/storefront/components/stylist/referrals.cljs
+++ src-cljs/storefront/components/stylist/referrals.cljs
@@ -28,7 +28,7 @@
     (html
      ;; Absolute centering: https://www.smashingmagazine.com/2013/08/absolute-horizontal-vertical-centering-css/
      [:.relative
-      [:.h6.gray.muted.center.absolute.overlay.m-auto {:style {:height "15%"}} "No Sales"]
+      [:.h6.gray.muted.center.absolute.overlay.m-auto {:style {:height "1em"}} "No Sales"]
       [:.border-dashed.border-gray.circle {:style {:width width :height width}}]])))
 (def paid-icon
   (let [width (str state-diameter "px")]
@@ -56,7 +56,7 @@
                  paid-at                      :paid
                  (zero? commissioned-revenue) :referred
                  :else                        :in-progress)]
-     [:.flex.items-center.justify-between.border.border-right.border-white.p2.sm-pr3
+     [:.flex.items-center.justify-between.border-right.border-white.p2
       [:.mr1 (profile-picture-circle profile-picture-url)]
       [:.flex-auto
        [:.h2 name]
@@ -64,10 +64,11 @@
         [:div "Joined " (f/long-date join-date)]
         (when (= state :paid)
           [:div "Credit Earned: " [:span.black (f/as-money-without-cents bonus-due) " on " (f/locale-date paid-at)]])]]
-      [:.ml1 (state-icon state earning-amount commissioned-revenue)]])))
+      [:.ml1.sm-mr3 (state-icon state earning-amount commissioned-revenue)]])))
 
 (defn show-lifetime-total [lifetime-total]
-  (let [message (str "You have earned " (f/as-money-without-cents lifetime-total) " in referrals since you joined Mayvenn.")]
+  (let [message (goog.string/format "You have earned %s in referrals since you joined Mayvenn."
+                                    (f/as-money-without-cents lifetime-total))]
     [:.h6.muted
      [:.p3.to-sm-hide
       [:.mb1.center svg/micro-dollar-sign]
@@ -78,17 +79,19 @@
 
 (defn show-refer-ad [sales-rep-email bonus-amount earning-amount]
   (let [mailto (str "mailto:" sales-rep-email "?Subject=Referral&body=name:%0D%0Aemail:%0D%0Aphone:")
-        message (str "Earn " (f/as-money-without-cents bonus-amount) " in credit when each stylist sells their first " (f/as-money-without-cents earning-amount))]
+        message (goog.string/format "Earn %s in credit when each stylist sells their first %s"
+                                    (f/as-money-without-cents bonus-amount)
+                                    (f/as-money-without-cents earning-amount))]
     [:.border-bottom.border-white
      [:.py2.px3.to-sm-hide.border-bottom.border-white
       [:.img-mail-icon.bg-no-repeat.bg-center {:style {:height "4em"}}]
-      [:p.py1.h5.muted.overflow-hidden.line-height-3 message]
+      [:p.py1.h5.muted.line-height-3 message]
       [:.h3.col-8.mx-auto.mb3 [:a.col-12.btn.btn-primary.border-teal {:href mailto :target "_top"} "Refer"]]]
 
      [:.p2.clearfix.sm-up-hide
       [:.left.mx1.img-mail-icon.bg-no-repeat {:style {:height "4em" :width "4em"}}]
       [:.right.ml2.m1.h2.col-4 [:a.col-12.btn.btn-primary.btn-big.border-teal {:href mailto :target "_top"} "Refer"]]
-      [:p.py1.h5.muted.overflow-hidden.line-height-3 message]] ]))
+      [:p.overflow-hidden.py1.h5.muted.line-height-3 message]] ]))
 
 (defn stylist-referrals-component [data owner]
   (om/component
