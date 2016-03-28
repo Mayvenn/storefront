f351dae1bae2e6f0007f27d25679cdf644b7f91c
Corey Reece <corey@mayvenn.com>
2016-03-28 13:31:06 -0700

    Use cursors instead of whole app-state for slide-out nav

--- src-cljs/storefront/components/slideout_nav.cljs
+++ src-cljs/storefront/components/slideout_nav.cljs
@@ -10,45 +10,38 @@
             [storefront.accessors.navigation :as navigation]
             [storefront.components.formatters :refer [as-money]]))
 
-(defn show-store-credit? [app-state]
-  (when-let [credit (get-in app-state keypaths/user-total-available-store-credit)]
-    (pos? credit)))
-
 (defn fake-href [event & [args]]
   {:href "#"
    :on-click (utils/send-event-callback event args)})
 
-(defn logged-in? [data]
-  (boolean (get-in data keypaths/user-email)))
+(defn navigate-hair [shop-now-navigation-message]
+  (apply utils/route-to shop-now-navigation-message))
 
-(defn navigate-hair [data]
-  (apply utils/route-to (navigation/shop-now-navigation-message data)))
-
-(defn navigate-kits [data]
-  (when-let [path (default-stylist-taxon-path data)]
+(defn navigate-kits [kits-path]
+  (when kits-path
     (utils/route-to events/navigate-category
-                    {:taxon-path path})))
+                    {:taxon-path kits-path})))
 
-(defn navigate-community [data]
-  {:href (or (get-in data keypaths/community-url) "#")
+(defn navigate-community [community-url]
+  {:href (or community-url "#")
    :on-click (utils/send-event-callback events/external-redirect-community)})
 
-(defn drop-down [data expanded-keypath [link-tag & link-contents] menu]
+(defn drop-down [expanded? menu-keypath [link-tag & link-contents] menu]
   [:.relative.z1
    (into [link-tag
-          (fake-href events/control-menu-expand {:keypath expanded-keypath})]
+          (fake-href events/control-menu-expand {:keypath menu-keypath})]
          link-contents)
-   (when (get-in data expanded-keypath)
+   (when expanded?
      [:div
-      {:on-click (utils/send-event-callback events/control-menu-collapse {:keypath expanded-keypath})}
+      {:on-click (utils/send-event-callback events/control-menu-collapse {:keypath menu-keypath})}
       [:.fixed.overlay]
       menu])])
 
-(defn account-store-credit [data]
-  (when (show-store-credit? data)
+(defn account-store-credit [available-store-credit]
+  (when (pos? available-store-credit)
     [:span.stylist-user-label
      "Store credit:"
-     [:span.store-credit-amount (as-money (get-in data keypaths/user-total-available-store-credit))]]))
+     [:span.store-credit-amount (as-money available-store-credit)]]))
 
 (def guest-account-menu
   [:span
@@ -56,60 +49,67 @@
    " | "
    [:a (utils/route-to events/navigate-sign-up) "Sign Up"]])
 
-(defn stylist-account-menu [data]
+(defn stylist-account-menu [{:keys [expanded? available-store-credit user-email community-url]}]
   (drop-down
-   data keypaths/account-menu-expanded
+   expanded?
+   keypaths/account-menu-expanded
    [:a.account-menu-link
-    (account-store-credit data)
-    [:span.account-detail-name [:span.stylist-user-label "Stylist:"] (get-in data keypaths/user-email)]
+    (account-store-credit available-store-credit)
+    [:span.account-detail-name [:span.stylist-user-label "Stylist:"] user-email]
     [:figure.down-arrow]]
    [:ul.account-detail-expanded
     [:li [:a (utils/route-to events/navigate-stylist-dashboard-commissions) "Dashboard"]]
     [:li [:a (utils/route-to events/navigate-stylist-manage-account) "Manage Account"]]
-    [:li [:a (navigate-community data) "Stylist Community"]]
+    [:li [:a (navigate-community community-url) "Stylist Community"]]
     [:li [:a (fake-href events/control-sign-out) "Logout"]]]))
 
-(defn customer-account-menu [data]
+(defn customer-account-menu [{:keys [expanded? available-store-credit user-email]}]
   (drop-down
-   data keypaths/account-menu-expanded
+   expanded?
+   keypaths/account-menu-expanded
    [:a.account-menu-link
-    (account-store-credit data)
-    [:span.account-detail-name (get-in data keypaths/user-email)]
+    (account-store-credit available-store-credit)
+    [:span.account-detail-name user-email]
     [:figure.down-arrow]]
    [:ul.account-detail-expanded
     [:li [:a (utils/route-to events/navigate-account-referrals) "Refer A Friend"]]
     [:li [:a (utils/route-to events/navigate-account-manage) "Manage Account"]]
     [:li [:a (fake-href events/control-sign-out) "Logout"]]]))
 
-(defn invasive-top-nav-component [data owner]
+(defn invasive-top-nav-component [{:keys [own-store? user-email] :as cursors} _]
   ;; WAT 1: This is within the slideout nav, but is invisible on small screens
   ;; WAT 2: It is part of the secondary nav, but overlays primary nav through positioning hacks
   (om/component
    (html
     [:div.account-detail
      (cond
-       (own-store? data) (stylist-account-menu data)
-       (logged-in? data) (customer-account-menu data)
-       :else             guest-account-menu)])))
+       own-store?           (stylist-account-menu cursors)
+       (boolean user-email) (customer-account-menu cursors)
+       :else                guest-account-menu)])))
 
-(defn stylist-shop-link [data]
+(defn stylist-shop-link [{:keys [expanded? navigate-hair-message stylist-kits-path]}]
   (drop-down
-   data keypaths/shop-menu-expanded
+   expanded?
+   keypaths/shop-menu-expanded
    [:a "Shop " [:figure.down-arrow]]
    [:ul.shop-menu-expanded.top-0
-    [:li [:a (navigate-hair data) "Hair Extensions"]]
-    [:li [:a (navigate-kits data) "Stylist Only Products"]]]))
+    [:li [:a (navigate-hair navigate-hair-message) "Hair Extensions"]]
+    [:li [:a (navigate-kits stylist-kits-path) "Stylist Only Products"]]]))
 
-(defn non-stylist-shop-link [data]
-  [:a (navigate-hair data) "Shop"])
+(defn non-stylist-shop-link [{:keys [navigate-hair-message]}]
+  [:a (navigate-hair navigate-hair-message) "Shop"])
 
-(defn middle-nav-component [data owner]
+(defn middle-nav-component [{:keys [navigate-hair-message expanded? stylist-kits-path own-store? store-name]} _]
   (om/component
    (html
     [:div
-     [:h2.horizontal-nav-title (:store_name (get-in data keypaths/store))]
+     [:h2.horizontal-nav-title store-name]
      [:ul.horizontal-nav-menu
-      [:li (if (own-store? data) (stylist-shop-link data) (non-stylist-shop-link data))]
+      [:li (if own-store?
+             (stylist-shop-link {:navigate-hair-message navigate-hair-message
+                                 :expanded?             expanded?
+                                 :stylist-kits-path     stylist-kits-path})
+             (non-stylist-shop-link {:navigate-hair-message navigate-hair-message}))]
       [:li [:a (utils/route-to events/navigate-guarantee) "30 Day Guarantee"]]
       [:li [:a (utils/route-to events/navigate-help) "Customer Service"]]]])))
 
@@ -123,62 +123,64 @@
      [:div.slideout-nav-link-icon {:class (str "icon-" icon-class)}]
      label]]))
 
-(defn nav-hair-box [data]
-  (nav-box "hair-extensions" "Hair Extensions" (navigate-hair data) {:full-width? true}))
+(defn nav-hair-box [navigate-hair-message]
+  (nav-box "hair-extensions" "Hair Extensions" (navigate-hair navigate-hair-message) {:full-width? true}))
 
 (defn logout-box []
   (nav-box "logout" "Logout" (fake-href events/control-sign-out)))
 
-(defn slideout-stylist-nav [data]
+(defn slideout-stylist-nav [{:keys [navigate-hair-message community-url stylist-kits-path]}]
   (list
    [:li.slideout-nav-section.stylist
     [:h3.slideout-nav-section-header.highlight "Manage Store"]
     (nav-box "stylist-dashboard" "Dashboard" (utils/route-to events/navigate-stylist-dashboard-commissions))
     (nav-box "edit-profile" "Edit Profile" (utils/route-to events/navigate-stylist-manage-account))
-    (nav-box "community" "Stylist Community" (navigate-community data) {:full-width? true})]
+    (nav-box "community" "Stylist Community" (navigate-community community-url) {:full-width? true})]
    [:li.slideout-nav-section
     [:h3.slideout-nav-section-header "Shop"]
-    (nav-hair-box data)
-    (nav-box "stylist-products" "Stylist Products" (navigate-kits data) {:full-width? true})]
+    (nav-hair-box navigate-hair-message)
+    (nav-box "stylist-products" "Stylist Products" (navigate-kits stylist-kits-path) {:full-width? true})]
    [:li.slideout-nav-section
     [:h3.slideout-nav-section-header "My Account"]
     (nav-box "manage-account" "Manage Account" (utils/route-to events/navigate-stylist-manage-account))
     (logout-box)]))
 
-(defn slideout-customer-nav [data]
+(defn slideout-customer-nav [navigate-hair-message]
   (list
    [:li.slideout-nav-section
     [:h3.slideout-nav-section-header "Shop"]
-    (nav-hair-box data)]
+    (nav-hair-box navigate-hair-message)]
    [:li.slideout-nav-section
     [:h3.slideout-nav-section-header "My Account"]
     (nav-box "refer-friend" "Refer A Friend" (utils/route-to events/navigate-account-referrals) {:full-width? true})
     (nav-box "manage-account" "Manage Account" (utils/route-to events/navigate-account-manage))
     (logout-box)]))
 
-(defn slideout-guest-nav [data]
+(defn slideout-guest-nav [navigate-hair-message]
   (list
    [:li.slideout-nav-section
     [:h3.slideout-nav-section-header "Shop"]
-    (nav-hair-box data)]
+    (nav-hair-box navigate-hair-message)]
    [:li.slideout-nav-section
     [:h3.slideout-nav-section-header "My Account"]
     (nav-box "sign-in" "Sign In" (utils/route-to events/navigate-sign-in))
     (nav-box "join" "Join" (utils/route-to events/navigate-sign-up))]))
 
-(defn slideout-nav-component-really [data owner]
+(defn slideout-nav-component-really [{:keys [available-store-credit user-email navigate-hair-message community-url stylist-kits-path own-store?]} _]
   (om/component
    (html
     [:ul.slideout-nav-list
-     (when (show-store-credit? data)
+     (when (pos? available-store-credit)
        [:li.slideout-nav-section
         [:h4.store-credit
          [:span.label "Available store credit:"]
-         [:span.value (as-money (get-in data keypaths/user-total-available-store-credit))]]])
+         [:span.value (as-money available-store-credit)]]])
      (cond
-       (own-store? data) (slideout-stylist-nav data)
-       (logged-in? data) (slideout-customer-nav data)
-       :else             (slideout-guest-nav data))
+       own-store?           (slideout-stylist-nav {:navigate-hair-message navigate-hair-message
+                                                   :community-url         community-url
+                                                   :stylist-kits-path     stylist-kits-path})
+       (boolean user-email) (slideout-customer-nav navigate-hair-message)
+       :else                (slideout-guest-nav navigate-hair-message))
      [:li.slideout-nav-section
       [:h3.slideout-nav-section-header "Help"]
       (nav-box "customer-service" "Customer Service" (utils/route-to events/navigate-help))
@@ -187,9 +189,17 @@
 (defn slideout-nav-component [data owner]
   (om/component
    (html
-    (let [store (get-in data keypaths/store)
-          store-photo (:profile_picture_url store)
-          slid-out? (get-in data keypaths/menu-expanded)]
+    (let [{store-photo :profile_picture_url
+           store-name  :store_name} (get-in data keypaths/store)
+          slid-out?                 (get-in data keypaths/menu-expanded)
+          account-menu-expanded?    (get-in data keypaths/account-menu-expanded)
+          shop-menu-expanded?       (get-in data keypaths/shop-menu-expanded)
+          available-store-credit    (get-in data keypaths/user-total-available-store-credit)
+          community-url             (get-in data keypaths/community-url)
+          user-email                (get-in data keypaths/user-email)
+          own-store?                (own-store? data)
+          navigate-hair-message     (navigation/shop-now-navigation-message data)
+          stylist-kits-path         (default-stylist-taxon-path data)]
       [:div.slideout-nav-wrapper
        {:class (when slid-out? "slideout-nav-open")}
        [:nav.slideout-nav (when-not store-photo {:class "no-picture"})
@@ -201,14 +211,30 @@
           (if store-photo
             [:img.slideout-nav-portrait {:src store-photo}]
             [:div.slideout-nav-portrait.missing-picture])]
-         [:h2.slideout-nav-title (store :store_name)]]
+         [:h2.slideout-nav-title store-name]]
         ;; WAT: This is invisible on small screens
         [:div.horizontal-nav-list
-         (om/build invasive-top-nav-component data)
-         (om/build middle-nav-component data)]
+         (om/build invasive-top-nav-component
+                   {:own-store?             own-store?
+                    :expanded?              account-menu-expanded?
+                    :available-store-credit available-store-credit
+                    :user-email             user-email
+                    :community-url          community-url})
+         (om/build middle-nav-component
+                   {:navigate-hair-message navigate-hair-message
+                    :expanded?             shop-menu-expanded?
+                    :stylist-kits-path     stylist-kits-path
+                    :own-store?            own-store?
+                    :store-name            store-name})]
         (when slid-out?
           [:div
            {:on-click (utils/send-event-callback events/control-menu-collapse
                                                  {:keypath keypaths/menu-expanded})}
            [:.fixed.overlay]
-           (om/build slideout-nav-component-really data)])]]))))
+           (om/build slideout-nav-component-really
+                     {:available-store-credit available-store-credit
+                      :user-email                  user-email
+                      :own-store?             own-store?
+                      :navigate-hair-message  navigate-hair-message
+                      :stylist-kits-path      stylist-kits-path
+                      :community-url          community-url})])]]))))
