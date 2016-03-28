a39c384fe2e5fd907b254059ba0d81846e620b98
Corey Reece <corey@mayvenn.com>
2016-03-28 15:39:19 -0700

    Introduce cursors for header
    - Split component for getsat

--- src-cljs/storefront/components/header.cljs
+++ src-cljs/storefront/components/header.cljs
@@ -7,36 +7,40 @@
             [storefront.keypaths :as keypaths]
             [storefront.hooks.experiments :as experiments]))
 
+(def getsat-header
+  (html
+   [:header#header.header.comm-login
+    [:div.logo.comm-login]]))
+
+(defn topnav-component [{:keys [store order home-page?]} _]
+  (om/component
+   (html
+    (let [product-quantity (orders/product-quantity order)
+          {store-photo-url :profile_picture_url store-name :store_name} store]
+      [:header#header.header
+       (when-not store-photo-url
+         {:class "no-picture"})
+       [:a.header-menu {:href "#"
+                        :on-click
+                        (utils/send-event-callback events/control-menu-expand
+                                                   {:keypath keypaths/menu-expanded})}
+        "Menu"]
+       [:a.logo (utils/route-to events/navigate-home)]
+       (if (> product-quantity 0)
+         [:a.cart.populated (utils/route-to events/navigate-cart) product-quantity]
+         [:a.cart (utils/route-to events/navigate-cart)])
+       (when home-page?
+         [:div.stylist-bar
+          [:div.stylist-bar-img-container
+           [:img.stylist-bar-portrait {:src store-photo-url}]]
+          [:div.stylist-bar-name store-name]])]))))
+
 (defn header-component [data owner]
-  (let [getsat-login? (get-in data keypaths/get-satisfaction-login?)]
-    (om/component
-     (html
-      (let [store (get-in data keypaths/store)]
-        [:header#header.header
-         (merge (when-not (:profile_picture_url store)
-                  {:class "no-picture"})
-                (when getsat-login?
-                  {:class "comm-login"}))
-         (when-not getsat-login?
-           [:a.header-menu {:href "#"
-                            :on-click
-                            (utils/send-event-callback events/control-menu-expand
-                                                       {:keypath keypaths/menu-expanded})}
-            "Menu"])
-         (if getsat-login?
-           [:div.logo.comm-login]
-           [:a.logo (utils/route-to events/navigate-home)])
-         (when-not getsat-login?
-           (list
-            (let [product-quantity (orders/product-quantity (get-in data keypaths/order))]
-              (if (> product-quantity 0)
-                [:a.cart.populated
-                 (utils/route-to events/navigate-cart)
-                 product-quantity]
-                [:a.cart
-                 (utils/route-to events/navigate-cart)]))
-            (when (= (get-in data keypaths/navigation-event) events/navigate-home)
-              [:div.stylist-bar
-               [:div.stylist-bar-img-container
-                [:img.stylist-bar-portrait {:src (store :profile_picture_url)}]]
-               [:div.stylist-bar-name (store :store_name)]])))])))))
+  (om/component
+   (html
+    (if (get-in data keypaths/get-satisfaction-login?)
+      getsat-header
+      (om/build topnav-component
+                {:store      (get-in data keypaths/store)
+                 :home-page? (= (get-in data keypaths/navigation-event) events/navigate-home)
+                 :order      (get-in data keypaths/order)})))))
