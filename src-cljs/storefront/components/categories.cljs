8ec98191dbb23ae62e197630a9bc5fcc08afd9ab
Jeff Hui <jeff@mayvenn.com>
2016-03-30 10:13:57 -0700

    Add margin top to categories page
    Restore --breakpoint-sm back to its original value. These breakpoint
    values indicate minimum-cutoff. The unprefixed CSS values should be used
    to style mobile.

--- src-cljs/storefront/components/categories.cljs
+++ src-cljs/storefront/components/categories.cljs
@@ -9,7 +9,7 @@
             [sablono.core :refer-macros [html]]
             [storefront.events :as events]))
 
-(defn category [taxon index]
+(defn category [frontals? index taxon]
   (let [taxon-name (taxon :name)
         taxon-path (taxon-path-for taxon)]
     [:a.col.mn1.p1
@@ -18,41 +18,25 @@
       (utils/route-to events/navigate-category
                       {:taxon-path taxon-path})
       (if (> index 5)
-        {:class "col-12"}
-        {:class "sm-col-6 md-col-6 lg-col-4"}))
-     [:.bg-no-repeat.bg-top.bg-cover.flex.items-center
+        {:class (str "col-12" (when frontals? " lg-col-6"))}
+        {:class "col-6 lg-col-4"}))
+     [:.bg-no-repeat.bg-top.bg-cover.flex.items-center.col-12
       {:class (str "img-" taxon-path)
-       :style {:width "100%"
-               :height "200px"}}
-      [:.h1.white.center.col-12.titleize.shadow
-       taxon-name]]]))
-
-(defn frontal-category [taxon index]
-  (let [taxon-name (taxon :name)
-        taxon-path (taxon-path-for taxon)]
-    [:a.col.mn1.p1
-     (merge
-      (utils/route-to events/navigate-category
-                      {:taxon-path taxon-path})
-      (if (> index 5)
-        {:class "sm-col-12 lg-col-6"}
-        {:class "sm-col-6 md-col-6 lg-col-4"}))
-     [:.bg-no-repeat.bg-top.bg-cover.flex.items-center
-      {:class (str "img-" taxon-path)
-       :style {:width "100%"
-               :height "200px"}}
+       :style {:height "200px"}}
       [:.h1.white.center.col-12.titleize.shadow
        taxon-name]]]))
 
 (defn categories-component [data owner]
   (om/component
    (html
-    [:div.bundle-builder
+    [:div.mt2
      [:.center.black
       [:h1.regular
        [:div "Select your favorite style"]]]
      [:div.clearfix.px2.py1.category-grid
-      (if (experiments/frontals? data)
-        (map frontal-category (filter-nav-taxons (get-in data keypaths/taxons)) (range))
-        (drop-last (map category (filter-nav-taxons (get-in data keypaths/taxons)) (range))))
+      (let [frontals? (experiments/frontals? data)
+            taxons (map-indexed (partial category frontals?) (filter-nav-taxons (get-in data keypaths/taxons)))]
+        (if frontals?
+          taxons
+          (drop-last taxons)))
       [:div {:style {:clear "both"}}]]])))
