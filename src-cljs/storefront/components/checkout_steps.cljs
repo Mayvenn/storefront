262581c8a65a8de9f3dc94297944567b2656051a
Jacob Maine <jacob@mayvenn.com>
2016-04-01 14:55:15 -0700

    Fix heat, which needs specific step names

--- src-cljs/storefront/components/checkout_steps.cljs
+++ src-cljs/storefront/components/checkout_steps.cljs
@@ -7,30 +7,31 @@
             [storefront.hooks.experiments :as experiments]
             [clojure.string :as string]))
 
-(defn ^:private steps [three-steps?]
-  (if three-steps?
-    [{:event events/navigate-checkout-address :name "your details"}
-     {:event events/navigate-checkout-payment :name "payment"}
-     {:event events/navigate-checkout-confirmation :name "review"}]
-    [{:event events/navigate-checkout-address :name "address"}
-     {:event events/navigate-checkout-delivery :name "shipping"}
-     {:event events/navigate-checkout-payment :name "payment"}
-     {:event events/navigate-checkout-confirmation :name "confirm"}]))
+(def ^:private three-steps
+  [{:event events/navigate-checkout-address :name "your details" :id "address"}
+   {:event events/navigate-checkout-payment :name "payment" :id "payment"}
+   {:event events/navigate-checkout-confirmation :name "review" :id "confirm"}])
 
-(defn ^:private display-progress-step [steps current-index index {step-name :name event :event}]
+(def ^:private four-steps
+  [{:event events/navigate-checkout-address :name "address" :id "address"}
+   {:event events/navigate-checkout-delivery :name "shipping" :id "shipping"}
+   {:event events/navigate-checkout-payment :name "payment" :id "payment"}
+   {:event events/navigate-checkout-confirmation :name "confirm" :id "confirm"}])
+
+(defn ^:private display-progress-step [steps-count current-index index {step-name :name step-id :id event :event}]
   [:li.progress-step
-   {:class (->> [step-name
-                 (str "col-" (/ 12 (count steps)))
+   {:class (->> [step-id
+                 (str "col-" (/ 12 steps-count))
                  (when (< index current-index) "completed")
                  (when (= index (inc current-index)) "next")
                  (when (= index current-index) "current")
                  (when (zero? index) "first")
-                 (when (= index (dec (count steps))) "last")]
+                 (when (= index (dec steps-count)) "last")]
                 (filter identity)
                 (string/join " "))}
    [:span
     (let [text [:div.progress-step-index
-                (str (inc index) " ")
+                (inc index)
                 [:br]
                 (string/capitalize step-name)]]
       (if (< index current-index)
@@ -38,13 +39,13 @@
         text))]])
 
 (defn checkout-step-bar [data]
-  (let [steps (steps (experiments/three-steps? data))
+  (let [steps (if (experiments/three-steps? data) three-steps four-steps)
         [current-index current-step] (->> steps
                                           (map-indexed vector)
                                           (filter #(= (:event (second %)) (get-in data keypaths/navigation-event)))
                                           first)]
     [:div.row
      [:div.columns.thirteen.omega
-      [:ol {:class (str "progress-steps checkout-step-" (:name current-step))}
-       (map-indexed (partial display-progress-step steps current-index)
+      [:ol {:class (str "progress-steps checkout-step-" (:id current-step))}
+       (map-indexed (partial display-progress-step (count steps) current-index)
                     steps)]]]))
