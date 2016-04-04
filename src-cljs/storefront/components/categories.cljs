58371a9e776c1d2efda0a25309c2ff505a694c54
Jacob Maine <jacob@mayvenn.com>
2016-04-04 14:23:39 -0700

    Narrow image for closures on desktop for people not in the experiment

--- src-cljs/storefront/components/categories.cljs
+++ src-cljs/storefront/components/categories.cljs
@@ -11,18 +11,20 @@
 (defn category [frontals? index taxon]
   (let [taxon-name (taxon :name)
         taxon-path (taxon-path-for taxon)]
-    [:a.col.p1
+    [:a.p1.inline-block
      (merge
       {:key taxon-path}
       (utils/route-to events/navigate-category
                       {:taxon-path taxon-path})
       (if (> index 5)
-        {:class (str "col-12" (when frontals? " lg-col-6"))}
+        {:class (if frontals?
+                  "col-6"
+                  "col-12 lg-col-6")}
         {:class "col-6 lg-col-4"}))
-     [:.bg-no-repeat.bg-top.bg-cover.flex.items-center.col-12
+     [:.bg-no-repeat.bg-top.bg-cover.flex.items-center
       {:class (str "img-" taxon-path)
        :style {:height "200px"}}
-      [:.h1.white.center.col-12.titleize.shadow
+      [:.h1.white.center.col-12.titleize.shadow.nowrap
        taxon-name]]]))
 
 (defn categories-component [data owner]
@@ -32,7 +34,7 @@
      [:.center.black
       [:h1.regular.py2
        [:div "Select your favorite style"]]]
-     [:div.clearfix.mxn1.category-grid
+     [:div.mxn1.category-grid.center
       (let [frontals? (experiments/frontals? data)
             taxons (map-indexed (partial category frontals?) (filter-nav-taxons (get-in data keypaths/taxons)))]
         (if frontals?
