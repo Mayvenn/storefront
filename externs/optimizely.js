3ed0c093314e995fae849d507d4e8355a1d43f80
Corey Reece <corey@mayvenn.com>
2016-04-05 14:57:00 -0700

    Add optimizely externs

--- externs/optimizely.js
+++ externs/optimizely.js
@@ -1,2 +1,6 @@
 var optimizely = [];
 optimizely.activateUniversalAnalytics = function() {};
+optimizely.data = {};
+optimizely.data.state = {};
+optimizely.data.state.activeExperiments = [];
+optimizely.data.state.variationIdsMap = {};
