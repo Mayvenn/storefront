8aebde61ee6db01b471d1f6c5ed94a94a0130f07
Jacob Maine <jacob@mayvenn.com>
2016-04-11 09:26:50 -0700

    Rename variable and improve comment

--- src-cljs/storefront/accessors/navigation.cljs
+++ src-cljs/storefront/accessors/navigation.cljs
@@ -3,11 +3,11 @@
             [storefront.events :as events]))
 
 (defn shop-now-navigation-message [data]
-  ;; If the last page was an individual product page, return there.
+  ;; If the last page was an individual category (bundle builder) page, return there.
   ;; Otherwise go to the categories page.
-  (let [[prev-nav-event _ :as product-page] (get-in data keypaths/previous-navigation-message)
+  (let [[prev-nav-event _ :as category-page] (get-in data keypaths/previous-navigation-message)
         current-page (get-in data keypaths/navigation-message)]
     (if (and (= prev-nav-event events/navigate-category)
-             (not= product-page current-page))
-      product-page
+             (not= category-page current-page))
+      category-page
       [events/navigate-categories {}])))
