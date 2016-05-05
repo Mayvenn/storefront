8d58cce5b3cc25e2508abedd3f32d394e5b18bf2
Corey Reece <corey@mayvenn.com>
2016-05-05 14:15:12 -0700

    Add redesigned forgot password page

--- src-cljs/storefront/components/forgot_password.cljs
+++ src-cljs/storefront/components/forgot_password.cljs
@@ -2,13 +2,38 @@
   (:require [om.core :as om]
             [sablono.core :refer-macros [html]]
             [storefront.components.utils :as utils]
+            [storefront.components.ui :as ui]
             [storefront.components.facebook :as facebook]
             [storefront.events :as events]
             [storefront.keypaths :as keypaths]
             [storefront.hooks.experiments :as experiments]
             [storefront.components.validation-errors :refer [validation-errors-component]]))
 
-(defn forgot-password-component [data owner]
+(defn redesigned-forgot-password-component [{:keys [email facebook-loaded?]} owner]
+  (om/component
+   (html
+    [:.bg-white
+     [:.flex.flex-column.items-center.black.sans-serif.col-12.md-col-9.lg-col-6.mx-auto.py3
+      [:.h2.mt1.mb2 "Reset your forgotten password"]
+
+      [:form.col-12.flex.flex-column.items-center
+       {:on-submit (utils/send-event-callback events/control-forgot-password-submit)}
+       [:input.hide {:type "submit"}]
+
+       (ui/text-field "Email" keypaths/forgot-password-email email
+                      {:autofocus "autofocus"
+                       :type "email"
+                       :name "email"
+                       :required true})
+
+       [ui/large-button
+        {:on-click (utils/send-event-callback events/control-forgot-password-submit)}
+        [ui/large-button-text "Reset my password"]]]
+
+      [:.h4.gray.extra-light.my2 "OR"]
+      (facebook/redesigned-sign-in-button facebook-loaded?)]])))
+
+(defn old-forgot-password-component [data owner]
   (om/component
    (html
     [:div
@@ -29,3 +54,16 @@
                                     :value "Reset my password"}]]]
       [:div.or-divider.my0 [:span "or"]]
       (facebook/sign-in-button (get-in data keypaths/loaded-facebook))]])))
+
+(defn query [data]
+  {:facebook-loaded?  (get-in data keypaths/loaded-facebook)
+   :email             (get-in data keypaths/forgot-password-email)})
+
+(defn forgot-password-component [data owner]
+  (om/component
+   (html
+    [:div
+     (if (experiments/three-steps-redesign? data)
+       (om/build redesigned-forgot-password-component (query data))
+       (om/build old-forgot-password-component data))])))
+
