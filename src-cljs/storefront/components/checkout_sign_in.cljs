beb395f12596c160823315bc2c26b9335c319a89
Jeff Hui <jeff@mayvenn.com>
2016-05-04 18:08:03 -0700

    Use redesigned checkout sign in as default sign in
    - Move sign in component to sign-in namespace
    - Share a derived component between checkout and default sign in

--- src-cljs/storefront/components/checkout_sign_in.cljs
+++ src-cljs/storefront/components/checkout_sign_in.cljs
@@ -3,6 +3,7 @@
             [sablono.core :refer-macros [html]]
             [storefront.components.utils :as utils]
             [storefront.components.facebook :as facebook]
+            [storefront.components.sign-in :as sign-in]
             [storefront.events :as events]
             [storefront.hooks.experiments :as experiments]
             [storefront.keypaths :as keypaths]))
@@ -19,49 +20,19 @@
 (def large-button-style {:class "my2 btn btn-large btn-primary btn-teal-gradient col-10"})
 (def large-button-text :.h3.p1.extra-light.letter-spacing-1)
 
-(defn redesigned-checkout-sign-in-component [{:keys [facebook-loaded? email password remember-me?]}]
-  [:.flex.flex-column.items-center.black.sans-serif.bg-white
-   [:.my2.h2.block. "I'm new here"]
-   [:div
-    (merge large-button-style
-           {:on-click (utils/send-event-callback events/control-checkout-as-guest-submit)})
-    [large-button-text "Guest Checkout"]]
-   [:.my3.border.col-2.border-light-gray]
-   [:.h2.mb1 "Already registered?"]
-   [:.h5 "Sign into your account below, and checkout even faster!"]
-   (facebook/sign-in-button facebook-loaded?)
-   [:.h4.gray.extra-light.mb2 "OR"]
-
-   [:form.col-12.flex.flex-column.items-center
-    {:on-submit (utils/send-event-callback events/control-sign-in-submit)}
-    [:input.hide {:type "submit"}]
-
-    (text-field "Email" keypaths/sign-in-email email
-                {:autofocus "autofocus"
-                 :type "email"
-                 :name "email"
-                 :required true})
-
-    (text-field "Password" keypaths/sign-in-password password
-                {:type "password"
-                 :name "password"
-                 :required true})
-    [:div
-     (merge large-button-style
-            {:on-click (utils/send-event-callback events/control-sign-in-submit)})
-     [large-button-text "Sign In"]]
-
-   [:.mt2.col-10.mb3
-     [:label.col.col-6.left-align.gray
-      [:input#remember-me.align-middle
-       (merge (utils/toggle-checkbox keypaths/sign-in-remember remember-me?)
-              {:type "checkbox" :name "remember-me"})]
-      [:.inline.ml1 "Remember me"]]
-    [:a.col.col-6.right-align.gray
-     (utils/route-to events/navigate-forgot-password) "Forgot Password?"]]]
-
-   [:.clearfix.center.gray.mb2 "Don't have an account? "
-    [:a.teal (utils/route-to events/navigate-sign-up) "Register Here"]]])
+(defn redesigned-checkout-sign-in-component [sign-in-form-data owner]
+  (om/component
+   (html
+    [:.flex.flex-column.items-center.black.sans-serif.bg-white
+     [:.my2.h2.block. "I'm new here"]
+     [:div
+      (merge large-button-style
+             {:on-click (utils/send-event-callback events/control-checkout-as-guest-submit)})
+      [large-button-text "Guest Checkout"]]
+     [:.my3.border.col-2.border-light-gray]
+     [:.h2.mb1 "Already registered?"]
+     [:.h5 "Sign into your account below, and checkout even faster!"]
+     (om/build sign-in/redesigned-sign-in-form-component sign-in-form-data)])))
 
 (defn old-checkout-sign-in [data owner]
   [:div.centered-content
@@ -106,18 +77,12 @@
    [:p.center "Don't have an account? "
     [:a (utils/route-to events/navigate-sign-up) "Register Here"]]])
 
-(defn query [data]
-  {:email (get-in data keypaths/sign-in-email)
-   :password (get-in data keypaths/sign-in-password)
-   :remember-me? (get-in data keypaths/sign-in-remember)
-   :facebook-loaded? (get-in data keypaths/loaded-facebook)})
-
 (defn checkout-sign-in-component [data owner]
   (om/component
    (html
     (if (experiments/three-steps-redesign? data)
-      (redesigned-checkout-sign-in-component (query data))
-      (old-checkout-sign-in data owner)))))
+      (om/build redesigned-checkout-sign-in-component (sign-in/query data))
+      (om/build old-checkout-sign-in data)))))
 
 (defn requires-sign-in-or-guest [app-state authorized-component]
   (if (or (get-in app-state keypaths/user-id)
--- src-cljs/storefront/components/sign_in.cljs
+++ src-cljs/storefront/components/sign_in.cljs
@@ -7,10 +7,69 @@
             [storefront.hooks.experiments :as experiments]
             [storefront.keypaths :as keypaths]))
 
-(defn sign-in-component [data owner]
+(defn text-field [label keypath value input-attributes]
+  (list
+   [:input.col-10.h3.border.border-width-1.border-light-gray.border-teal-gradient.mb1.col-10.rounded-1.glow.floating-label
+    (merge (utils/change-text keypath value)
+           (when (seq value) {:class "has-value"})
+           {:placeholder label}
+           input-attributes)]
+   [:label.col-10.h6.teal label]))
+
+(def large-button-style {:class "my2 btn btn-large btn-primary btn-teal-gradient col-10"})
+(def large-button-text :.h3.p1.extra-light.letter-spacing-1)
+
+(defn redesigned-sign-in-form-component [{:keys [facebook-loaded? email password remember-me? get-satisfaction-login?]}]
   (om/component
    (html
-    [:div.centered-content
+    [:.flex.flex-column.items-center.black.sans-serif.bg-white.col-12
+     (facebook/sign-in-button facebook-loaded?)
+     [:.h4.gray.extra-light.mb2 "OR"]
+
+     [:form.col-12.flex.flex-column.items-center
+      {:on-submit (utils/send-event-callback events/control-sign-in-submit)}
+      [:input.hide {:type "submit"}]
+
+      (text-field "Email" keypaths/sign-in-email email
+                  {:autofocus "autofocus"
+                   :type "email"
+                   :name "email"
+                   :required true})
+
+      (text-field "Password" keypaths/sign-in-password password
+                  {:type "password"
+                   :name "password"
+                   :required true})
+      [:div
+       (merge large-button-style
+              {:on-click (utils/send-event-callback events/control-sign-in-submit)})
+       [large-button-text "Sign In"]]
+
+      [:.mt2.col-10.mb3
+       [:label.col.col-6.left-align.gray
+        [:input#remember-me.align-middle
+         (merge (utils/toggle-checkbox keypaths/sign-in-remember remember-me?)
+                {:type "checkbox" :name "remember-me"})]
+        [:.inline.ml1 "Remember me"]]
+       [:a.col.col-6.right-align.gray
+        (utils/route-to events/navigate-forgot-password) "Forgot Password?"]]]
+
+     (when-not get-satisfaction-login?
+       [:.clearfix.center.gray.mb2 "Don't have an account? "
+        [:a.teal (utils/route-to events/navigate-sign-up) "Register Here"]])])))
+
+(defn redesigned-sign-in-component [{:keys [get-satisfaction-login?] :as form-data} owner]
+  (om/component
+   (html
+    [:.flex.flex-column.items-center.black.sans-serif.bg-white
+     (when-not get-satisfaction-login?
+       [:.h2.mb1.mt3 "Sign In to Your Account"])
+     (om/build redesigned-sign-in-form-component form-data)])))
+
+(defn old-sign-in-component [data owner]
+  (om/component
+   (html
+    [:.flex.flex-column.items-center.black.sans-serif.bg-white
      (when-not (get-in data keypaths/get-satisfaction-login?)
        [:div.page-heading.center "Sign In to Your Account"])
      [:div#existing-customer.new_spree_user
@@ -45,6 +104,19 @@
      (when-not (get-in data keypaths/get-satisfaction-login?)
        [:p.center "Don't have an account? "
         [:a (utils/route-to events/navigate-sign-up) "Register Here"]])])))
+
+(defn query [data]
+  {:email (get-in data keypaths/sign-in-email)
+   :password (get-in data keypaths/sign-in-password)
+   :remember-me? (get-in data keypaths/sign-in-remember)
+   :facebook-loaded? (get-in data keypaths/loaded-facebook)
+   :get-satisfaction-login? (get-in data keypaths/get-satisfaction-login?)})
+
+(defn sign-in-component [app-state owner]
+  (om/component
+   (html
+    [:div
+     (om/build redesigned-sign-in-component (query app-state))])))
 
 (defn requires-sign-in [app-state authorized-component]
   (if (get-in app-state keypaths/user-id)
