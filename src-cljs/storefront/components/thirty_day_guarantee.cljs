a639057bbf564290e04a435c5c89913d600bafb4
Jacob Maine <jacob@mayvenn.com>
2016-06-08 15:44:38 -0700

    Guarantee is in narrow container

--- src-cljs/storefront/components/thirty_day_guarantee.cljs
+++ src-cljs/storefront/components/thirty_day_guarantee.cljs
@@ -1,28 +1,30 @@
 (ns storefront.components.thirty-day-guarantee
   (:require [om.core :as om]
-            [sablono.core :refer-macros [html]]))
+            [sablono.core :refer-macros [html]]
+            [storefront.components.ui :as ui]))
 
 (defn thirty-day-guarantee-component [data owner]
   (om/component
    (html
-    [:.sans-serif.dark-gray
-     [:.bg-white.p2
-      [:.img-guarantee-icon.bg-no-repeat.bg-center {:style {:height "155px"}}]]
-     [:.center.px2.dark-black
-      [:.border-bottom.border-dark-white.border-width-2.py2
-       "We guarantee that you'll love Mayvenn hair!"]
-      [:.border-bottom.border-dark-white.border-width-2.py2
-       "Buy " [:div.green.inline "RISK FREE"] " with easy returns and exchanges."]]
-     [:.line-height-3.px2
-      [:.border-bottom.border-dark-white.border-width-2.py2.px2
-       [:.medium.navy "EXCHANGES"]
-       [:div "Wear it, dye it, even flat iron it. "
-        "If you do not love your Mayvenn hair we will exchange it within 30 days of purchase. "
+    (ui/narrow-container
+     [:.sans-serif.dark-gray
+      [:.bg-white.p2
+       [:.img-guarantee-icon.bg-no-repeat.bg-center {:style {:height "155px"}}]]
+      [:.center.px2.dark-black
+       [:.border-bottom.border-dark-white.border-width-2.py2
+        "We guarantee that you'll love Mayvenn hair!"]
+       [:.border-bottom.border-dark-white.border-width-2.py2
+        "Buy " [:div.green.inline "RISK FREE"] " with easy returns and exchanges."]]
+      [:.line-height-3.px2
+       [:.border-bottom.border-dark-white.border-width-2.py2.px2
+        [:.medium.navy "EXCHANGES"]
+        [:div "Wear it, dye it, even flat iron it. "
+         "If you do not love your Mayvenn hair we will exchange it within 30 days of purchase. "
+         "Just call us:"
+         [:a.medium.navy.block {:href "tel:1-888-562-7952"} "1-888-562-7952"]]]
+       [:.py2.px2
+        [:.medium.navy "RETURNS"]
+        "If you are not completely happy with your Mayvenn hair before it is installed, "
+        "we will refund your purchase if the bundle is unopened and the hair is in its original condition. "
         "Just call us:"
-        [:a.medium.navy.block {:href "tel:1-888-562-7952"} "1-888-562-7952"]]]
-      [:.py2.px2
-       [:.medium.navy "RETURNS"]
-       "If you are not completely happy with your Mayvenn hair before it is installed, "
-       "we will refund your purchase if the bundle is unopened and the hair is in its original condition. "
-       "Just call us:"
-        [:a.medium.navy.block {:href "tel:1-888-562-7952"} "1-888-562-7952"]]]])))
+        [:a.medium.navy.block {:href "tel:1-888-562-7952"} "1-888-562-7952"]]]]))))
