e2b7f30f61f1c61a4ef56d0b0a97f1d87b364447
Jacob Maine <jacob@mayvenn.com>
2016-06-08 18:37:54 -0700

    Remove dead code

--- src-cljs/storefront/components/friend_referrals.cljs
+++ src-cljs/storefront/components/friend_referrals.cljs
@@ -1,8 +1,5 @@
 (ns storefront.components.friend-referrals
   (:require [om.core :as om]
-            [storefront.hooks.talkable :as talkable]
-            [storefront.keypaths :as keypaths]
-            [storefront.events :as events]
             [sablono.core :refer-macros [html]]))
 
 (defn friend-referrals-component [data owner]
