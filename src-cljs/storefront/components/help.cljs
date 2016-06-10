46c0027447db5e6f278bc93380d8f2a9591e56ed
Jacob Maine <jacob@mayvenn.com>
2016-06-09 18:25:36 -0700

    Rework help icon gutters

--- src-cljs/storefront/components/help.cljs
+++ src-cljs/storefront/components/help.cljs
@@ -12,18 +12,14 @@
        (string/join "-")
        (str "1-")))
 
-(defn cells [& body]
-  [:.my2.lg-col-8.mx-auto
-   (into [:.clearfix.mxnp3] body)])
-(defn cell [& body]
-  (into [:.col.col-6.lg-up-col-3.pp3] body))
+(defn cell [link-attrs & body]
+  [:a.dark-gray.btn.btn-outline.border-silver.pxp6.py2.sm-m2.m1
+   (merge {:style {:height "134px" :width "142px"}}
+          link-attrs)
+   (into
+    [:.flex.flex-column.justify-between.items-center]
+    body)])
 
-(def link :a.dark-gray)
-
-(defn cell-border [& body]
-  (into [:.btn.btn-outline.border-silver.pxp6.py2.flex.flex-column.justify-between.items-center.mx-auto
-         {:style {:height "134px" :max-width "142px"}}]
-        body))
 (defn cell-icon [icon-class width height]
   [:.bg-center.bg-contain.bg-no-repeat.mb1
    {:style {:height height :width width}
@@ -46,32 +42,31 @@
       [:.border-bottom.border-light-silver.py2.line-height-4
        [:div "Monday to Friday from 9am to 5pm PST"]
        [:div "Orders placed before 10am PST ship that day"]]
-      (cells
-       (cell
-        [link {:href "http://help.mayvenn.com" :target "_blank"}
-         (cell-border
-          (cell-icon "img-faqs-icon" "44px" "52px")
-          [cell-text "FAQs"]
-          [cell-description "Visit our help center"])])
-       (cell
-        [link {:href "tel://+18885627952"}
-         (cell-border
-          (cell-icon "img-phone-icon" "45px" "52px")
-          [cell-text "Call"]
-          [cell-description "1-888-562-7952"])])
-       (cell
-        [link (when sms-number {:href (str "sms://+1" sms-number)})
-         (cell-border
-          (cell-icon "img-text-icon" "54px" "52px")
-          [cell-text "Text"]
-          [cell-description
-           (if sms-number
-             (display-sms sms-number)
-             "Loading...")])])
-       (cell
-        [link {:href "mailto:help@mayvenn.com"}
-         (cell-border
-          (cell-icon "img-email-icon" "49px" "52px")
-          [cell-text "Email"]
-          [cell-description
-           [:span {:style {:word-break "break-all"}} "help@mayvenn.com"]])]))]])))
+      [:.my2.flex.flex-wrap.justify-center
+       [:.flex
+        (cell
+         {:href "http://help.mayvenn.com"
+          :target "_blank"}
+         (cell-icon "img-faqs-icon" "44px" "52px")
+         [cell-text "FAQs"]
+         [cell-description "Visit our help center"])
+        (cell
+         {:href "tel://+18885627952"}
+         (cell-icon "img-phone-icon" "45px" "52px")
+         [cell-text "Call"]
+         [cell-description "1-888-562-7952"])]
+       [:.flex
+        (cell
+         (when sms-number {:href (str "sms://+1" sms-number)})
+         (cell-icon "img-text-icon" "54px" "52px")
+         [cell-text "Text"]
+         [cell-description
+          (if sms-number
+            (display-sms sms-number)
+            "Loading...")])
+        (cell
+         {:href "mailto:help@mayvenn.com"}
+         (cell-icon "img-email-icon" "49px" "52px")
+         [cell-text "Email"]
+         [cell-description
+          [:span {:style {:word-break "break-all"}} "help@mayvenn.com"]])]]]])))
