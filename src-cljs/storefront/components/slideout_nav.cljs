bc773d0760df3a6cd411043a9ed6bc743c2d0b08
Sheel Choksi <sheelchoksi@gmail.com>
2016-06-18 20:45:14 -0700

    Add divs to slideout nav

--- src-cljs/storefront/components/slideout_nav.cljs
+++ src-cljs/storefront/components/slideout_nav.cljs
@@ -14,24 +14,24 @@
             [storefront.app-routes :as app-routes]
             [storefront.hooks.experiments :as experiments]))
 
-(def section-inner :.ml3.py2)
-(def section-outer :.border-bottom.border-light-silver.bg-pure-white.black)
-(def section-outer-darker :.border-bottom.border-light-silver)
+(def section-inner :div.ml3.py2)
+(def section-outer :div.border-bottom.border-light-silver.bg-pure-white.black)
+(def section-outer-darker :div.border-bottom.border-light-silver)
 
 (defn row
   ([right] (row nil right))
   ([left right]
-   [:.clearfix.pyp1
-    [:.col.col-2 [:.px1 (or left ui/nbsp)]]
-    [:.col.col-10.line-height-3 right]]))
+   [:div.clearfix.pyp1
+    [:div.col.col-2 [:div.px1 (or left ui/nbsp)]]
+    [:div.col.col-10.line-height-3 right]]))
 
 (def menu-x
   (html
-   [:.absolute {:style {:width "60px"}}
-    [:.relative.rotate-45.p2 {:style {:height "60px"}
+   [:div.absolute {:style {:width "60px"}}
+    [:div.relative.rotate-45.p2 {:style {:height "60px"}
                               :on-click #(messages/handle-message events/control-menu-collapse-all)}
-     [:.absolute.border-right.border-dark-gray {:style {:width "18px" :height "36px"}}]
-     [:.absolute.border-bottom.border-dark-gray {:style {:width "36px" :height "18px"}}]]]))
+     [:div.absolute.border-right.border-dark-gray {:style {:width "18px" :height "36px"}}]
+     [:div.absolute.border-bottom.border-dark-gray {:style {:width "36px" :height "18px"}}]]]))
 
 (def logo
   (html
@@ -51,15 +51,15 @@
     content]))
 
 (defn store-credit-flag [credit]
-  [:.right.border-bottom.border-left.border-light-silver.bg-white
+  [:div.right.border-bottom.border-left.border-light-silver.bg-white
    {:style {:border-bottom-left-radius "8px"}
     :class (when (zero? credit) :invisible)}
-   [:.h5.px2.py1.line-height-1
+   [:div.h5.px2.py1.line-height-1
     [:span.gray "Credit: "] [:span.navy (as-money credit)]]])
 
 (defn customer-section [selectable? user-email]
   [:div
-   (row [:.truncate user-email])
+   (row [:div.truncate user-email])
    [:a.green.block (utils/route-to events/navigate-account-manage)
     (row (selectable? events/navigate-account-manage
                       [:span {:data-test "account-settings"} "Account Settings"]))]
@@ -71,7 +71,7 @@
     [:div
      (row
       (when store-photo
-        [:.mxn1.pyp3 (ui/circle-picture {:width "32px"} store-photo)])
+        [:div.mxn1.pyp3 (ui/circle-picture {:width "32px"} store-photo)])
       [:div nickname])
      [:div
       [:a.green.block (utils/route-to events/navigate-stylist-dashboard-commissions)
@@ -84,15 +84,15 @@
 
 (defn products-section [selectable? title taxons]
   [:div
-   (row [:.border-bottom.border-light-silver.nav.navy title])
-   [:.my1
+   (row [:div.border-bottom.border-light-silver.nav.navy title])
+   [:div.my1
     (for [{:keys [name slug]} taxons]
       [:a
        (merge {:key slug :data-test (str "menu-" slug)}
               (utils/route-to events/navigate-category {:taxon-slug slug}))
        (row
         (when (new-taxon? slug) ui/new-flag)
-        [:.green.titleize
+        [:div.green.titleize
          (selectable? events/navigate-category {:taxon-slug slug} name)])])]])
 
 (defn extensions-section [selectable? taxons]
@@ -107,14 +107,14 @@
 (defn customer-shop-section [selectable? taxons]
   [section-outer
    [section-inner
-    [:.sans-serif.medium "Shop"]
+    [:div.sans-serif.medium "Shop"]
     (extensions-section selectable? taxons)
     (closures-section selectable? taxons)]])
 
 (defn stylist-shop-section [selectable? taxons]
   [section-outer
    [section-inner
-    [:.sans-serif.medium "Shop"]
+    [:div.sans-serif.medium "Shop"]
     (extensions-section selectable? taxons)
     (closures-section selectable? taxons)
     (stylist-products-section selectable? taxons)]])
@@ -133,14 +133,14 @@
   (html
    [section-outer-darker
     [section-inner
-     [:.clearfix
-      [:.col.col-6.p1
+     [:div.clearfix
+      [:div.col.col-6.p1
        [:a.btn.btn-outline.navy.col-12
         (merge {:data-test "sign-in"}
                (utils/route-to events/navigate-sign-in))
         "Sign In"]]
-      [:.col.col-6.p1.center.h5.line-height-2
-       [:.gray "No account?"]
+      [:div.col.col-6.p1.center.h5.line-height-2
+       [:div.gray "No account?"]
        [:a.green
         (merge {:data-test "sign-up"}
                (utils/route-to events/navigate-sign-up))
@@ -181,14 +181,14 @@
   (om/component
    (html
     (let [selectable? (partial selectable current-navigation-message)]
-      [:.h3.lg-up-hide
+      [:div.h3.lg-up-hide
        {:class (when-not slid-out? "hide")}
-       [:.fixed.overlay.bg-darken-4.z3
+       [:div.fixed.overlay.bg-darken-4.z3
         ;; Clicks on the overlay close the slideout nav, without letting the click through to underlying links
         {:on-click (utils/send-event-callback events/control-menu-collapse-all)}]
-       [:.fixed.overflow-auto.top-0.left-0.col-10.z3.lit.bg-white.rounded-bottom-right-1
+       [:div.fixed.overflow-auto.top-0.left-0.col-10.z3.lit.bg-white.rounded-bottom-right-1
         {:style {:max-height "100%"}}
-        [section-outer-darker menu-x [:.p2 logo]]
+        [section-outer-darker menu-x [:div.p2 logo]]
         (cond
           stylist?   (stylist-content selectable? data)
           user-email (customer-content selectable? data)
