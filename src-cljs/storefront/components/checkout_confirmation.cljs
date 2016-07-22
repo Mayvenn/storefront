88950deaf7ce637d4f496258e40c5c6ac7e7d2a7
Greg Winn <gregw@mayvenn.com>
2016-07-21 17:14:54 -0700

    Additional payment should be before order summary

--- src-cljs/storefront/components/checkout_confirmation.cljs
+++ src-cljs/storefront/components/checkout_confirmation.cljs
@@ -41,7 +41,6 @@
        [:form
         {:on-submit (utils/send-event-callback events/control-checkout-confirmation-submit
                                                {:place-order? requires-additional-payment?})}
-        (summary/display-order-summary order)
         (when requires-additional-payment?
           [:div
            (ui/note-box
@@ -50,6 +49,7 @@
             [:.p2.navy
              "Please enter an additional payment method below for the remaining total on your order."])
            (om/build checkout-payment/credit-card-form-component payment)])
+        (summary/display-order-summary order)
         (ui/submit-button "Place Order" {:spinning? (or saving-card? placing-order?)
                                          :disabled? updating-shipping?
                                          :data-test "confirm-form-submit"})]]]))))
