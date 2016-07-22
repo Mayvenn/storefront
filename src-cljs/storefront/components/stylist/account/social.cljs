fefe6a00db20919d2617bc9c9ffc6ebed24a43d6
Greg Winn <gregw@mayvenn.com>
2016-07-21 17:12:27 -0700

    Change copy on social so it makes sense

--- src-cljs/storefront/components/stylist/account/social.cljs
+++ src-cljs/storefront/components/stylist/account/social.cljs
@@ -15,7 +15,7 @@
    [:form {:on-submit
            (utils/send-event-callback events/control-stylist-account-social-submit)}
 
-    [:h1.h2.light.my3.center.col.col-12.md-up-col-6 "Update your password"]
+    [:h1.h2.light.my3.center.col.col-12.md-up-col-6 "Connect your social"]
 
     [:div.col.col-12.md-up-col-6
      [:div.flex.col-12
