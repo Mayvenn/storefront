0cd47be53bc316e3a6795bf34d69540c1b40073f
Jacob Maine <jacob@mayvenn.com>
2016-08-09 20:51:28 -0700

    Fix ns

--- src/storefront/platform/images.clj
+++ src/storefront/platform/images.clj
@@ -1,4 +1,4 @@
 (ns storefront.platform.images
-  (require [storefront.assets :refer [asset-path]]))
+  (:require [storefront.assets :refer [asset-path]]))
 
 (def canonical-image (asset-path "/images/canonical_image.jpg"))
