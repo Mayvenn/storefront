7dd2575edf821850d53041889a97cfe429e57e52
Corey Reece <corey@mayvenn.com>
2016-08-24 15:27:00 -0700

    Improve video modal
    - Fix touch events
    - Added play button and copy

--- src-cljs/storefront/platform/video.cljs
+++ src-cljs/storefront/platform/video.cljs
@@ -18,15 +18,20 @@
     om/IRender
     (render [this]
       (html
-       (ui/modal
-        {:on-close on-close :bg-class "bg-darken-4"}
-        [:div.flex.items-center
-         {:style {:height "100vh"}}
-         [:div.wistia_responsive_padding.relative.col-12
-          {:style {:padding-top "56.25%"}}
-          [:div.wistia_responsive_wrapper.absolute.left-0.top-0.col-12
-           {:style {:height "100%"}}
-           [:span.wistia_embed.col-12
-            {:class (str "wistia_async_" video-id " videoFoam=true autoPlay=true volume=0.33")
-             :style {:height "100%"}}
-            ui/nbsp]]]])))))
+       (letfn [(cancel-close [e] (.stopPropagation e) false)]
+         (ui/modal
+          {:on-close on-close :bg-class "bg-darken-4"}
+          [:div.flex.items-center
+           {:style          {:height "100vh"}
+            :on-click       on-close
+            :on-touch-start on-close}
+           [:div.wistia_responsive_padding.relative.col-12
+            {:style {:padding-top "56.25%"}}
+            [:div.wistia_responsive_wrapper.absolute.left-0.top-0.col-12
+             {:style          {:height "100%"}
+              :on-click       cancel-close
+              :on-touch-start cancel-close}
+             [:span.wistia_embed.col-12
+              {:class (str "wistia_async_" video-id " videoFoam=true autoPlay=true volume=0.33")
+               :style {:height "100%"}}
+              ui/nbsp]]]]))))))
