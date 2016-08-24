e80ee61e24c5b5cbdd72c3dba6cbfad4fe4e8654
Corey Reece <corey@mayvenn.com>
2016-08-24 16:56:09 -0700

    Tweaks to video modal

--- src-cljs/storefront/platform/video.cljs
+++ src-cljs/storefront/platform/video.cljs
@@ -26,7 +26,7 @@
           [:div.fixed.overlay.bg-darken-4.z3
            (on-click-or-touch on-close)
            [:div.fixed.overlay.bg-darken-4]]
-          [:div.fixed.z3.left-0.right-0.mx-auto.overflow-auto.col-11
+          [:div.fixed.z3.left-0.right-0.mx-auto.overflow-auto.col-10
            {:style        {:max-height "100%"}
             :data-snap-to "top"}
            [:div.flex.flex-column.items-center.justify-center
@@ -41,6 +41,6 @@
                {:class (str "wistia_async_" video-id " videoFoam=true autoPlay=true volume=0.33")
                 :style {:height "100%"}}
                ui/nbsp]]]
-            [:div.white.p1.mt3
+            [:div.white.p2.m2
              (on-click-or-touch on-close)
              "close video"]]]])))))
