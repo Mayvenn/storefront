dde8c20bf36b36b99f34b502074e1200dae0e49e
Jeff Hui <jeff@mayvenn.com>
2016-09-27 12:01:39 -0700

    Add server side render for slide carousels

--- src/storefront/platform/carousel_two.clj
+++ src/storefront/platform/carousel_two.clj
@@ -3,6 +3,14 @@
 
 (defn component [{:keys [slides]} _ _]
   (component/create
-   [:div.overflow-hidden.nowrap
-    (for [slide slides]
-      [:div.inline-block slide])]))
+   [:div.center.relative
+    [:div.overflow-hidden.relative
+     {:ref "items"}
+     [:div.overflow-hidden.relative
+      (for [i (range (count slides))
+            :let [slide (nth slides i)]]
+        [:div.left.col-12.relative
+         (merge {:key (str i)}
+                (when (> i 0)
+                  {:class "display-none"}))
+         slide])]]]))
