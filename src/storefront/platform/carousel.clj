27e881e4a96b2ec83a0f627d617987eac32841bb
Jacob Maine <jacob@mayvenn.com>
2016-10-05 10:44:13 -0700

    Revert server-side version of carousel
    To one that renders all slides, instead of just first. This was
    incorrectly changed as the old carousel was being removed.

--- src/storefront/platform/carousel.clj
+++ src/storefront/platform/carousel.clj
@@ -3,14 +3,6 @@
 
 (defn component [{:keys [slides]} _ _]
   (component/create
-   [:div.center.relative
-    [:div.overflow-hidden.relative
-     {:ref "items"}
-     [:div.overflow-hidden.relative
-      (for [i (range (count slides))
-            :let [slide (nth slides i)]]
-        [:div.left.col-12.relative
-         (merge {:key (str i)}
-                (when (> i 0)
-                  {:class "display-none"}))
-         slide])]]]))
+   [:div.overflow-hidden.nowrap
+    (for [slide slides]
+      [:div.inline-block slide])]))
