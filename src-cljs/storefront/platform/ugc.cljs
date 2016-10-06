15114770868707138084bed7bedb5692e05b4487
Jacob Maine <jacob@mayvenn.com>
2016-10-06 10:34:07 -0700

    Center slides in pixlee carousel
    Update react-slick to fix centering algorithm.

--- src-cljs/storefront/platform/ugc.cljs
+++ src-cljs/storefront/platform/ugc.cljs
@@ -6,7 +6,7 @@
             [storefront.platform.carousel :as carousel]))
 
 (defn image-thumbnail [{:keys [id photo user-handle purchase-link]}]
-  [:div
+  [:div.p1
    [:img.col-12.block {:src photo}]
    #_[:div.my1.light-gray.bold.h4.right-align "@" user-handle]])
 
@@ -16,7 +16,15 @@
     (when (seq album)
       [:div.center.mt4
        [:div.h2.medium.dark-gray.crush.m2 "#MayvennMade"]
-       (om/build carousel/component {:slides (map image-thumbnail album)} opts)
+       (om/build carousel/component {:slides   (map image-thumbnail album)
+                                     :settings {:centerMode    true
+                                                :centerPadding "36px"
+                                                :slidesToShow  3
+                                                ;; The breakpoints are mobile-last. That is, the
+                                                ;; default values apply to the largest screens, and
+                                                ;; 768 means 768 and below.
+                                                :responsive [{:breakpoint 768
+                                                              :settings   {:slidesToShow 2}}]}} opts)
        [:p.center.gray.m2
         "Want to show up on our homepage? "
         "Tag your best pictures wearing Mayvenn with " [:span.bold "#MayvennMade"]]]))))
