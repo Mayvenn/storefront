8713181c90646a8491216ff2f1ce9510d423cb73
Jacob Maine <jacob@mayvenn.com>
2016-10-07 09:46:12 -0700

    Add comment

--- src-cljs/storefront/platform/ugc.cljs
+++ src-cljs/storefront/platform/ugc.cljs
@@ -17,15 +17,19 @@
     (when (seq album)
       [:div.center.mt4
        [:div.h2.medium.dark-gray.crush.m2 "#MayvennMade"]
-       (om/build carousel/component {:slides   (map image-thumbnail album)
-                                     :settings {:centerMode    true
-                                                :centerPadding "36px"
-                                                :slidesToShow  3
-                                                ;; The breakpoints are mobile-last. That is, the
-                                                ;; default values apply to the largest screens, and
-                                                ;; 768 means 768 and below.
-                                                :responsive [{:breakpoint 768
-                                                              :settings   {:slidesToShow 2}}]}} opts)
+       (om/build carousel/component
+                 {:slides   (map image-thumbnail album)
+                  :settings {:centerMode    true
+                             ;; must be in px, because it gets parseInt'd for
+                             ;; the slide width calculation
+                             :centerPadding "36px"
+                             ;; The breakpoints are mobile-last. That is, the
+                             ;; default values apply to the largest screens, and
+                             ;; 768 means 768 and below.
+                             :slidesToShow  3
+                             :responsive    [{:breakpoint 768
+                                              :settings   {:slidesToShow 2}}]}}
+                 opts)
        [:p.center.gray.m2
         "Want to show up on our homepage? "
         "Tag your best pictures wearing Mayvenn with " [:span.bold "#MayvennMade"]]]))))
