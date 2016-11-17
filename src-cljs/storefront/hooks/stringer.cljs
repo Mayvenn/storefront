ddb140f5d91ab24025b86532e83be3890e32d049
Jacob Maine <jacob@mayvenn.com>
2016-11-17 14:45:03 -0800

    Bump stringer, fix ability to remove duplicate script tags

--- src-cljs/storefront/hooks/stringer.cljs
+++ src-cljs/storefront/hooks/stringer.cljs
@@ -2,14 +2,16 @@
   (:require [storefront.browser.tags :refer [insert-tag-with-text remove-tags-by-class remove-tag-by-src]]
             [storefront.config :as config]))
 
+(def stringer-src "//d6w7wdcyyr51t.cloudfront.net/cdn/stringer/stringer-a2ceefe.js")
+
 (defn insert-tracking []
   (insert-tag-with-text
-   (str "(function(d,e){function g(a){return function(){var b=Array.prototype.slice.call(arguments);b.unshift(a);c.push(b);return d.stringer}}var c=d.stringer=d.stringer||[],a=[\"init\",\"track\",\"identify\",\"clear\"];if(!c.snippetRan&&!c.loaded){c.snippetRan=!0;for(var b=0;b<a.length;b++){var f=a[b];c[f]=g(f)}a=e.createElement(\"script\");a.type=\"text/javascript\";a.async=!0;a.src=\"//d6w7wdcyyr51t.cloudfront.net/cdn/stringer/stringer-a657521.js\";b=e.getElementsByTagName(\"script\")[0];b.parentNode.insertBefore(a,b);c.init({environment:\"" config/environment "\",sourceSite:\"storefront\"})}})(window,document);")
+   (str "(function(d,e){function g(a){return function(){var b=Array.prototype.slice.call(arguments);b.unshift(a);c.push(b);return d.stringer}}var c=d.stringer=d.stringer||[],a=[\"init\",\"track\",\"identify\",\"clear\"];if(!c.snippetRan&&!c.loaded){c.snippetRan=!0;for(var b=0;b<a.length;b++){var f=a[b];c[f]=g(f)}a=e.createElement(\"script\");a.type=\"text/javascript\";a.async=!0;a.src=\"" stringer-src "\";b=e.getElementsByTagName(\"script\")[0];b.parentNode.insertBefore(a,b);c.init({environment:\"" config/environment "\",sourceSite:\"storefront\"})}})(window,document);")
    "stringer"))
 
 (defn remove-tracking []
   (remove-tags-by-class "stringer")
-  (remove-tag-by-src config/stringer-src))
+  (remove-tag-by-src stringer-src))
 
 (defn track-event [event-name payload]
   (.track js/window.stringer event-name (clj->js payload)))
