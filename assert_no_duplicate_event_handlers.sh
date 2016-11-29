4143fc501e587e4c298e1122645e5beacdc321d3
Jacob Maine <jacob@mayvenn.com>
2016-11-28 16:49:44 -0800

    Add scripts to check for duplicate event handlers
    So we can abort the build if it finds duplicates.

---
+++ assert_no_duplicate_event_handlers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+event_dups () {
+  grep "defmethod $1" src-cljs/storefront/$2 | perl -pe "s/.*defmethod $1 (\S+).*/\1/" | sort | uniq -c | egrep '  1 ' -v
+}
+
+effect_dups ()     { event_dups perform-effects effects.cljs; }
+transition_dups () { event_dups transition-state transitions.cljs; }
+tracking_dups ()   { event_dups perform-track trackings.cljs; }
+
+test "0" = `transition_dups | wc -l` && 
+  test "0" = `effect_dups | wc -l` && 
+  test "0" = `tracking_dups | wc -l`
+
+SOME_DUPS=$?
+
+if [[ ! $SOME_DUPS = 0 ]]; then
+  (
+    echo "Duplicate handlers:"
+    echo "transitions:"
+    echo "  " `transition_dups`
+    echo "effects:"
+    echo "  " `effect_dups`
+    echo "trackings:"
+    echo "  " `tracking_dups`
+  ) 1>&2
+fi
+
+exit $SOME_DUPS
