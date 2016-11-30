83f19712c42c885966d4f3e6b2072a04864a4527
Sheel Choksi <sheelchoksi@gmail.com>
2016-11-29 17:48:33 -0800

    Remove other platform black friday code

--- src/storefront/platform/date.clj
+++ src/storefront/platform/date.clj
@@ -6,14 +6,3 @@
 
 (defn full-year [d]
   (.get d Calendar/YEAR))
-
-(defn between [start-iso8601 end-iso8601]
-  (<= (.toEpochMilli (.toInstant (java.time.OffsetDateTime/parse start-iso8601)))
-      (.getTimeInMillis (current-date))
-      (.toEpochMilli (.toInstant (java.time.OffsetDateTime/parse end-iso8601)))))
-
-(defn after [start-iso8601]
-  (<= (.toEpochMilli (.toInstant (java.time.OffsetDateTime/parse start-iso8601)))
-      (.getTimeInMillis (current-date))))
-
-(def black-friday "2016-11-24T21:00:00.000-08:00")
