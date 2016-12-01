74d7eb006a30576dcd818abd361b999857a9533c
Greg Winn <gregw@mayvenn.com>
2016-12-01 14:48:54 -0800

    Track successful reset password reset

--- src-cljs/storefront/trackings.cljs
+++ src-cljs/storefront/trackings.cljs
@@ -198,3 +198,6 @@
 
 (defmethod perform-track events/api-success-update-order-update-shipping-method [_ events args app-state]
   (stringer/track-event "checkout-shipping_method_change" {:order_number (get-in app-state keypaths/order-number)}))
+
+(defmethod perform-track events/api-success-forgot-password [_ events args app-state]
+  (stringer/track-event "request_reset_password" {}))
