cff6618b0c37d61fe7532dce10285f9fbe7fa3e1
Greg Winn <gregw@mayvenn.com>
2016-12-01 14:34:47 -0800

    track shipping method changes

--- src-cljs/storefront/trackings.cljs
+++ src-cljs/storefront/trackings.cljs
@@ -195,3 +195,6 @@
 
 (defmethod perform-track events/api-success-update-order-update-cart-payments [_ events args app-state]
   (stringer/track-event "checkout-payment_enter" {:order_number (get-in app-state keypaths/order-number)}))
+
+(defmethod perform-track events/api-success-update-order-update-shipping-method [_ events args app-state]
+  (stringer/track-event "checkout-shipping_method_change" {:order_number (get-in app-state keypaths/order-number)}))
