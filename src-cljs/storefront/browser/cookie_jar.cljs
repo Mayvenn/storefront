0ca0790afd92684f560aee02f20f1f0250d9c7e4
Sheel Choksi <sheelchoksi@gmail.com>
2016-12-20 14:31:18 -0800

    Properly use spec override of cookie
    The or condition was leaning back to true unfortunately

--- src-cljs/storefront/browser/cookie_jar.cljs
+++ src-cljs/storefront/browser/cookie_jar.cljs
@@ -77,10 +77,10 @@
       attrs
       (clear-cookie spec cookie))))
 
-(defn save-cookie [{:keys [max-age domain secure] :as spec} cookie attrs]
+(defn save-cookie [{:keys [max-age domain] :as spec} cookie attrs]
   (doseq [attr (all-keys spec)]
     (if-let [val (get attrs attr)]
-      (.set    cookie attr val max-age "/" domain (or secure config/secure?))
+      (.set    cookie attr val max-age "/" domain (get spec :secure config/secure?))
       (.remove cookie attr             "/" domain))))
 
 (def clear-order (partial clear-cookie order))
