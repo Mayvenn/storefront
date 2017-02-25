6c3107aac56da3e27aabf96fc6a037c4e0cded56
Jacob Maine <jacob@mayvenn.com>
2017-02-24 16:21:20 -0800

    Use built-in validator which only allows image uplaod

--- src-cljs/storefront/hooks/uploadcare.cljs
+++ src-cljs/storefront/hooks/uploadcare.cljs
@@ -13,17 +13,13 @@
               "uploadcare")
      #(handle-message events/inserted-uploadcare))))
 
-(def invalid-image-error-message
-  "Invalid Image Uploaded")
-
 (defn ^:private receive-file-info [file-info]
   (handle-message events/uploadcare-api-success-upload-image
                   {:file-info (js->clj file-info :keywordize-keys true)}))
 
 (defn ^:private handle-error [error file-info]
   (handle-message events/uploadcare-api-failure
-                  {:upload-was-not-valid-image? (= (.-message error) invalid-image-error-message)
-                   :error error
+                  {:error error
                    :file-info (js->clj file-info :keywordize-keys true)}))
 
 (defn ^:private handle-file [file]
@@ -32,21 +28,11 @@
         fail (.call (object/get promise "fail") promise handle-error)]
     (.call (object/get fail "done") fail receive-file-info)))
 
-(defn ^:private image-only [file-info]
-  (let [file-info (js->clj file-info :keywordize-keys true)]
-    (when (= (:isImage file-info) false) ;; nil means we don't have this information yet
-      ;; as per protocol, uploadcare wants us to throw errors to fail validation
-      (throw (js/Error. invalid-image-error-message)))))
-
 (defn dialog []
   (when (.hasOwnProperty js/window "uploadcare")
     (-> js/uploadcare
         (.openDialog
          nil
          (clj->js {:imageShrink "1600x1600"
-                   ;; note: validator functions get called 3 times:
-                   ;;       1. When the file is first specified, only the fileinfo's name field is available
-                   ;;       2. When the size is determined, only the fileinfo's name and size are available
-                   ;;       3. After the file is uploaded to uploadcare, all of the fileinfo's fields are available
-                   :validators [image-only]}))
+                   :imagesOnly  true}))
         (.done handle-file))))
