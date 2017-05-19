83fe830f92eb88b71dffff334f22422f4a303128
Jacob Maine <jacob@mayvenn.com>
2017-05-18 17:20:44 -0700

    Stop hand encoding shared cart urls
    [#nodeps]

--- src-cljs/storefront/components/cart.cljs
+++ src-cljs/storefront/components/cart.cljs
@@ -1,6 +1,7 @@
 (ns storefront.components.cart
   (:require [om.core :as om]
             [sablono.core :refer-macros [html]]
+            [cemerick.url :as url]
             [storefront.assets :as assets]
             [storefront.accessors.experiments :as experiments]
             [storefront.accessors.orders :as orders]
@@ -25,84 +26,88 @@
    (str cnt " " (if (= 1 (max cnt (- cnt))) singular plural))))
 
 (defn facebook-link [share-url]
-  (str "https://www.facebook.com/sharer/sharer.php?u=" share-url "%3Futm_medium=facebook%26utm_campaign=sharebuttons"))
+  (-> (url/url "https://www.facebook.com/sharer/sharer.php")
+      (assoc :query {:u (assoc-in share-url [:query :utm_medium] "facebook")})
+      str))
 
 (defn sms-link [share-url]
   ;; the ?& is to get this to work on iOS8 and Android at the same time
   (str "sms:?&body="
-       (js/encodeURIComponent "Shop the bundles I picked for you here: ")
-       share-url
-       "%3Futm_medium=sms%26utm_campaign=sharebuttons"))
+       (url/url-encode (str "Shop the bundles I picked for you here: "
+                            (assoc-in share-url [:query :utm_medium] "sms")))))
 
 (defn twitter-link [share-url]
-  (str "https://twitter.com/intent/tweet?url="
-       share-url "%3Futm_medium=twitter%26utm_campaign=sharebuttons"
-       "&text=" (js/encodeURIComponent "Shop my top virgin hair bundle picks here:")
-       "&hashtags=mayvennhair"))
+  (-> (url/url "https://twitter.com/intent/tweet")
+      (assoc :query {:url      (assoc-in share-url [:query :utm_medium] "twitter")
+                     :text     "Shop my top virgin hair bundle picks here:"
+                     :hashtags "mayvennhair"})
+      str))
 
 (defn email-link [share-url store-nickname]
   (str "mailto:?Subject="
-       (js/encodeURIComponent "My recommended bundles for you")
+       (url/url-encode "My recommended bundles for you")
        "&body="
-       (js/encodeURIComponent (str "Hey,
 
-I've created a ready-to-shop cart with the bundles I recommend for you. Mayvenn is my pick for the quality virgin human hair. They offer a totally free 30 day exchange program (if you have any issues with your hair at all). All you have to do is click the link below to check out.
+       (url/url-encode (str "Hey,
 
-Shop here: "
-                                   share-url"?utm_medium=email&utm_campaign=sharebuttons"
+I've created a ready-to-shop cart with the bundles I recommend for you. Mayvenn is my pick for quality virgin human hair. They offer a totally free 30 day exchange program (if you have any issues with your hair at all). All you have to do is click the link below to check out.
 
-                                   "
+Shop here: " (assoc-in share-url [:query :utm_medium] "email") "
 
 Let me know if you have any questions.
 
 Thanks,
 "
-                                   store-nickname))))
+                            store-nickname))))
 
 (defn share-link-component [{:keys [share-url store-nickname]} owner {:keys [close-attrs]}]
   (om/component
    (html
-    (ui/modal {:close-attrs close-attrs}
-              [:.bg-white.rounded.p4.center
-               (ui/modal-close {:close-attrs close-attrs :data-test "share-url-close"})
-               [:.p1
-                [:.h3.navy.medium "Share your bag"]
-                [:.h5.dark-gray.light.my2 "Share this link so your customers know exactly what to buy"]
-                [:.border-top.border-bottom.border-gray.py2
-                 [:div.clearfix.mxn1
-                  [:div.p1.col.col-6.col-12-on-dt
-                   [:a.h6.col-12.btn.btn-primary.bg-fb-blue
-                    {:href   (facebook-link share-url)
-                     :target "_blank"}
-                    [:img.align-middle.mr1 {:style {:height "18px"}
-                                            :src   (assets/path "/images/share/fb.png")}]
-                    "Share"]]
-                  [:div.p1.col.col-6.col-12-on-dt
-                   [:a.h6.col-12.btn.btn-primary.bg-twitter-blue
-                    {:href   (twitter-link share-url)
-                     :target "_blank"}
-                    [:img.align-middle.mr1 {:style {:height "18px"}
-                                            :src   (assets/path "/images/share/twitter.png")}]
-                    "Tweet"]]
-                  [:div.p1.col.col-6.col-12-on-dt.hide-on-dt
-                   [:a.h6.col-12.btn.btn-primary.bg-sms-green
-                    {:href (sms-link share-url)}
-                    [:img.align-middle.mr1 {:style {:height "18px"}
-                                            :src   (assets/path "/images/share/sms.png")}]
-                    "SMS"]]
-                  [:div.p1.col.col-6.col-12-on-dt
-                   [:a.h6.col-12.btn.btn-primary.bg-dark-gray
-                    {:href (email-link share-url store-nickname)}
-                    (svg/mail-envelope {:class "stroke-white align-middle mr1"
-                                        :style {:height "18px" :width "18px"}})
-                    "Email"]]]]
-                [:div.mt3.mb1
-                 [:input.border.border-dark-gray.rounded.pl1.py1.bg-white.teal.col-12
-                  {:type      "text"
-                   :value     share-url
-                   :data-test "share-url"
-                   :on-click  utils/select-all-text}]]
-                [:div.navy "(select and copy link to share)"]]]))))
+    (let [utm-url (-> (url/url share-url)
+                      (assoc-in [:query :utm_campaign] "sharebuttons"))]
+      (ui/modal {:close-attrs close-attrs}
+                [:.bg-white.rounded.p4.center
+                 (ui/modal-close {:close-attrs close-attrs :data-test "share-url-close"})
+                 [:.p1
+                  [:.h3.navy.medium "Share your bag"]
+                  [:.h5.dark-gray.light.my2 "Share this link so your customers know exactly what to buy"]
+                  [:.border-top.border-bottom.border-gray.py2
+                   [:div.clearfix.mxn1
+                    [:div.p1.col.col-6.col-12-on-dt
+                     [:a.h6.col-12.btn.btn-primary.bg-fb-blue
+                      {:href   (facebook-link utm-url)
+                       :target "_blank"}
+                      [:img.align-middle.mr1 {:style {:height "18px"}
+                                              :src   (assets/path "/images/share/fb.png")}]
+                      "Share"]]
+                    [:div.p1.col.col-6.col-12-on-dt
+                     [:a.h6.col-12.btn.btn-primary.bg-twitter-blue
+                      {:href   (twitter-link utm-url)
+                       :target "_blank"}
+                      [:img.align-middle.mr1 {:style {:height "18px"}
+                                              :src   (assets/path "/images/share/twitter.png")}]
+                      "Tweet"]]
+                    [:div.p1.col.col-6.col-12-on-dt.hide-on-dt
+                     [:a.h6.col-12.btn.btn-primary.bg-sms-green
+                      {:href (sms-link utm-url)
+                       :target "_blank"}
+                      [:img.align-middle.mr1 {:style {:height "18px"}
+                                              :src   (assets/path "/images/share/sms.png")}]
+                      "SMS"]]
+                    [:div.p1.col.col-6.col-12-on-dt
+                     [:a.h6.col-12.btn.btn-primary.bg-dark-gray
+                      {:href (email-link utm-url store-nickname)
+                       :target "_blank"}
+                      (svg/mail-envelope {:class "stroke-white align-middle mr1"
+                                          :style {:height "18px" :width "18px"}})
+                      "Email"]]]]
+                  [:div.mt3.mb1
+                   [:input.border.border-dark-gray.rounded.pl1.py1.bg-white.teal.col-12
+                    {:type      "text"
+                     :value     share-url
+                     :data-test "share-url"
+                     :on-click  utils/select-all-text}]]
+                  [:div.navy "(select and copy link to share)"]]])))))
 
 (defn query-share-link [data]
   {:share-url (get-in data keypaths/shared-cart-url)
