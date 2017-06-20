ff12cbcf375af674a9d0851be3a10cbf36d3d8a6
Greg Winn <gregw@mayvenn.com>
2017-06-20 13:54:58 -0700

    Configure yotpo for yaki and water wave
    [#nodeps]

--- src-cljs/storefront/platform/reviews.cljs
+++ src-cljs/storefront/platform/reviews.cljs
@@ -12,14 +12,26 @@
                     :data-name        "Brazilian Natural Straight Hair"
                     :data-description "Perfect for those who mostly wear straight hair extensions. After washing, our Brazilian Straight hair extensions have just a hint of wave that can be easily flat ironed. Lightweight and silky. Holds a curl beautifully. Each bundle weighs 3.5 ounces. Available in a natural dark brown color equivalent to color 1B. Can be easily colored or bleached (consult a licensed cosmetologist)."
                     :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6414704/6154340/thumb.jpg"}
+   :yaki-straight  {:data-product-id  642
+                    :data-name        "Brazilian Yaki Straight Hair"
+                    :data-description "Our Yaki Straight hair matches the rhythm of natural hair that's been pressed straight or freshly relaxed. This hair is lightweight, yet full of body and flowy movement. 100% human hair, machine-wefted and backed by our 30 Day Quality Guarantee, it's a great option for those who want a textured straightened look that can be flat ironed to be even more sleek when desired."
+                    :data-image-url   "http://ucarecdn.com/98e8b217-73ee-475a-8f5e-2c3aaa56af42/-/scale_crop/250x227/Yaki-Straight-Bundle.jpg"}
    :kinky-straight {:data-product-id  525
                     :data-name        "Brazilian Kinky Straight Hair"
                     :data-description "100% virgin human hair, machine-wefted and backed by our 30 Day Quality Guarantee. Our kinky straight hair mimic the blown-out texture of your natural hair."
                     :data-image-url   "https://d275k6vjijb2m1.cloudfront.net/cellar/kinky_straight/3/250x227_1.jpg"}
+   :body-wave      {:data-product-id  15
+                    :data-name        "Malaysian Body Wave Hair"
+                    :data-description "Our Malaysian Body Wave human hair has been steam processed so you can enjoy long-lasting beautiful waves. This texture has an \"S\" shape wave that is soft and bouncy. Each bundle weighs 3.5 ounces. Available in a natural dark brown color equivalent to color 1B. Excessive flat-ironing will loosen the curls over time. These hair extensions can be colored or bleached (consult a licensed cosmetologist)."
+                    :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6414706/6318976/thumb.jpg"}
    :loose-wave     {:data-product-id  2
                     :data-name        "Brazilian Loose Wave Hair"
                     :data-description "Our Brazilian Loose Wave human hair has been steam processed so you can enjoy long-lasting beautiful waves. The waves are tighter than our body wave texture but looser than our deep wave texture. Each bundle weighs 3.5 ounces. Available in a natural dark brown color equivalent to color 1B. Excessive flat-ironing will loosen the curls over time. These hair extensions can be colored (consult a licensed cosmetologist)."
                     :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6414703/6154339/thumb.jpg"}
+   :water-wave     {:data-product-id  618
+                    :data-name        "Brazilian Water Wave Hair"
+                    :data-description "No beach necessary with our luscious Water Wave hair. This texture is your tropical getaway in the form of loose, carefree curly bundles. With it's soft S-shaped and C-shaped coils, it's versatility and styling options are endless. 100% human hair, machine-wefted and backed by our 30 Day Quality Guarantee, bask in your glow by wearing it wet & sleek or voluminous & fluffy."
+                    :data-image-url   "http://ucarecdn.com/5f6c669f-8274-4bef-afa9-3c08813842f6/-/scale_crop/250x227/Water-Wave-Bundle.jpg"}
    :deep-wave      {:data-product-id  67
                     :data-name        "Brazilian Deep Wave Hair"
                     :data-description "Premier Brazilian Deep Wave
@@ -37,13 +49,9 @@ Excessive flat-ironing will loosen the texture over time.
 
 Lengths: 12\" to 28\""
                     :data-image-url "http://s3.amazonaws.com/yotpo-images-production/Product/6414702/6154338/thumb.jpg"}
-   :body-wave {:data-product-id  15
-               :data-name        "Malaysian Body Wave Hair"
-               :data-description "Our Malaysian Body Wave human hair has been steam processed so you can enjoy long-lasting beautiful waves. This texture has an \"S\" shape wave that is soft and bouncy. Each bundle weighs 3.5 ounces. Available in a natural dark brown color equivalent to color 1B. Excessive flat-ironing will loosen the curls over time. These hair extensions can be colored or bleached (consult a licensed cosmetologist)."
-               :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6414706/6318976/thumb.jpg"}
-   :curly     {:data-product-id  54
-               :data-name        "Brazilian Curly Hair"
-               :data-description "Premier Brazilian Curly
+   :curly    {:data-product-id  54
+              :data-name        "Brazilian Curly Hair"
+              :data-description "Premier Brazilian Curly
 Material:High Quality 100% Virgin Human Hair
 Color: 1B
 Bleach-able up to level:7 or 8 (#27)
@@ -54,15 +62,15 @@ When purchasing the curly hair extensions, keep in mind that each bundled is mea
 Holds a natural tight curl beautifully.
 Each bundle weighs 3.5 ounces. Available in a natural dark brown color equivalent to color 1B. These hair extensions can be colored (consult a licensed cosmetologist). Excessive flat-ironing will loosen the curls over time.
 Lengths: 12\" to 28\""
-               :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6414708/6154343/thumb.jpg"}
-   :closures  {:data-product-id  90
-               :data-name        "Brazilian Natural Straight Lace Closure"
-               :data-description "Our 4x4 lace closures create an illusion of a scalp with a natural density, and is used to leave more of your own natural hair protected. The lace closure has a natural brown scalp color and can be restyled, re-parted, cut, and colored."
-               :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6575214/6319000/thumb.jpg"}
-   :frontals  {:data-product-id  90
-               :data-name        "Brazilian Natural Straight Lace Closure"
-               :data-description "Our 4x4 lace closures create an illusion of a scalp with a natural density, and is used to leave more of your own natural hair protected. The lace closure has a natural brown scalp color and can be restyled, re-parted, cut, and colored."
-               :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6575214/6319000/thumb.jpg"}})
+              :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6414708/6154343/thumb.jpg"}
+   :closures {:data-product-id  90
+              :data-name        "Brazilian Natural Straight Lace Closure"
+              :data-description "Our 4x4 lace closures create an illusion of a scalp with a natural density, and is used to leave more of your own natural hair protected. The lace closure has a natural brown scalp color and can be restyled, re-parted, cut, and colored."
+              :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6575214/6319000/thumb.jpg"}
+   :frontals {:data-product-id  90
+              :data-name        "Brazilian Natural Straight Lace Closure"
+              :data-description "Our 4x4 lace closures create an illusion of a scalp with a natural density, and is used to leave more of your own natural hair protected. The lace closure has a natural brown scalp color and can be restyled, re-parted, cut, and colored."
+              :data-image-url   "http://s3.amazonaws.com/yotpo-images-production/Product/6575214/6319000/thumb.jpg"}})
 
 (defn product-options-for [{:keys [slug]}]
   (get product-options-by-named-search (keyword slug)))
