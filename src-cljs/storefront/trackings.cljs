2b45209ff4077f44434d488a1667694512eecbd2
Jeff Hui <jeff@mayvenn.com>
2017-07-31 17:16:50 -0700

    Add event data details for fb pixel
    [#nodeps]

--- src-cljs/storefront/trackings.cljs
+++ src-cljs/storefront/trackings.cljs
@@ -124,7 +124,25 @@
 
 (defmethod perform-track events/order-completed [_ event {:keys [total] :as order} app-state]
   (stringer/track-event "checkout-complete" (stringer-order-completed order))
-  (facebook-analytics/track-event "Purchase" {:value (str total) :currency "USD"})
+  (let [store-slug (get-in app-state keypaths/store-slug)
+        shipping (orders/shipping-item order)
+        user (get-in app-state keypaths/user)]
+    (facebook-analytics/track-event "Purchase" {:value (str total)
+                                                :currency "USD"
+                                                :content_ids (map :sku (orders/product-items order))
+                                                :content_type "product"
+                                                :num_items (count (orders/product-items order))
+                                                :store_slug store-slug
+                                                :is_stylist_store (boolean (#{"shop" "store"} store-slug))
+                                                :used_promotion_codes (map :code (:promotions order))
+                                                :shipping_method_sku (:sku shipping)
+                                                :shipping_method_name (:name shipping)
+                                                :shipping_method_price (:unit-price shipping)
+                                                :discount_total (:promotion-discount order)
+                                                :buyer_type (cond
+                                                              (:store-slug user) "stylist"
+                                                              (:id user) "registered_user"
+                                                              :else "guest_user")}))
   (convert/track-conversion "place-order")
   (convert/track-revenue (convert-revenue order))
   (google-analytics/track-event "orders" "placed_total" nil (int total))
