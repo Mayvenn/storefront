7d85d80249c31ccc48654e08c70452bf5ff2ac9f
Corey Reece <corey@mayvenn.com>
2017-08-03 09:23:55 -0700

    Fix .dir-locals to auto-load
    [#nodeps]

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
-(setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
-(setq cider-refresh-before-fn "user/stop" cider-refresh-after-fn "user/go")
-
-(setq cider-refresh-show-log-buffer t)
-
+((nil . ((cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
+         (cider-refresh-before-fn . "user/stop")
+         (cider-refresh-after-fn  . "user/go")
+         (cider-refresh-show-log-buffer . t))))
