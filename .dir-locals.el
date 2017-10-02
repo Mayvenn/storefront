6a174002a8dec9866bda783c868c527c53f0353f
Jeff Hui <jeff@mayvenn.com>
2017-10-02 11:53:35 -0700

    Don't show log buffer on refresh [#nodeps]

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,3 @@
 ((nil . ((cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
          (cider-refresh-before-fn . "user/stop")
-         (cider-refresh-after-fn  . "user/go")
-         (cider-refresh-show-log-buffer . t))))
+         (cider-refresh-after-fn  . "user/go"))))
