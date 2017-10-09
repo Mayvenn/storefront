e5acc564f7c328356501f31b44cfe90971fe836e
Jonathan Harford <jonathanh@mayvenn.com>
2017-10-09 13:29:09 -0700

    Bump stringer version [#nodeps]

--- src-cljs/storefront/hooks/stringer.cljs
+++ src-cljs/storefront/hooks/stringer.cljs
@@ -2,7 +2,7 @@
   (:require [storefront.browser.tags :refer [insert-tag-with-text remove-tags-by-class remove-tag-by-src]]
             [storefront.config :as config]))
 
-(def stringer-src "//d6w7wdcyyr51t.cloudfront.net/cdn/stringer/stringer-a1931bc.js")
+(def stringer-src "//d6w7wdcyyr51t.cloudfront.net/cdn/stringer/stringer-4e19a90.js")
 
 (defn insert-tracking []
   (insert-tag-with-text
