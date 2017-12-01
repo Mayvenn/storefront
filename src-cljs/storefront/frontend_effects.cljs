d8caf30d4f49c63aba2c9df85051d32007a77ab9
Jeff Hui <jeff@mayvenn.com>
2017-12-01 10:54:38 -0800

    Redirect from /shop/bundle-deals to home page

--- src-cljs/storefront/frontend_effects.cljs
+++ src-cljs/storefront/frontend_effects.cljs
@@ -287,7 +287,7 @@
   (wistia/load))
 
 (defmethod perform-effects events/navigate-shop-bundle-deals [_ event _ _ app-state]
-  (pixlee/fetch-bundle-deals))
+  (redirect events/navigate-home))
 
 (defmethod perform-effects events/navigate-shop-by-look [_ event {:keys [look-id]} _ app-state]
   (when-not look-id ;; we are on navigate-shop-by-look, not navigate-shop-by-look-details
