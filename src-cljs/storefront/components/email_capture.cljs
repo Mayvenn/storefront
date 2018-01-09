afcfcf2fcf63ec224845cb43eeb29fed088dbc82
Justin Watt <justinw@mayvenn.com>
2018-01-09 11:34:51 -0800

    Adjust email capture modal styling
    - Transparency applies to background only
    - Font sizing
    - Vertical size
    - Maintain mobile width on desktop

--- src-cljs/storefront/components/email_capture.cljs
+++ src-cljs/storefront/components/email_capture.cljs
@@ -11,17 +11,18 @@
     (component/create
      (html
       (ui/modal {:close-attrs close-attrs
+                 :col-class   "col-11 col-5-on-tb col-4-on-dt"
                  :bg-class    "bg-darken-4"}
                 [:div.flex.flex-column.bg-cover.bg-top.bg-email-capture
                  [:div.flex.justify-end
                   (ui/big-x {:data-test "dismiss-email-capture" :attrs close-attrs})]
-                 [:div {:style {:height "230"}}]
-                 [:div.p4.m3.bg-white {:style {:opacity 0.8}}
+                 [:div {:style {:height "110"}}]
+                 [:div.p4.m3.bg-lighten-4
                   [:form.col-12.flex.flex-column.items-center
                    {:on-submit (utils/send-event-callback events/control-email-captured-submit)}
-                   [:div.h0.bold.teal.mb0.center "You're Flawless"]
-                   [:p.mb2 "Make sure your hair is too"]
-                   [:p.mb2.center "Sign up now for exclusive discounts, stylist-approved hair tips, and first access to new products."]
+                   [:h1.bold.teal.mb0.center {:style {:font-size "36px"}} "You're Flawless"]
+                   [:p.h5.mb1 "Make sure your hair is too"]
+                   [:p.h5.mb2.line-height-2.center "Sign up now for exclusive discounts, stylist-approved hair tips, and first access to new products."]
                    (ui/text-field {:errors   (get field-errors ["email"])
                                    :keypath  keypaths/captured-email
                                    :focused  focused
@@ -31,7 +32,7 @@
                                    :type     "email"
                                    :value    email
                                    :class    "center"})
-                   [:div.col-12.col-6-on-tb-dt.mx-auto.my2 (ui/submit-button "Sign Up Now")]]]])))))
+                   [:div.col-12.col-6-on-tb-dt.mx-auto (ui/submit-button "Sign Up Now")]]]])))))
 
 (defn query [data]
   {:email        (get-in data keypaths/captured-email)
