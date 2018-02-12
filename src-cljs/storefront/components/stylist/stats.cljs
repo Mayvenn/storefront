4acdbbb69cb66d859873b22ff3d51274f6d3774b
Heather Farley <heather@mayvenn.com>
2018-02-12 11:56:55 -0800

    Refactor Stylist stats carousel
    Co-authored-by: Justin Watt <justinwatt@gmail.com>

--- src-cljs/storefront/components/stylist/stats.cljs
+++ src-cljs/storefront/components/stylist/stats.cljs
@@ -9,8 +9,6 @@
             [storefront.platform.component-utils :as util]
             [storefront.platform.carousel :as carousel]))
 
-(def ordered-stats [:next-payout :previous-payout :lifetime-payouts])
-
 (def payday 3) ;; 3 -> Wednesday in JS
 
 (defn days-till-payout []
@@ -29,75 +27,75 @@
 (def re-center-money {:style {:margin-left "-5px"}})
 
 (defn previous-payout-slide [{:keys [amount date]}]
-  [:.my4
-   {:key "previous-payout" :class stat-card}
-   [:.p1 "LAST PAYMENT"]
-   (if (> amount 0)
-     [:div
-      [:.py2.h0 re-center-money (ui/big-money amount)]
-      [:div "On " (f/long-date date)]]
-     [:div
-      [:.py2.h0 svg/large-payout]
-      [:div "Your last payment will show here."]])])
+  [:div.my4.clearfix
+   [:.my4
+    {:key "previous-payout" :class stat-card}
+    [:.p1 "LAST PAYMENT"]
+    (if (> amount 0)
+      [:div
+       [:.py2.h0 re-center-money (ui/big-money amount)]
+       [:div "On " (f/long-date date)]]
+      [:div
+       [:.py2.h0 svg/large-payout]
+       [:div "Your last payment will show here."]])]])
 
-;; TODO Conditionalize on payment method
 (defn cash-out-now-slide [{:keys [amount]}]
-  [:.my4
-   {:key "available-earnings" :class stat-card}
-   [:.p1 "AVAILABLE EARNINGS"]
-   (if (> amount 0)
-     [:div
-      [:.py2.h0 re-center-money (ui/big-money amount)]
-      [:div.col-5.mt1.mb2.mx-auto
-       (ui/light-ghost-button (merge
-                               {:class "rounded-1 p1 light"}
-                               (util/route-to events/navigate-stylist-dashboard-cash-out-now))
-                              [:span.ml1 "Cash Out Now"]
-                              [:span.ml2
-                               (svg/dropdown-arrow {:class  "stroke-white"
-                                                    :width  "12px"
-                                                    :height "10px"
-                                                    :style  {:transform "rotate(-90deg)"}})])]]
-     [:div
-      [:.py2.h0 svg/large-dollar]
-      [:div "See your available earnings here."]])])
+  [:div.my4.clearfix
+   [:.my4
+    {:key "available-earnings" :class stat-card}
+    [:.p1 "AVAILABLE EARNINGS"]
+    (if (> amount 0)
+      [:div
+       [:.py2.h0 re-center-money (ui/big-money amount)]
+       [:div.col-5.mt1.mb2.mx-auto
+        (ui/light-ghost-button (merge
+                                {:class "rounded-1 p1 light"}
+                                (util/route-to events/navigate-stylist-dashboard-cash-out-now))
+                               [:span.ml1 "Cash Out Now"]
+                               [:span.ml2
+                                (svg/dropdown-arrow {:class  "stroke-white"
+                                                     :width  "12px"
+                                                     :height "10px"
+                                                     :style  {:transform "rotate(-90deg)"}})])]]
+      [:div
+       [:.py2.h0 svg/large-dollar]
+       [:div "See your available earnings here."]])]])
 
 (defn next-payout-slide [{:keys [amount]}]
-  [:.my4
-   {:key "next-payment" :class stat-card}
-   [:.p1 "NEXT PAYMENT"]
-   (if (> amount 0)
-     [:div
-      [:.py2.h0 re-center-money (ui/big-money amount)]
-      [:div "Payment " (in-x-days)]]
-     [:div
-      [:.py2.h0 svg/large-dollar]
-      [:div "See your next payment amount here."]])])
+  [:div.my4.clearfix
+   [:.my4
+    {:key "next-payment" :class stat-card}
+    [:.p1 "NEXT PAYMENT"]
+    (if (> amount 0)
+      [:div
+       [:.py2.h0 re-center-money (ui/big-money amount)]
+       [:div "Payment " (in-x-days)]]
+      [:div
+       [:.py2.h0 svg/large-dollar]
+       [:div "See your next payment amount here."]])]])
 
 (defn lifetime-payouts-slide [{:keys [amount]}]
-  [:.my4
-   {:class stat-card :key "render-stat"}
-   [:.p1 "LIFETIME COMMISSIONS"]
-   (if (> amount 0)
-     [:div
-      [:.py2.h0 re-center-money [:div.line-height-1 (mf/as-money-without-cents amount)]]
-      [:div "Sales since you joined Mayvenn"]]
-     [:div
-      [:.py2.h0 svg/large-percent]
-      [:div "All sales since you joined Mayvenn."]])])
+  [:div.my4.clearfix
+   [:.my4
+    {:class stat-card :key "render-stat"}
+    [:.p1 "LIFETIME COMMISSIONS"]
+    (if (> amount 0)
+      [:div
+       [:.py2.h0 re-center-money [:div.line-height-1 (mf/as-money-without-cents amount)]]
+       [:div "Sales since you joined Mayvenn"]]
+      [:div
+       [:.py2.h0 svg/large-percent]
+       [:div "All sales since you joined Mayvenn."]])]])
 
 (defn stylist-dashboard-stats-component [{:keys [stats show-cash-out-now-ui?]} owner]
   (om/component
    (html
-    (let [items [[:div.my4.clearfix
-                  (previous-payout-slide (:previous-payouts stats))]
+    (let [items [(previous-payout-slide (:previous-payouts stats))
                  (if show-cash-out-now-ui?
-                   [:div.my4.clearfix
-                    (cash-out-now-slide (:next-payout stats))]
-                   [:div.my4.clearfix
-                    (next-payout-slide (:next-payout stats))])
-                 [:div.my4.clearfix
-                  (lifetime-payouts-slide (:lifetime-payouts stats))]] ]
+                   (cash-out-now-slide (:next-payout stats))
+                   (next-payout-slide (:next-payout stats)))
+                 (lifetime-payouts-slide (:lifetime-payouts stats))]
+          initial-slide-index (if show-cash-out-now-ui? 1 0)]
       [:div.bg-teal.white.center
        [:div.bg-darken-bottom-1
         (om/build carousel/component
@@ -105,5 +103,5 @@
                    :settings {:arrows true
                               :dots   true
                               :swipe  true
-                              :initialSlide (if show-cash-out-now-ui? 1 0)}}
+                              :initialSlide initial-slide-index}}
                   {:react-key "stat-swiper"})]]))))
