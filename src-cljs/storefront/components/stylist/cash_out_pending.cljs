05fa23d46b921e72d74941a3977ea1756fc10fe2
Greg Winn <gregw@mayvenn.com>
2018-02-20 17:20:12 -0800

    Fix instapay failure case

--- src-cljs/storefront/components/stylist/cash_out_pending.cljs
+++ src-cljs/storefront/components/stylist/cash_out_pending.cljs
@@ -71,10 +71,10 @@
   [_ _ args _ app-state]
   (let [payout-method (get-in app-state (conj keypaths/stylist-payout-stats-next-payout :payout-method))
         message       (case (:name payout-method)
-                        "Paypal"   (str "Your cash out failed. "
-                                        "Please confirm your PayPal account or switch to Mayvenn Debit.")
-                        "InstaPay" (str "Your cash out failed. "
-                                        "Please try another card with Mayvenn InstaPay "
-                                        "or switch to PayPal and try again."))]
+                        "Paypal"           (str "Your cash out failed. "
+                                                "Please confirm your PayPal account or switch to Mayvenn Debit.")
+                        "Mayvenn InstaPay" (str "Your cash out failed. "
+                                                "Please try another card with Mayvenn InstaPay "
+                                                "or switch to PayPal and try again."))]
     (effects/redirect events/navigate-stylist-account-commission)
     (messages/handle-later events/flash-show-failure {:message message})))
