47f381ae91be9caf69e65006487dfe95ff6d6469
Heather Farley <heather@mayvenn.com>
2018-02-27 15:30:38 -0800

    Track cash out button pressed
    [#154412528]
    
    Co-authored-by: Justin Watt <justinwatt@gmail.com>

--- src-cljs/storefront/trackings.cljs
+++ src-cljs/storefront/trackings.cljs
@@ -403,11 +403,13 @@
 (defmethod perform-track events/control-stylist-dashboard-cash-out-submit
   [_ _ _ app-state]
   (let [stylist-id                     (get-in app-state keypaths/store-stylist-id)
+        store-slug                     (get-in app-state keypaths/store-slug)
         {:keys [amount payout-method]} (get-in app-state keypaths/stylist-payout-stats-next-payout)]
-    (stringer/track-event "dashboard_cash_out_button_pressed"
-                          {:stylist_id         stylist-id
-                           :amount             (js/parseFloat amount)
-                           :payout_method_name (:name payout-method)})))
+    (stringer/track-event "cash_out_button_pressed"
+                          {:stylist_id stylist-id
+                           :store_slug store-slug
+                           :amount     (js/parseFloat amount)
+                           :method     (:slug payout-method)})))
 
 (defmethod perform-track events/navigate-stylist-dashboard-cash-out-success
   [_ _ _ app-state]
