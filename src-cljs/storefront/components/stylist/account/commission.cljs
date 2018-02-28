f647be3ffce589ad9b39c8bcd55b3695cbe050c2
Justin Watt <justinw@mayvenn.com>
2018-02-27 17:15:40 -0800

    Move 'view your earnings' link under Update Commission Info
    [#155574466]
    
    Co-authored-by: Heather Farley <heather@mayvenn.com>

--- src-cljs/storefront/components/stylist/account/commission.cljs
+++ src-cljs/storefront/components/stylist/account/commission.cljs
@@ -152,10 +152,10 @@
 
          "mayvenn_debit" [:p.ml1.mb3.h6 "A prepaid Visa debit card will be mailed to the address entered here"]
          "check"         [:p.ml1.mb3.h6 "Checks will mail to the address entered here"]
-         [:p.ml1.mb3.h6 "Checks will mail to the address entered here"])]]
+         [:p.ml1.mb3.h6 "Checks will mail to the address entered here"])]
 
-     [:div.col.col-12.col-6-on-tb-dt.pt3.pb4.underline
-      [:a.teal (utils/route-to events/navigate-stylist-dashboard-earnings) "View your earnings"]]
+      [:div.col.col-12.pt3.pb4.underline.center
+       [:a.teal (utils/route-to events/navigate-stylist-dashboard-earnings) "View your earnings"]]]
 
      [:div.col.col-12.col-6-on-tb-dt
       [:div.mx-auto.col-12.col-10-on-tb-dt
