b3b0726507f4f3b9a897e0142f51404e8edc5239
Ellie Winn <ellie@Ellies-MacBook-Pro.local>
2018-03-06 10:59:00 -0800

    Avoid using deprecated var "cider-cljs-lein-repl" in dir-locals.el
    Co-authored-by: Jonathan Harford <jonathanh@mayvenn.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
+((nil . ((cider-default-cljs-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
          (cider-refresh-before-fn . "user/stop")
          (cider-refresh-after-fn  . "user/go"))))
