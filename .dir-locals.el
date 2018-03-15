8dc18312fd3865c707e93c024ca8dfedaad1cf81
Heather Farley <heather@mayvenn.com>
2018-03-15 14:18:34 -0700

    Do both deprecated and new version of cider
    [#155868093]
    
    Co-authored-by: Ellie Winn <ellie@mayvenn.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((cider-default-cljs-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
+         (cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
          (cider-refresh-before-fn . "user/stop")
          (cider-refresh-after-fn  . "user/go"))))
