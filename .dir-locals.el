389d76d4b2e41907f08d7becb358441d0a7c5e5e
Corey Reece <corey@mayvenn.com>
2018-05-09 10:38:52 -0700

    Let's just wait for this thing to shake out

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,2 @@
-((nil . ((cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
-         (cider-refresh-before-fn . "user/stop")
+((nil . ((cider-refresh-before-fn . "user/stop")
          (cider-refresh-after-fn  . "user/go"))))
