261671e71935a52c7157f91beca0eb150f949298
Jonathan Harford <jonathanh@mayvenn.com>
2018-09-10 10:30:25 -0700

    Revert carelessly unwrapped spinner
    Co-authored-by: Heather Farley <heather@mayvenn.com>

--- src-cljs/storefront/components/stylist/pagination.cljs
+++ src-cljs/storefront/components/stylist/pagination.cljs
@@ -6,10 +6,10 @@
   (> (or pages 0) (or page 0)))
 
 (defn fetch-more [event fetching? page pages]
-  (if fetching?
-    [:.h2 ui/spinner]
-    (when (more-pages? page pages)
-      [:.col-5.mx-auto.my3
+  [:.col-5.mx-auto.my3
+   (if fetching?
+     [:.h2 ui/spinner]
+     (when (more-pages? page pages)
        [:.btn.btn-outline.navy.col-12
         {:on-click (utils/send-event-callback event)}
-        "Load More"]])))
+        "Load More"]))])
