acaa71a835a77e20e7a69907c02447c67eb416b0
Ag Ibragimov <agzam.ibragimov@gmail.com>
2018-09-11 10:01:48 -0700

    sets cider-default-cljs-repl as figwheel in dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((nil . ((cider-ns-refresh-before-fn . "user/stop")
-         (cider-ns-refresh-after-fn  . "user/go"))))
+         (cider-ns-refresh-after-fn  . "user/go")
+         (eval . (with-eval-after-load 'clojure-mode
+                   (setq cider-default-cljs-repl 'figwheel))))))
