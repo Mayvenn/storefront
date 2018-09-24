431de765320efdfe41b93839a37cb229e086156c
Ellie Winn <ellie@mayvenn.com>
2018-09-24 10:36:48 -0700

    For privacy consistency

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ((nil . ((cider-ns-refresh-before-fn . "user/stop")
          (cider-ns-refresh-after-fn  . "user/go")
          (eval . (with-eval-after-load 'clojure-mode
-                   (setq cider-default-cljs-repl 'figwheel))))))
+                   (setq
+                    cider-default-cljs-repl 'figwheel
+                    clojure-use-metadata-for-privacy t))))))
