40251187efbd22c6ec91e89c3afcd6cbacd4c9a7
Ellie Winn <ellie@mayvenn.com>
2018-09-24 17:28:29 -0700

    Work around bug in comb eval
    Co-authored-by: Jeff Hui <jeff@mayvenn.com>

--- src/storefront/handler.clj
+++ src/storefront/handler.clj
@@ -54,8 +54,18 @@
     (catch NumberFormatException _
       nil)))
 
+(defn render-template
+  "Evaluate a template using the supplied bindings. The template source may
+  be a string, or an I/O source such as a File, Reader or InputStream."
+  ([source]
+   (render-template source {}))
+  ([source bindings]
+   (let [keys (mapv (comp symbol name) (keys bindings))
+         func (template/compile-fn [{:keys keys}] source)]
+     (func bindings))))
+
 (defn render-static-page [template]
-  (template/eval template {:url assets/path}))
+  (render-template template {:url assets/path}))
 
 (defn static-page [[navigate-kw content-kw & static-content-id]]
   (when (= [navigate-kw content-kw] events/navigate-content)
