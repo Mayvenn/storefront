f48fab60c6476bd1a121f63f0b45668d04fe45b5
Jeff Hui <jeff@mayvenn.com>
2018-09-25 13:57:51 -0700

    Don't float right for cash out now notice
    - since there isn't any element to float right to, it becomes the only element
      that's not aligned

--- src-cljs/storefront/components/stylist/v2_dashboard_stats.cljs
+++ src-cljs/storefront/components/stylist/v2_dashboard_stats.cljs
@@ -64,7 +64,7 @@
           [:div.flex.items-center.justify-center.regular.h5
            (ui/ucare-img {:width "28" :class "mr2 flex items-center"} "3d651ddf-b37d-441b-a162-b83728f2a2eb")
            "Cash Out"])
-         [:div.h7.right
+         [:div.h7
           "Cash out now with " [:a.teal (utils/fake-href events/navigate-stylist-account-commission) "Mayvenn InstaPay"]])]]
    [:div
       (when-not expanded? {:class "hide"})
