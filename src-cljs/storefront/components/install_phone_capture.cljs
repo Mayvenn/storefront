863df9b329b2f947941f2d2c59ce22c1db8007d0
Ag Ibragimov <agzam.ibragimov@gmail.com>
2018-10-25 10:27:22 -0700

    Send stringer 'phone-capture' event
    [#161455796]
    
    Co-authored-by: Ag <agzam.ibragimov@gmail.com>

--- src-cljs/storefront/components/install_phone_capture.cljs
+++ src-cljs/storefront/components/install_phone_capture.cljs
@@ -3,6 +3,7 @@
             [i18n.phonenumbers.PhoneNumberUtil :as phone]
             [om.core :as om]
             [sablono.core :refer [html]]
+            [storefront.hooks.stringer :as stringer]
             [storefront.components.svg :as svg]
             [storefront.components.ui :as ui]
             [storefront.effects :as effects]
@@ -23,48 +24,49 @@
 
 (defn component [{:keys [captured-install-phone field-errors]} owner {:keys [close-attrs]}]
   (om/component
-   (html
-    (ui/modal {:col-class   "col-12 col-6-on-tb col-6-on-dt my8-on-tb-dt"
-               :bg-class    "bg-darken-1"
-               :close-attrs close-attrs}
-              [:div.fixed.z4.bottom-0.left-0.right-0
-               [:div.border-top.border-dark-gray.border-width-2
-                [:div.border-top.border-dark-gray.border-width-2
-                 [:a.h2.bold.white.bg-purple.px3.py2.flex.items-center
-                  (utils/fake-href events/control-popup-hide)
-                  [:div.flex-auto.center "Get $25 – Share Your Opinion"]
-                  [:div.stroke-white.ml1
-                   (svg/dropdown-arrow {:height "16" :width "16"})]]]
-                [:div.bg-light-gray.px3.py4
-                 [:div.h6.mb3.mx1.line-height-3
-                  "Enter your phone number to apply. Selected participants will be "
-                  "interviewed by phone and sent $25."]
-                 (let [valid-phone? (boolean (->e164-phone captured-install-phone))]
-                   (ui/input-group
-                    {:keypath       keypaths/captured-install-phone
-                     :wrapper-class "col-8 flex bg-white pl3 items-center circled-item"
-                     :class         ""
-                     :data-test     ""
-                     :name          "phone"
-                     :focused       true
-                     :placeholder   "(xxx) xxx - xxxx"
-                     :type          "tel"
-                     :value         captured-install-phone
-                     :errors        (when (and (seq captured-install-phone) (not valid-phone?))
-                                      [{:long-message "Please enter a valid US phone number"}])}
-                    {:ui-element ui/teal-button
-                     :content    "Get Survey"
-                     :args       {:on-click       (utils/send-event-callback events/control-install-phone-captured-submit)
-                                  :class          "flex justify-center medium items-center circled-item"
-                                  :size-class     "col-4"
-                                  :height-class   "py2"
-                                  :data-test      ""
-                                  :disabled-class "disabled bg-teal"
-                                  :disabled?      (not valid-phone?)
-                                  :spinning?      false #_applying?}}))
-                 [:a.h6.dark-gray.mx2
-                  {:href "#"}
-                  "No thanks."]]]]))))
+    (html
+      (ui/modal {:col-class   "col-12 col-6-on-tb col-6-on-dt my8-on-tb-dt"
+                 :bg-class    "bg-darken-1"
+                 :close-attrs close-attrs}
+                [:div.fixed.z4.bottom-0.left-0.right-0
+                 [:div.border-top.border-dark-gray.border-width-2
+                  [:div.border-top.border-dark-gray.border-width-2
+                   [:a.h2.bold.white.bg-purple.px3.py2.flex.items-center
+                    (utils/fake-href events/control-popup-hide)
+                    [:div.flex-auto.center "Get $25 – Share Your Opinion"]
+                    [:div.stroke-white.ml1
+                     (svg/dropdown-arrow {:height "16" :width "16"})]]]
+                  [:div.bg-light-gray.px3.py4
+                   [:div.h6.mb3.mx1.line-height-3
+                    "Enter your phone number to apply. Selected participants will be "
+                    "interviewed by phone and sent $25."]
+                   (let [valid-phone? (boolean (->e164-phone captured-install-phone))]
+                     (ui/input-group
+                       {:keypath       keypaths/captured-install-phone
+                        :wrapper-class "col-8 flex bg-white pl3 items-center circled-item"
+                        :class         ""
+                        :data-test     ""
+                        :name          "phone"
+                        :focused       true
+                        :placeholder   "(xxx) xxx - xxxx"
+                        :type          "tel"
+                        :value         captured-install-phone
+                        :errors        (when (and (seq captured-install-phone) (not valid-phone?))
+                                         [{:long-message "Please enter a valid US phone number"}])}
+                       {:ui-element ui/teal-button
+                        :content    "Get Survey"
+                        :args       {:on-click       (utils/send-event-callback events/control-install-phone-captured-submit {:phone-number captured-install-phone})
+                                     :class          "flex justify-center medium items-center circled-item"
+                                     :size-class     "col-4"
+                                     :height-class   "py2"
+                                     :data-test      ""
+                                     :disabled-class "disabled bg-teal"
+                                     :disabled?      (not valid-phone?)
+                                     :spinning?      false #_applying?
+                                     }}))
+                   [:a.h6.dark-gray.mx2
+                    {:href "#"}
+                    "No thanks."]]]]))))
 
 (defn query
   [data]
@@ -72,10 +74,5 @@
    :captured-install-phone (get-in data keypaths/captured-install-phone)})
 
 (defmethod effects/perform-effects events/control-install-phone-captured-submit
-  [_ _ _ _ app-state]
-  (prn "submitted"))
-
-(defmethod transitions/transition-state events/control-install-phone-captured-submit
-  [_ _ _ app-state]
-  (prn "submitted")
-  app-state)
+  [_ _ {:keys [phone-number]} _ app-state]
+  (stringer/track-event "phone_captured" {:phone-number phone-number}))
