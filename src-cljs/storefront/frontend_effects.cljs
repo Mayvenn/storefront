0d0d2d929e0c1b77434838ebf3b9d175ff9c1808
Ag Ibragimov <agzam.ibragimov@gmail.com>
2018-11-19 17:15:19 -0800

    Enable body scroll when following product-alternative link
    [Fixes #162074602]
    
    Co-authored-by: Justin Watt <justinwatt@gmail.com>

--- src-cljs/storefront/frontend_effects.cljs
+++ src-cljs/storefront/frontend_effects.cljs
@@ -1050,3 +1050,6 @@
                                   {:user-id    (get-in app-state keypaths/user-id)
                                    :user-token (get-in app-state keypaths/user-token)
                                    :stylist-id (get-in app-state keypaths/store-stylist-id)}))
+
+(defmethod perform-effects events/navigate-category [_ event args app-state-before app-state]
+  (scroll/enable-body-scrolling))
