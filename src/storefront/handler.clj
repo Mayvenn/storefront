044c89794f959d303f6ee6a7ccea7ff0dcfd1698
Justin Watt <justinw@mayvenn.com>
2019-01-17 16:25:11 -0800

    Fetch stylist on all freeinstall page server-side loads
    [#163105882]

--- src/storefront/handler.clj
+++ src/storefront/handler.clj
@@ -674,9 +674,8 @@
         (not on-freeinstall-subdomain?)       nil ;; defer handling elsewhere for non-freeinstall domains
         is-www-prefixed?                      (util.response/redirect (store-url "freeinstall" environment req))
         (or on-install-page?
-            is-adventure?)                    (h req)
-
-        is-cart?                              ((-> h
+            is-adventure?
+            is-cart)                          ((-> h
                                                    (wrap-fetch-store (:storeback-config ctx))
                                                    (wrap-fetch-order (:storeback-config ctx))
                                                    (wrap-cookies (storefront-site-defaults (:environment ctx))))
