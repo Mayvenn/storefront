1119c783c27d4a802137492e3f086c19dd9250d0
Corey Reece <corey@mayvenn.com>
2019-01-25 12:02:25 -0800

    Fill the space between the header and the footer
    Co-authored-by: Heather Farley <heather@mayvenn.com>

--- src-cljs/storefront/components/checkout_complete.cljs
+++ src-cljs/storefront/components/checkout_complete.cljs
@@ -50,37 +50,38 @@
 (defn adventure-component
   [_ _ _]
   (component/create
-   (ui/narrow-container
-    [:div.bg-lavender.white.center
-     [:div.col-10.mx-auto.py4
-      [:div
-       [:h2.h2.bold.py2
-        "Thank you for your order!"]
-       [:div.h2.line-height-2
-        "We've received your order and will be processing it right away."
-        "Once your order ships we will send you an email confirmation."]]
-
-      [:div.py2.mx-auto.white.border-bottom.border-width-1]
-
-      [:div
-       [:h1.py4.h1.bold
-        "Let's match you with a Certified Mayvenn Stylist!"]
-       [:div.h2.line-height-2
-        "A Mayvenn representative will contact you soon to help select a "
-        "Certified Mayvenn Stylist with the following criteria:"]
+   [:div.bg-lavender.white {:style {:min-height "95vh"}}
+    (ui/narrow-container
+     [:div.center
+      [:div.col-10.mx-auto.py4
        [:div
-        [:ul.col-10.h3.purple-checkmark.py4.left-align.mx6
-         (mapv (fn [%] [:li.pl1.mb1 %])
-               ["Licensed Salon Stylist"
-                "Mayvenn Certified"
-                "In your area"])]]]
+        [:h2.h2.bold.py2
+         "Thank you for your order!"]
+        [:div.h2.line-height-2
+         "We've received your order and will be processing it right away."
+         "Once your order ships we will send you an email confirmation."]]
 
-      [:div.pt6.pb2
-       [:h1.bold "In the meantime…"]
-       [:h2.py2 "Get inspired for your appointment"]
-       [:div.py2
-        (ui/teal-button {:href "https://www.instagram.com/explore/tags/mayvennfreeinstall/"}
-                        "View #MayvennFreeInstall")]]]])))
+       [:div.py2.mx-auto.white.border-bottom.border-width-1]
+
+       [:div
+        [:h1.py4.h1.bold
+         "Let's match you with a Certified Mayvenn Stylist!"]
+        [:div.h2.line-height-2
+         "A Mayvenn representative will contact you soon to help select a "
+         "Certified Mayvenn Stylist with the following criteria:"]
+        [:div
+         [:ul.col-10.h3.purple-checkmark.py4.left-align.mx6
+          (mapv (fn [%] [:li.pl1.mb1 %])
+                ["Licensed Salon Stylist"
+                 "Mayvenn Certified"
+                 "In your area"])]]]
+
+       [:div.pt6.pb2
+        [:h1.bold "In the meantime…"]
+        [:h2.py2 "Get inspired for your appointment"]
+        [:div.py2
+         (ui/teal-button {:href "https://www.instagram.com/explore/tags/mayvennfreeinstall/"}
+                         "View #MayvennFreeInstall")]]]])]))
 
 (defn query
   [data]
