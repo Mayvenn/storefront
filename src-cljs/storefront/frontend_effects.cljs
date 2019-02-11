4dc4e48d1c323b5008638d83683d3166ef276258
Jonathan Harford <jonathanh@mayvenn.com>
2019-02-11 11:32:41 -0800

    Direct load of adventure cart redirects to adventure home
    [#163877252]
    
    Co-authored-by: Jeff Hui <jeff@mayvenn.com>

--- src-cljs/storefront/frontend_effects.cljs
+++ src-cljs/storefront/frontend_effects.cljs
@@ -412,6 +412,9 @@
                                       "please continue with your order below.")})
 
 (defmethod perform-effects events/navigate-cart [_ event args _ app-state]
+  (when (and (= "freeinstall" (get-in app-state keypaths/store-slug))
+             (empty? (get-in app-state adv-keypaths/adventure-choices)))
+    (history/enqueue-navigate events/navigate-adventure-home nil))
   (api/get-shipping-methods)
   (api/get-states (get-in app-state keypaths/api-cache))
   (stripe/insert)
