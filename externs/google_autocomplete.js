d9487f9aa99a2309a019e7967ae29b3aa3d0525c
Jeff Hui <jeff@mayvenn.com>
2019-02-25 16:32:45 -0800

    Add google autocomplete externs

---
+++ externs/google_autocomplete.js
@@ -0,0 +1,8 @@
+var google = {};
+google.maps = {};
+google.maps.places = {};
+google.maps.places.Autocomplete = function(){};
+
+google.maps.places.Autocomplete.prototype.setFields = function(s){};
+google.maps.places.Autocomplete.prototype.addListener = function(event, cb){};
+google.maps.places.Autocomplete.prototype.getPlace = function(){};
