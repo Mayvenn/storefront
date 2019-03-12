d38682e97e86691a12bd4381ed6834fd95c4cdac
Jonathan Harford <jonathanh@mayvenn.com>
2019-03-12 10:59:03 -0700

    Use dev profile when starting repl with cider
    Co-authored-by: Justin Watt <justinw@mayvenn.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ((nil . ((cider-ns-refresh-before-fn . "user/stop")
          (cider-ns-refresh-after-fn  . "user/go")
+         (cider-lein-parameters . "with-profile +dev repl :headless :host localhost")
          (eval . (with-eval-after-load 'clojure-mode
                    (setq
                     cider-default-cljs-repl 'figwheel
