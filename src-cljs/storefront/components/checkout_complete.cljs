3abacaad6da618dff3f592142b1fd1ed4528a50b
Heather Farley <heather@mayvenn.com>
2019-03-27 16:14:21 -0700

    Simplify need-match component
    [#164808140]
    
    Co-authored-by: Jonathan Harford <jonathanh@mayvenn.com>

--- src-cljs/storefront/components/checkout_complete.cljs
+++ src-cljs/storefront/components/checkout_complete.cljs
@@ -103,34 +103,27 @@
    [:div.bg-white.px1.my4.mxn2.rounded.py3
     (stylist-card servicing-stylist)]])
 
-(defn need-match-via-web-component
+(defn need-match
   [matched-stylists]
-  [:div {:data-test "to-be-matched"}
-   [:div.py4.h3.bold
-    "Let's match you with a Certified Mayvenn Stylist!"]
-   [:div.h5.line-height-3
-    "If you don’t love the install, we’ll pay for you to get it taken down and redone. It’s a win-win!"]
-   [:div
-    [:ul.col-10.h6.list-img-purple-checkmark.py4.left-align.mx6
-     (mapv (fn [txt] [:li.pl1.mb1 txt])
-           ["Licensed Salon Stylist"
-            "Mayvenn Certified"
-            "In your area"])]]
-   (ui/teal-button {:data-test "pick-a-stylist"} "Pick a stylist")])
-
-(def need-match-via-phone-component
-  [:div {:data-test "to-be-matched"}
-   [:div.py4.h3.bold
-    "Let's match you with a Certified Mayvenn Stylist!"]
-   [:div.h5.line-height-3
-    (copy "A Mayvenn representative will contact you soon to help select a"
-          "Certified Mayvenn Stylist with the following criteria:")]
-   [:div
-    [:ul.col-10.h6.list-img-purple-checkmark.py4.left-align.mx6
-     (mapv (fn [txt] [:li.pl1.mb1 txt])
-           ["Licensed Salon Stylist"
-            "Mayvenn Certified"
-            "In your area"])]]])
+  (let [match-via-web? (seq matched-stylists)]
+    [:div
+     [:div.py4.h3.bold
+      "Let's match you with a Certified Mayvenn Stylist!"]
+     [:div.h5.line-height-3
+      {:data-test (str "to-be-matched" (if match-via-web? "-via-web" "-via-phone"))}
+      (if match-via-web?
+        "If you don’t love the install, we’ll pay for you to get it taken down and redone. It’s a win-win!"
+        (copy "A Mayvenn representative will contact you soon to help select a"
+              "Certified Mayvenn Stylist with the following criteria:"))]
+     [:div
+      [:ul.col-10.h6.list-img-purple-checkmark.py4.left-align.mx6
+       (mapv (fn [txt] [:li.pl1.mb1 txt])
+             ["Licensed Salon Stylist"
+              "Mayvenn Certified"
+              "In your area"])]]
+     (when match-via-web?
+       (ui/teal-button (merge (utils/route-to events/navigate-adventure-matching-stylist-wait-post-purchase)
+                              {:data-test "pick-a-stylist"}) "Pick a stylist"))]))
 
 (def get-inspired-cta
   [:div.py2
@@ -157,10 +150,10 @@
 
        [:div.py2.mx-auto.white.border-bottom
         {:style {:border-width "0.5px"}}]
-       (cond servicing-stylist                                             (servicing-stylist-component phone-number servicing-stylist)
-             (and need-match? match-post-purchase? (seq matched-stylists)) (need-match-via-web-component matched-stylists)
-             need-match?                                                   need-match-via-phone-component
-             :else                                                         get-inspired-cta)]])]))
+       (cond servicing-stylist                      (servicing-stylist-component phone-number servicing-stylist)
+             (and need-match? match-post-purchase?) (need-match matched-stylists)
+             need-match?                            (need-match [])
+             :else                                  get-inspired-cta)]])]))
 
 (defmethod transitions/transition-state events/api-success-fetch-matched-stylist
   [_ event {:keys [stylist]} app-state]
