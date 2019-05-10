6858d1ef4c76e62729ed29772259b7c19d756a43
Justin Watt <justinw@mayvenn.com>
2019-05-10 10:22:55 -0700

    Clean up implementation of update-fetti

--- src-cljs/storefront/confetti.cljs
+++ src-cljs/storefront/confetti.cljs
@@ -37,25 +37,34 @@
      :tilt-angle       (* (js/Math.random) js/Math.PI)
      :tilt-angle-speed (+ 0.1 (* (js/Math.random) 0.3))}))
 
+(defn advance-physics
+  [drag-friction
+   {:keys [angle-2d
+           angle-3d
+           velocity
+           wobble-speed
+           tilt-angle-speed]
+    :as   physics}]
+  (-> physics
+      (update :x + (* velocity (js/Math.cos angle-2d)))
+      (update :y + (+ (* velocity (js/Math.sin angle-2d)) 3))
+      (update :z + (* velocity (js/Math.sin angle-3d)))
+      (update :wobble + wobble-speed)
+      (update :velocity - (* velocity drag-friction))
+      (update :tilt-angle + tilt-angle-speed)))
+
 (defn ^:private update-fetti
   [progress drag-friction {:keys [element physics] :as fetti}]
-  (let [{:keys [angle-2d angle-3d velocity wobble-speed tilt-angle-speed]} physics
-        {:keys [wobble x y tilt-angle] :as new-physics}
-        (-> physics
-            (update :x + (* velocity (js/Math.cos angle-2d)))
-            (update :y + (+ (* velocity (js/Math.sin angle-2d)) 3))
-            (update :z + (* velocity (js/Math.sin angle-3d)))
-            (update :wobble + wobble-speed)
-            (update :velocity - (* velocity drag-friction))
-            (update :tilt-angle + tilt-angle-speed))
-        wobble-x                   (+ x (* 10 (js/Math.cos wobble)))
-        wobble-y                   (+ y (* 10 (js/Math.sin wobble)))
-        transform                  (str "translate3d(" wobble-x "px, " wobble-y "px, 0) rotate3d(1, 1, 1, " tilt-angle "rad)")]
+  (let [{:keys [wobble x y tilt-angle]
+         :as   physics'} (advance-physics drag-friction physics)
+        wobble-x         (+ x (* 10 (js/Math.cos wobble)))
+        wobble-y         (+ y (* 10 (js/Math.sin wobble)))
+        transform        (str "translate3d(" wobble-x "px, " wobble-y "px, 0) rotate3d(1, 1, 1, " tilt-angle "rad)")]
     (set! element.style.visibility "visible")
     (set! element.style.transform transform)
     (set! element.style.opacity (- 1 progress))
 
-    (assoc fetti :physics new-physics)))
+    (assoc fetti :physics physics')))
 
 (defn ^:private animate [{:keys [node drag-friction duration delay]} fettis]
   (let [start-time-state (atom nil)
