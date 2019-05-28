64cb6c52b8eb85e72912e6363c93e4480c15b421
Heather Farley <heather@mayvenn.com>
2019-05-28 14:09:07 -0700

    Quote filepath for stats

--- gulpfile.js
+++ gulpfile.js
@@ -160,7 +160,7 @@ gulp.task('write-js-stats', function(cb){
         cb(err);
       } else {
         var fileSize = stdout.trim();
-        exec('(time -p node --check ' + mainJsFilePath + ' 2>/dev/null 1>/dev/null) 2>&1 | head -n1 | awk \'{print $2}\'', function(err, stdout) {
+        exec('(time -p node --check "' + mainJsFilePath + '" 2>/dev/null 1>/dev/null) 2>&1 | head -n1 | awk \'{print $2}\'', function(err, stdout) {
           var parseTime = stdout.trim();
 
           fs.writeFile("resources/main.js.file_size.stat", fileSize, function(err) {
