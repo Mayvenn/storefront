e014c6d8718273345133a8ace917c0a900e13aa7
Jonathan Harford <jonathanh@mayvenn.com>
2019-06-20 17:13:32 -0700

    Don't set the profile photo to the logged-in stylist
    [#166770299]
    
    Co-authored-by: Corey Reece <corey@mayvenn.com>

--- src-cljs/storefront/frontend_transitions.cljs
+++ src-cljs/storefront/frontend_transitions.cljs
@@ -340,8 +340,7 @@
   (let [stylist-zipcode (-> stylist :address :zipcode)]
     (-> app-state
         (update-in keypaths/stylist-manage-account merge stylist)
-        (update-in (conj keypaths/stylist-manage-account-green-dot-payout-attributes :postalcode) #(or % stylist-zipcode))
-        (update-in keypaths/store merge (select-keys stylist [:instagram-account :styleseat-account :portrait])))))
+        (update-in (conj keypaths/stylist-manage-account-green-dot-payout-attributes :postalcode) #(or % stylist-zipcode)))))
 
 (defmethod transition-state events/api-success-stylist-account-commission [_ event {:keys [stylist]} app-state]
   (let [green-dot-payout-attributes (some-> stylist :green-dot-payout-attributes (select-keys [:last-4 :payout-timeframe]))]
