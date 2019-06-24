940a68b96983e665cf33f50a38a02360a18a9a67
Jeff Hui <jeff@mayvenn.com>
2019-06-24 15:06:55 -0700

    Add another potentially risky extern var name to be excluded

--- externs/riskified.js
+++ externs/riskified.js
@@ -1,2 +1,7 @@
 var RISKX = {};
 RISKX.go = function() {};
+
+
+// Unused by storefront, but noted to prevent Google Closure from generating a
+// variable name like this during advanced optimization
+var RI22 = {};
