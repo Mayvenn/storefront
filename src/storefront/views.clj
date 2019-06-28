f2bd809dc423f831dbc824c84828e0d5afcb14c3
Jeff Hui <jeff@mayvenn.com>
2019-06-27 17:33:07 -0700

    Add defer to app script tags

--- src/storefront/views.clj
+++ src/storefront/views.clj
@@ -140,7 +140,8 @@
                 }"))]
           (when-not (config/development? environment)
             (for [n ["cljs_base.js" "main.js"]]
-              [:script {:src (assets/path (str "/js/out/" n))}]))
+              [:script {:src   (assets/path (str "/js/out/" n))
+                        :defer "true"}]))
           ;; inline styles in production because our css file is so small and it avoids another round
           ;; trip request. At time of writing this greatly includes our pagespeed score
           (if (#{"development" "test"} environment)
