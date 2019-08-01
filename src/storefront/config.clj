3f28fc7571fca9fc0fe4f82827ed541910537e72
Jeff Hui <jeff@mayvenn.com>
2019-08-01 15:48:10 -0700

    Use a function to convert snake-case to underscore for modules

--- src/storefront/config.clj
+++ src/storefront/config.clj
@@ -2,6 +2,7 @@
   (:require [environ.core :refer [env]]
             [clojure.java.io :as io]
             [clojure.string :as string]
+            [spice.maps :as maps]
             [storefront.assets :as assets]
             [storefront.platform.asset-mappings :as asset-mappings]))
 
@@ -12,13 +13,16 @@
 (defn define-frontend-modules []
   ;; all module names must be cljs mangled (aka - hyphens get converted to underscore)
   ;; See implementation of cljs.loader: https://github.com/clojure/clojurescript/blob/master/src/main/cljs/cljs/loader.cljs#L15-L46
-  {:cljs_base     [(assets/path "/js/out/cljs_base.js")]
-   :main          [(assets/path "/js/out/main.js")]
-   :dashboard     [(assets/path "/js/out/dashboard.js")]
-   :redeem        [(assets/path "/js/out/redeem.js")]
-   :design_system [(assets/path "/js/out/design-system.js")]
-   :catalog       [(assets/path "/js/out/catalog.js")]
-   :checkout      [(assets/path "/js/out/checkout.js")]})
+  (letfn [(snake->underscore [kw]
+            (keyword (string/replace (name kw) #"-" "_")))]
+    (maps/map-keys snake->underscore
+                   {:cljs-base     [(assets/path "/js/out/cljs_base.js")]
+                    :main          [(assets/path "/js/out/main.js")]
+                    :dashboard     [(assets/path "/js/out/dashboard.js")]
+                    :redeem        [(assets/path "/js/out/redeem.js")]
+                    :design-system [(assets/path "/js/out/design-system.js")]
+                    :catalog       [(assets/path "/js/out/catalog.js")]
+                    :checkout      [(assets/path "/js/out/checkout.js")]})))
 
 (def frontend-modules (memoize define-frontend-modules))
 
