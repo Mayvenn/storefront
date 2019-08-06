0c22ee2b7d866898eee5070012ac7d3df45e9bca
Jeff Hui <jeff@mayvenn.com>
2019-08-06 11:41:56 -0700

    Remove pseudo-names

--- project.clj
+++ project.clj
@@ -101,7 +101,7 @@
                         :language-out     :es6-strict
                         :npm-deps         false
                         :install-deps     false
-                        :pseudo-names     true
+                        :pseudo-names     false
                         :libs             ["src-cljs/rng/rng.js"]
                         :foreign-libs     [{:file     "src-cljs/storefront/jsQR.js"
                                             :file-min "target/min-js/jsQR.js"
