54d499ea370372ecb1dc6ccb000e09bc416c7abe
Jonathan Harford <jonathanh@mayvenn.com>
2019-10-02 10:33:39 -0700

    Don't double-insert pinterest tracking
    [#168742759]
    
    Co-authored-by: Jeff Hui <jeff@mayvenn.com>

--- src-cljs/storefront/hooks/pinterest.cljs
+++ src-cljs/storefront/hooks/pinterest.cljs
@@ -3,11 +3,13 @@
             [storefront.config :as config]))
 
 (defn insert-tracking []
+  #_
   (insert-tag-with-text
    (str "!function(e){if(!window.pintrk){window.pintrk=function(){window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version='3.0';var t=document.createElement('script');t.async=!0,t.src=e;var r=document.getElementsByTagName('script')[0];r.parentNode.insertBefore(t,r)}}('https://s.pinimg.com/ct/core.js');pintrk('load','" config/pinterest-tag-id "');pintrk('page');")
    "pinterest-pixel"))
 
 (defn remove-tracking []
+  #_
   (remove-tags-by-class "pinterest-pixel"))
 
 (defn track-page
