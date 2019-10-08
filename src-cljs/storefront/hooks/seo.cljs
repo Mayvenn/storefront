bf838f5bb510ae42f6fa8881b63110f107d376f6
Andres Pineda <andres@mayvenn.com>
2019-10-07 17:55:52 -0700

    Remove about-us tags from the client side
    [#168743094]
    
    Co-authored-by: Heather Farley <heather@mayvenn.com>

--- src-cljs/storefront/hooks/seo.cljs
+++ src-cljs/storefront/hooks/seo.cljs
@@ -1,13 +1,16 @@
 (ns storefront.hooks.seo
-  (:require [storefront.seo-tags :as seo-tags]
-            [storefront.browser.tags :as tags]))
+  (:require [storefront.browser.tags :as tags]
+            [storefront.events :as events]
+            [storefront.keypaths :as keypaths]
+            [storefront.seo-tags :as seo-tags]))
 
 (defn set-tags [data]
   (tags/remove-tags-by-class seo-tags/tag-class)
-  (doseq [[el props content] (seo-tags/tags-for-page data)]
-    (let [tag (.createElement js/document (clj->js el))]
-      (doseq [[k v] props]
-        (.setAttribute tag (clj->js k) v))
-      (when content
-        (set! (.-innerHTML tag) content))
-      (tags/insert-in-head tag))))
+  (when-not (= events/navigate-content-about-us (get-in data keypaths/navigation-event))
+    (doseq [[el props content] (seo-tags/tags-for-page data)]
+     (let [tag (.createElement js/document (clj->js el))]
+       (doseq [[k v] props]
+         (.setAttribute tag (clj->js k) v))
+       (when content
+         (set! (.-innerHTML tag) content))
+       (tags/insert-in-head tag)))))
