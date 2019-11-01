ad4e966dbd32d80a397b76e8397a8cc270313eb6
Jonathan Harford <jonathanh@mayvenn.com>
2019-11-01 10:21:33 -0700

    Convert cash-out/component from om to defcomponent
    [#169218592]
    
    Co-authored-by: Jeff Hui <jeff@mayvenn.com>

--- src-cljs/storefront/components/stylist/cash_out.cljs
+++ src-cljs/storefront/components/stylist/cash_out.cljs
@@ -10,37 +10,36 @@
             [storefront.transitions :as transitions]
             [storefront.api :as api]
             [storefront.effects :as effects]
-            [storefront.request-keys :as request-keys]))
+            [storefront.request-keys :as request-keys]
+            [storefront.component :as component :refer [defcomponent]]))
 
-(defn component [{:keys [amount payout-method cash-out-pending? cashing-out?]} owner opts]
+(defcomponent component [{:keys [amount payout-method cash-out-pending? cashing-out?]} owner opts]
   (let [{:keys [name type last-4 email payout-timeframe]} payout-method]
-    (om/component
-     (html
-      [:div.container.p4
-       [:h3.mb3 "Cash Out Your Earnings"]
-       [:div.col-12.flex.items-center.justify-between.my3
-        [:div
-         [:div.h6 name]
-         (if (= type "Mayvenn::GreenDotPayoutMethod")
-           [:div.h6 "Linked Card xxxx-xxxx-xxxx-" (or last-4 "????")]
-           [:div.h6 email])]
-        [:h2.teal (mf/as-money amount)]]
-       [:div
-        [:div.navy.center.h8
-         (case payout-timeframe
-           "immediate"                 "Instant: Funds typically arrive in minutes"
-           "next_business_day"         "Funds paid out to this card will become available the next business day."
-           "two_to_five_business_days" "Funds paid out to this card will become available two to five business days later."
-           "")]
-        [:div.my3
-         {:data-test "cash-out-commit-button"
-          :data-ref  "cash-out-button"}
-         (ui/teal-button {:on-click  (utils/send-event-callback events/control-stylist-dashboard-cash-out-commit
-                                                                {:amount amount
-                                                                 :payout-method-name name})
-                          :disabled? (not (payouts/cash-out-eligible? payout-method))
-                          :spinning? cashing-out?}
-          "Cash out")]]]))))
+    [:div.container.p4
+     [:h3.mb3 "Cash Out Your Earnings"]
+     [:div.col-12.flex.items-center.justify-between.my3
+      [:div
+       [:div.h6 name]
+       (if (= type "Mayvenn::GreenDotPayoutMethod")
+         [:div.h6 "Linked Card xxxx-xxxx-xxxx-" (or last-4 "????")]
+         [:div.h6 email])]
+      [:h2.teal (mf/as-money amount)]]
+     [:div
+      [:div.navy.center.h8
+       (case payout-timeframe
+         "immediate"                 "Instant: Funds typically arrive in minutes"
+         "next_business_day"         "Funds paid out to this card will become available the next business day."
+         "two_to_five_business_days" "Funds paid out to this card will become available two to five business days later."
+         "")]
+      [:div.my3
+       {:data-test "cash-out-commit-button"
+        :data-ref  "cash-out-button"}
+       (ui/teal-button {:on-click  (utils/send-event-callback events/control-stylist-dashboard-cash-out-commit
+                                                              {:amount amount
+                                                               :payout-method-name name})
+                        :disabled? (not (payouts/cash-out-eligible? payout-method))
+                        :spinning? cashing-out?}
+                       "Cash out")]]]))
 
 (defn query [data]
   (let [{:keys [amount payout-method]} (get-in data keypaths/stylist-payout-stats-next-payout)]
