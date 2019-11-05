43d264809a92036de580c9f41c59b95581d293f9
Jeff Hui <jeff@mayvenn.com>
2019-11-05 03:15:33 -0800

    formatting

--- src-cljs/storefront/platform/component_utils.cljs
+++ src-cljs/storefront/platform/component_utils.cljs
@@ -15,20 +15,20 @@
   (let [ctor (fn constructor [props]
                (this-as this
                  ;; auto-bind methods
-                        (doseq [[method f] method-names
-                                :when f]
-                          (gobj/set this (munge method) (partial (.bind f this) this)))
+                 (doseq [[method f] method-names
+                         :when f]
+                   (gobj/set this (munge method) (partial (.bind f this) this)))
 
                  ;; move props.options -> this.options instead of props
-                        (gobj/set this "options" (gobj/get this "options"))
-                        (gobj/remove this "options")
+                 (gobj/set this "options" (gobj/get this "options"))
+                 (gobj/remove this "options")
 
                  ;; call super()
-                        (.call super-class this props)
+                 (.call super-class this #js{:props props})
 
-                        (when init-fn
-                          (set! (.-state this) #js {:state ((.bind init-fn this) this props)}))
-                        this))]
+                 (when init-fn
+                   (set! (.-state this) #js {:state ((.bind init-fn this) this props)}))
+                 this))]
     ;; set static properties on prototype
     (goog/inherits ctor super-class)
     (doseq [[k v] static-properties]
