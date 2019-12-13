5622acd5ba07bef9298553cda0983b93443a7212
Corey Reece <corey@users.noreply.github.com>
2019-12-13 11:27:20 -0800

    Remove freeinstall version of sign in
    [#170247912]
    
    Co-authored-by: Corey Reece <corey@users.noreply.github.com>

--- src-cljs/storefront/components/checkout_sign_in.cljs
+++ src-cljs/storefront/components/checkout_sign_in.cljs
@@ -1,12 +1,12 @@
 (ns storefront.components.checkout-sign-in
-  (:require [adventure.checkout.sign-in :as adventure-sign-in]
-            [storefront.components.sign-in :as sign-in]
+  (:require [storefront.components.sign-in :as sign-in]
             [storefront.components.ui :as ui]
             [storefront.components.facebook :as facebook]
             [storefront.keypaths :as keypaths]
             [storefront.component :as component :refer [defcomponent]]))
 
-(defcomponent component [{:keys [facebook-loaded?] :as sign-in-form-data} owner _]
+(defcomponent component
+  [{:keys [facebook-loaded?] :as sign-in-form-data} owner _]
   (ui/narrow-container
    [:div.p2
     [:h1.center.my2.mb3 "Sign in to your account"]
@@ -15,8 +15,6 @@
     [:div.col-12.col-6-on-tb-dt.mx-auto
      (facebook/sign-in-button facebook-loaded?)]]))
 
-;; GROT: FREEINSTALL subdomain branch
-(defn ^:export built-component [data opts]
-  (if (= "freeinstall" (get-in data keypaths/store-slug))
-    (adventure-sign-in/built-component data opts)
-    (component/build component (sign-in/query data) opts)))
+(defn ^:export built-component
+  [data opts]
+  (component/build component (sign-in/query data) opts))
