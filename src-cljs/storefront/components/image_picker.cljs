d3f1a1140c52363b5d3dcf852289792c11764a33
Justin Watt <justinw@mayvenn.com>
2020-01-03 11:00:50 -0800

    Update styling of uploadcare widget
    [#170295300]
    
    Co-authored-by: Heather Farley <heather@mayvenn.com>

--- src-cljs/storefront/components/image_picker.cljs
+++ src-cljs/storefront/components/image_picker.cljs
@@ -2,9 +2,8 @@
   (:require [storefront.components.ui :as ui]
             [storefront.component :as component :refer [defdynamic-component]]
             [storefront.events :as events]
-            [storefront.platform.component-utils :as utils]
             [storefront.platform.messages :refer [handle-message]]
-            [storefront.components.svg :as svg]))
+            ui.molecules))
 
 (defdynamic-component component
   (did-mount [this]
@@ -21,11 +20,11 @@
             (component/html
              [:div.container.sans-serif
               [:div.p2
-               [:a.inherit-color.block.mb2.flex.items-center
-                (utils/route-to (:navigation-event back-link))
-                (svg/left-caret {:class  "stroke-black"
-                                 :width  "1.25rem"
-                                 :height "1.25rem"})
-                (:back-copy back-link)]
-               [:h1.center "Select a source below"]]
-              [:div {:id selector} ui/nbsp]]))))
+               (ui.molecules/return-link
+                {:return-link/id            "back to gallery"
+                 :return-link/copy          "Back to Gallery"
+                 :return-link/event-message [(:navigation-event back-link)]})]
+              [:div.bg-warm-gray.border-top.border-gray
+               [:div.mt8.mb1.title-1.canela.center "Select a source"]
+               [:div.mb3.col-8.mx-auto.content-2.proxima.center "Click an icon below to select your photo source"]
+               [:div {:id selector} ui/nbsp]]]))))
