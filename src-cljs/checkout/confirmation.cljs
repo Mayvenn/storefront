c5839b2bf3f9326c8e2397d2d4ff444655d37698
Stella Clemens <Stella@Mayvenn.com>
2020-01-24 11:40:35 -0800

    Update confirmation summary to match cart summary DT changes
    Co-authored-by: Heather Farley <heather@mayvenn.com>

--- src-cljs/checkout/confirmation.cljs
+++ src-cljs/checkout/confirmation.cljs
@@ -271,7 +271,7 @@
                                            (for [{:keys [name price] :as adjustment}
                                                  (filter adjustments/non-zero-adjustment? adjustments)
                                                  :let [install-summary-line? (orders/freeinstall-promotion? adjustment)]]
-                                             (cond-> {:cart-summary-line/id    (text->data-test-name name)
+                                             (cond-> {:cart-summary-line/id    (str (text->data-test-name name) "-adjustment")
                                                       :cart-summary-line/icon  (svg/discount-tag {:class  "mxnp6 fill-gray pr1"
                                                                                                   :height "2em" :width "2em"})
                                                       :cart-summary-line/label (adjustments/display-adjustment-name adjustment)
