888093eb02cfcdefb9c74b37e0dabc3f3a7da6a9
Jonathan Harford <jonathanh@mayvenn.com>
2020-09-03 16:43:41 -0700

    Revert "Implement nav stack popping to fix back not working"
    This reverts commit 334ec84f7a31322b9eb40f6c31b029c58f769e2d.
    
    The use of a js Array was an aborted optimization that was previously only partially
    reverted.

--- src-cljs/storefront/frontend_transitions.cljs
+++ src-cljs/storefront/frontend_transitions.cljs
@@ -92,13 +92,6 @@
         nav-stack   (get-in app-state stack-keypath nil)]
     (->> (conj nav-stack item) (take max-nav-stack-depth))))
 
-(defn pop-nav-stack [stack]
-  (let [nav-stack (or stack (js/Array.))]
-    (if (pos? (.-length nav-stack))
-      (doto nav-stack
-        (.shift))
-      nav-stack)))
-
 (defmethod transition-state events/navigation-save [_ _ stack-item app-state]
   ;; Going to a new page; add an element to the undo stack, and discard the redo stack
   (let [nav-undo-stack (push-nav-stack app-state keypaths/navigation-undo-stack stack-item)]
@@ -110,7 +103,7 @@
   ;; Going to prior page; pop an element from the undo stack, push one onto the redo stack
   (let [nav-redo-stack (push-nav-stack app-state keypaths/navigation-redo-stack stack-item)]
     (-> app-state
-        (update-in keypaths/navigation-undo-stack pop-nav-stack)
+        (update-in keypaths/navigation-undo-stack rest)
         (assoc-in keypaths/navigation-redo-stack nav-redo-stack))))
 
 (defmethod transition-state events/navigation-redo [_ _ stack-item app-state]
@@ -118,7 +111,7 @@
   (let [nav-undo-stack (push-nav-stack app-state keypaths/navigation-undo-stack stack-item)]
     (-> app-state
         (assoc-in keypaths/navigation-undo-stack nav-undo-stack)
-        (update-in keypaths/navigation-redo-stack pop-nav-stack))))
+        (update-in keypaths/navigation-redo-stack rest))))
 
 (defmethod transition-state events/redirect [_ event _ app-state]
   (assoc-in app-state keypaths/redirecting? true))
