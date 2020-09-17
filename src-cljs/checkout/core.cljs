64b3e3003b15662343417fdc811e4a34fb6fc0db
Justin Watt <justinw@mayvenn.com>
2020-09-17 16:33:52 -0700

    Ensure checkout.behavior file is loaded in checkout core
    Co-authored-by: Corey Reece <corey@mayvenn.com>
    Co-authored-by: Justin Watt <justinw@mayvenn.com>

--- src-cljs/checkout/core.cljs
+++ src-cljs/checkout/core.cljs
@@ -1,10 +1,9 @@
 (ns checkout.core
   (:require [storefront.loader :as loader]
             storefront.components.checkout-address
+            storefront.components.checkout-address-auth-required
             storefront.components.checkout-steps
             storefront.components.checkout-sign-in
-            storefront.components.checkout-address
-            storefront.components.checkout-address-auth-required
             storefront.components.checkout-payment
             storefront.components.checkout-complete
             storefront.components.places
@@ -13,6 +12,7 @@
             checkout.confirmation
             checkout.returning-or-guest-v2020-05
             checkout.processing
-            checkout.add))
+            checkout.add
+            checkout.behavior))
 
 (loader/set-loaded! :checkout)
