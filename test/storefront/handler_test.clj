46346ffeedd128d9f325950020abaa809a379959
Corey Reece <corey@users.noreply.github.com>
2022-05-02 22:13:30 -0700

    Email Modal 2022 May: update copy

--- test/storefront/handler_test.clj
+++ test/storefront/handler_test.clj
@@ -428,7 +428,7 @@
                 (is (string/includes? (:body response) "Something nice here"))))))))))
 
 (def contentful-content-types
-  [:homepage :faq :advertisedPromo :ugc-collection :landingPage :homepageHero :emailModalContext :matchesAll :matchesAny :startsWithPath :emailModal])
+  [:homepage :faq :advertisedPromo :ugc-collection :landingPage :homepageHero :emailModalTrigger :emailModalTemplate :matchesAll :matchesAny :matchesNot :startsWithPath :emailModal])
 
 (deftest fetches-data-from-contentful
   (testing "transforming content"
