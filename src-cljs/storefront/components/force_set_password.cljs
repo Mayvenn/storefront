388907ee6aa6257d79fdb89cd9b6aa423c5723db
Jeff Hui <jeff@mayvenn.com>
2022-06-06 23:36:02 -0700

    Compliance: hardcode force password to stylist experience; Bugfix
    Some race condition between user fetching + store-id check that isn't
    worth covering a use cased that currently doesn't exist.
    
    Fix bug where null was given if the stylist did not touch the opt-out
    forms during the force-reset-password flow.

--- src-cljs/storefront/components/force_set_password.cljs
+++ src-cljs/storefront/components/force_set_password.cljs
@@ -49,29 +49,6 @@
       (ui/submit-button "Agree & Submit"
                         {:data-test "force-set-password-submit"})]]]))
 
-(defn opt-in-query [phone-transactional-opt-in-value phone-marketing-opt-in-value]
-  {:marketing-opt-in/id          "phone-marketing-opt-in"
-   :marketing-opt-in/label       "… text me marketing messages."
-   :marketing-opt-in/legal       (str "By selecting “Yes”, I’m signing an agreement to"
-                                      " permit Mayvenn to text me recurring automated marketing promotions, surveys and"
-                                      " personalized messages using the number I entered above.  I understand these texts may"
-                                      " be sent using an automatic telephone dialing system or other automated system for the"
-                                      " selection and dialing of numbers and that I am not required to consent to receive these"
-                                      " texts or sign this agreement as a condition of any purchase.")
-   :marketing-opt-in/value       phone-marketing-opt-in-value
-   :marketing-opt-in/keypath     keypaths/reset-password-sms-transactional-optin
-   :transactional-opt-in/id      "phone-transactional-opt-in"
-   :transactional-opt-in/label   "… text me updates about transactional orders that my customers or I purchase. This includes booking appointments."
-   :transactional-opt-in/legal   (str "By selecting “Yes”,"
-                                      " I agree that Mayvenn can text me automated messages about my order"
-                                      " (e.g. delivery updates and feedback requests)"
-                                      " using the number I entered above.")
-   :transactional-opt-in/value   phone-transactional-opt-in-value
-   :transactional-opt-in/keypath keypaths/reset-password-sms-marketing-optin
-   :opt-in-legalese/title        "Would you like to receive text messages from us?"
-   :opt-in-legalese/terms-nav    [events/navigate-content-sms]
-   :opt-in-legalese/privacy-nav  [events/navigate-content-privacy]})
-
 (defn query
   [data]
   (merge
@@ -82,7 +59,7 @@
     :show-optins?   (get-in data keypaths/reset-password-needs-optin-prompts?)
     :present-tos?   (get-in data keypaths/reset-password-present-tos?)
     :copy-type      (get-in data keypaths/reset-password-copy-type)}
-   (legal/opt-in-query (get-in data keypaths/reset-password-copy-type)
+   (legal/opt-in-query "stylist"
                        (get-in data keypaths/reset-password-sms-marketing-optin)
                        (get-in data keypaths/reset-password-sms-transactional-optin))))
 
@@ -123,7 +100,7 @@
                            :id                (get-in app-state keypaths/user-id)
                            :password          (get-in app-state keypaths/force-set-password-password)
                            :token             (get-in app-state keypaths/user-token)
-                           :wants-smsable-mkt (get-in app-state keypaths/reset-password-sms-marketing-optin)
-                           :wants-smsable-txn (get-in app-state keypaths/reset-password-sms-transactional-optin)}
+                           :wants-smsable-mkt (boolean (get-in app-state keypaths/reset-password-sms-marketing-optin))
+                           :wants-smsable-txn (boolean (get-in app-state keypaths/reset-password-sms-transactional-optin))}
                           #(messages/handle-message events/api-success-force-set-password
                                                     (api/select-user-keys %))))
