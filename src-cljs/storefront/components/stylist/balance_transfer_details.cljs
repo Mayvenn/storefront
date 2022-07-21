3d13f1c310eff3a6c409512ebd95549976e74fed
Heather Moore-Farley <heather@mayvenn.com>
2022-07-21 10:44:17 -0700

    Square: don't display order details on stylist dashboar if no shipments
    no shipments = square order
    
    Co-authored-by: Jonathan Harford <jonathanh@mayvenn.com>
    Co-authored-by: Stella Clemens <Stella@mayvenn.com>

--- src-cljs/storefront/components/stylist/balance_transfer_details.cljs
+++ src-cljs/storefront/components/stylist/balance_transfer_details.cljs
@@ -63,17 +63,18 @@
         subtotal          (orders/commissioned-products-subtotal order)
         shipping-total    (* (:quantity shipping-item) (:unit-price shipping-item))]
     [:div.pt2.pb6.border-top.border-gray
-     (summary-row "Subtotal" subtotal)
+     (when (:shipments order)
+       (summary-row "Subtotal" subtotal)
 
-     (for [{:keys [name price coupon-code]} adjustments]
-       (when (adjustments/non-zero-adjustment? coupon-code)
-         (summary-row {:key name} [:div (adjustments/display-adjustment-name name)] price)))
+       (for [{:keys [name price coupon-code]} adjustments]
+         (when (adjustments/non-zero-adjustment? coupon-code)
+           (summary-row {:key name} [:div (adjustments/display-adjustment-name name)] price)))
 
-     (when (pos? store-credit-used)
-       (summary-row "Store Credit" (- store-credit-used)))
+       (when (pos? store-credit-used)
+         (summary-row "Store Credit" (- store-credit-used)))
 
-     (when shipping-item
-       (summary-row "Shipping" shipping-total))
+       (when shipping-item
+         (summary-row "Shipping" shipping-total)))
 
      (summary-row "Total" commissionable-amount)]))
 
