d2202927312a2ef2bb67ea210b39a7938f44bc71
Heather Moore-Farley <heather@mayvenn.com>
2022-07-27 10:18:14 -0700

    Revert "Send looks-displayed for classic as well"
    This reverts commit 8132f8b0c26408aa46293e518b4120e4f6772052.

--- src-cljs/storefront/frontend_trackings.cljs
+++ src-cljs/storefront/frontend_trackings.cljs
@@ -80,26 +80,22 @@
         (stringer/track-page (get-in app-state keypaths/store-experience))))))
 
 (defmethod perform-track events/navigate-shop-by-look [_ event _ app-state]
-  (if (= "mayvenn-classic" (get-in app-state keypaths/store-experience))
-    (stringer/track-event "looks-displayed" {:looks (->> app-state :cms :ugc-collection :all-looks
-                                                         vals
-                                                         (map :content/id))})
-    (let [facet-filtering-state (get-in app-state catalog.keypaths/k-models-facet-filtering)
-          skus-db               (get-in app-state storefront.keypaths/v2-skus)
-          facets-db             (->> (get-in app-state storefront.keypaths/v2-facets)
-                                     (maps/index-by (comp keyword :facet/slug))
-                                     (maps/map-values (fn [facet]
-                                                        (update facet :facet/options
-                                                                (partial maps/index-by :option/slug)))))
+  (let [facet-filtering-state (get-in app-state catalog.keypaths/k-models-facet-filtering)
+        skus-db               (get-in app-state storefront.keypaths/v2-skus)
+        facets-db             (->> (get-in app-state storefront.keypaths/v2-facets)
+                                   (maps/index-by (comp keyword :facet/slug))
+                                   (maps/map-values (fn [facet]
+                                                      (update facet :facet/options
+                                                              (partial maps/index-by :option/slug)))))
 
-          looks-shared-carts-db  (get-in app-state storefront.keypaths/v1-looks-shared-carts)
-          selected-album-keyword (get-in app-state storefront.keypaths/selected-album-keyword)
-          faceted-models         (looks/looks<- app-state skus-db facets-db
-                                                looks-shared-carts-db
-                                                selected-album-keyword)]
-      (stringer/track-event "looks-displayed" {:looks (->> faceted-models
-                                                           (select (:facet-filtering/filters facet-filtering-state))
-                                                           (map :look/id))}))))
+        looks-shared-carts-db  (get-in app-state storefront.keypaths/v1-looks-shared-carts)
+        selected-album-keyword (get-in app-state storefront.keypaths/selected-album-keyword)
+        faceted-models         (looks/looks<- app-state skus-db facets-db
+                                      looks-shared-carts-db
+                                      selected-album-keyword)]
+    (stringer/track-event "looks-displayed" {:looks (->> faceted-models
+                                                         (select (:facet-filtering/filters facet-filtering-state))
+                                                         (map #(select-keys % [:look/id :look/cart-number])))})))
 
 (defmethod perform-track events/control-category-panel-open
   [_ event {:keys [selected]} app-state]
