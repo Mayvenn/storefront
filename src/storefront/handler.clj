297198b383ff085cc3e451ba4c857046872194f9
Heather Moore-Farley <heather@mayvenn.com>
2022-12-20 14:30:33 -0800

    Remove unused code

--- src/storefront/handler.clj
+++ src/storefront/handler.clj
@@ -382,8 +382,6 @@
                                                                                 contentful/extract-fields
                                                                                 (resolve-cms-node normalized-cms-cache))]))}
 
-                              #_(update-data {} [:faq :sitewide-footer])
-
                               (cond (= events/navigate-home nav-event)
                                     (-> {:homepage (let [id (if shop? :unified-fi :unified)]
                                                      {id (-> normalized-cms-cache (assemble-cms-node :homepage id) first)})}
