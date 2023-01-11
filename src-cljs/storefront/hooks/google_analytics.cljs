dc5a64acd378f30f8f8bfbec23e6833c5ff71fe1
Jonathan Harford <jonathanh@mayvenn.com>
2023-01-11 14:02:27 -0800

    Remove "spy"

--- src-cljs/storefront/hooks/google_analytics.cljs
+++ src-cljs/storefront/hooks/google_analytics.cljs
@@ -3,7 +3,6 @@
 
 (defn ^:private track
   [event-name data]
-  (prn "track" event-name data)
   (when (.hasOwnProperty js/window "dataLayer")
     (.push js/dataLayer (clj->js {:ecommerce nil}))
     (.push js/dataLayer (clj->js {:event event-name
