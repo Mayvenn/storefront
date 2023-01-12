12e1e07bfca48bd499c681226d6e0262daf1a5fe
Jonathan Harford <jonathanh@mayvenn.com>
2023-01-11 16:26:10 -0800

    GA4: Limit to add_to_cart event
    Co-authored-by: Corey Reece <corey@mayvenn.com>
    Co-authored-by: Jonathan Harford <jonathanh@mayvenn.com>

--- src-cljs/storefront/hooks/google_analytics.cljs
+++ src-cljs/storefront/hooks/google_analytics.cljs
@@ -3,40 +3,44 @@
 
 (defn ^:private track
   [event-name data]
+  #_(prn "track" event-name data)
   (when (.hasOwnProperty js/window "dataLayer")
     (.push js/dataLayer (clj->js {:ecommerce nil}))
     (.push js/dataLayer (clj->js {:event event-name
                                   :ecommerce data}))))
-(defn track-login
-  []
-  (track "login" {}))
 
-(defn track-sign-up
-  []
-  (track "sign_up" {}))
+(defn track-add-to-cart
+  "Track an add-to-cart event in GA4 schema.
+   
+   When things settle down we might want to consider the following or more:
+   order/number, order/quantity, store/slug, stylist?"
+  [{:keys [line-item-skuers]}]
+  ;; NOTE: We are ignoring discounts here
+  ;; TODO: We should probably minus the discount out of the value.
+  (let [value (reduce + 0 (map :sku/price line-item-skuers))]
+    (track "add_to_cart"
+           {:items    (mapv (fn mayvenn->ga4 [item]
+                              {:item_id   (:catalog/sku-id item)
+                               :item_name (or (:legacy/product-name item)
+                                              (:sku/title item))
+                               :quantity  (:item/quantity item)
+                               :price     1.99 #_ (:sku/price item)}) line-item-skuers)
+            :currency "USD"
+            :value    value})))
 
+(defn track-login [] #_  (track "login" {}))
+(defn track-sign-up [] #_(track "sign_up" {}))
+
+#_
 (defn ^:private line-item-skuer->ga-cart-item
   [line-item-skuer]
-  {:item_id        (:legacy/variant-id line-item-skuer)
-   :item_variant   (:catalog/sku-id line-item-skuer)
+  {:item_id        (:catalog/sku-id line-item-skuer) 
    :item_name      (or (:legacy/product-name line-item-skuer)
                        (:sku/title line-item-skuer))
-   :item_category  (-> line-item-skuer :catalog/department first)
-   :item_category2 (-> line-item-skuer :hair/family first)
-   :item_category3 (-> line-item-skuer :hair/material first)
-   :item_category4 (-> line-item-skuer :hair/origin first)
-   :item_category5 (-> line-item-skuer :hair/texture first)
    :quantity       (:item/quantity line-item-skuer)
-   :price          (:sku/price line-item-skuer)
-   ;; Custom:
-   :hairCategory   (-> line-item-skuer :hair/family first)
-   :hairMaterial   (-> line-item-skuer :hair/material first)
-   :hairOrigin     (-> line-item-skuer :hair/origin first)
-   :hairStyle      (-> line-item-skuer :hair/texture first)
-   :hairGrade      (-> line-item-skuer :hair/grade first)
-   :hairLength     (-> line-item-skuer :hair/length first)
-   :hairColor      (-> line-item-skuer :hair/color first)})
+   :price          (:sku/price line-item-skuer)})
 
+#_
 (defn order<
   [{:keys [total number line-item-skuers buyer-type is-stylist-store
            shipping-method-name shipping-method-sku
@@ -58,36 +62,29 @@
 (defn track-placed-order
   [{:keys [number shipping-method-price]
     :as   data}]
+#_
   (track "purchase" (merge (order< data)
                            {:transaction_id number
                             ;; :tax TODO
                             :shipping       shipping-method-price})))
-
 (defn track-checkout-initiate
   [data]
+#_
   (track "begin_checkout" (order< data)))
-
-(defn track-add-to-cart
-  [{:keys [number line-item-skuers store-slug store-is-stylist order-quantity]}]
-  (track "add_to_cart"
-         {:items          (mapv line-item-skuer->ga-cart-item line-item-skuers)
-          ;; Custom:
-          :orderNumber    number
-          :orderQuantity  order-quantity
-          :storeSlug      store-slug
-          :storeIsStylist store-is-stylist}))
 
 (defn track-view-item
   [sku]
+#_
   (track "view_item"
          {:items [(line-item-skuer->ga-cart-item sku)]}))
-
 (defn track-select-promotion
   [{:keys [promo-code]}]
+#_
   (track "select_promotion" {:promotion_id promo-code}))
 
 (defn track-remove-from-cart
   [{:keys [sku number]}]
+#_
   (track "remove_from_cart"
          {:items       [(line-item-skuer->ga-cart-item sku)]
           :orderNumber number}))
