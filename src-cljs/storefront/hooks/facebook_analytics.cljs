6779abb5adf1326f315da18867d52bb51ec083b5
Jonathan Harford <jonathanh@mayvenn.com>
2023-01-27 16:47:39 -0800

    Ad Tech: Comment out Facebook Pixel events
    All events (except page_view, which is automatic) will be sent server-side

--- src-cljs/storefront/hooks/facebook_analytics.cljs
+++ src-cljs/storefront/hooks/facebook_analytics.cljs
@@ -2,18 +2,18 @@
 
 (defn track-event
   ([action]
-   (when (.hasOwnProperty js/window "fbq")
-     (js/fbq "track" action)))
+   #_  (when (.hasOwnProperty js/window "fbq")
+         (js/fbq "track" action)))
   ([action args]
-   (when (.hasOwnProperty js/window "fbq")
-     (js/fbq "track" action (clj->js args)))))
+   #_   (when (.hasOwnProperty js/window "fbq")
+          (js/fbq "track" action (clj->js args)))))
 
-(defn track-custom-event
+(defn track-custom-event 
   ([action]
-   (when (.hasOwnProperty js/window "fbq")
+#_   (when (.hasOwnProperty js/window "fbq")
      (js/fbq "trackCustom" action)))
   ([action args]
-   (when (.hasOwnProperty js/window "fbq")
+#_   (when (.hasOwnProperty js/window "fbq")
      (js/fbq "trackCustom" action (clj->js args)))))
 
 (defn subscribe []
