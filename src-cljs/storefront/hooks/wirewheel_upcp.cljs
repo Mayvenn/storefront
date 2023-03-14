39377bc54e7095c77cef3f0c405235a3589d5db2
Stella Clemens <Stella@Mayvenn.com>
2023-03-14 16:27:19 -0700

    Eliminating the spies.
    Co-authored-by: Stella Clemens <Stella@mayvenn.com>

--- src-cljs/storefront/hooks/wirewheel_upcp.cljs
+++ src-cljs/storefront/hooks/wirewheel_upcp.cljs
@@ -7,17 +7,13 @@
 (defn ^:private iframe-mounted? [] (js/document.getElementById "wwiframe"))
 
 (defn init-iframe []
-  (prn "INITING")
   (when (and (loaded?)
              (iframe-mounted?))
-    (prn "RLY INITING")
     (js/window.cmpJavascriptSdk.WireWheelSDK.initEmbeddedParent
      #js {:targetIframe (js/document.getElementById "wwiframe")})))
 
 (defn insert []
-  (prn "INSERTING")
   (when-not (loaded?)
-    (prn "RLY INSERTING")
     (tags/insert-tag-with-callback
      (tags/src-tag "https://ui.upcp.wirewheel.io/extensions/upcp-sdk-0.8.3.min.js"
               "ww-upcp")
