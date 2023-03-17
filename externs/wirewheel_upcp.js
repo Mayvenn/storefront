4df1f16c05ff4bc833444cf36eae73d143f96700
Stella Clemens <Stella@Mayvenn.com>
2023-03-17 09:52:30 -0700

    Wirewheel UPCP: SDK externs file
    Co-authored-by: Jonathan Harford <jonathanh@mayvenn.com>
    Co-authored-by: Stella Clemens <Stella@mayvenn.com>

---
+++ externs/wirewheel_upcp.js
@@ -0,0 +1,31 @@
+var cmpJavascriptSdk = {};
+cmpJavascriptSdk.WireWheelSDK = {};
+cmpJavascriptSdk.WireWheelSDK.initEmbeddedParent = function() {};
+
+//   WireWheelSDK
+// getToken
+// createClient
+// registerPrivacyProtocols
+// initEmbeddedParent
+
+//   WireWheelClient
+// createConsent
+// getChannelConfig
+// getUnifiedConsent
+// getProfileVerificationCode
+// hasConsents
+// login
+// loginFromSession
+// logout
+// mergeSubjects
+// saveProfile
+// subject
+// verifySubjectProfile
+// Subject
+// verified()
+// anonymous()
+
+//   EmbeddedHandler
+// on
+// send
+// getParentDomain
