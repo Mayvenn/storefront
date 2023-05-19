16014c808352ee4ed973ba078ca7238b30066b9e
Jonathan Harford <jonathanh@mayvenn.com>
2023-05-19 10:38:38 -0700

    Analytics: Add  Meta-formatted user data to GA4 packet

--- src-cljs/storefront/hooks/google_analytics.cljs
+++ src-cljs/storefront/hooks/google_analytics.cljs
@@ -94,10 +94,22 @@
   [_ _event {:keys [email phone first-name last-name address1 city state zipcode]} app-state]
   (update-in app-state
              k/user-ecd
-             #(maps/deep-merge % (maps/deep-remove-nils {:email                email
+             #(maps/deep-merge % (maps/deep-remove-nils {:email                email                ;; Meta requires, google accepts
                                                          :phone_number         (f/e164-phone phone)
+                                                         
+                                                         ;; google accepts these fields
                                                          :sha256_email_address (sha256< email)
                                                          :sha256_phone_number  (sha256< (f/e164-phone phone))
+
+                                                         ;; Meta
+                                                         :phone                (f/e164-phone phone) ;; Meta requires
+                                                         :first_name           first-name
+                                                         :last_name            last-name
+                                                         :city                 city 
+                                                         :state                state
+                                                         :zip                  zipcode ; Meta
+                                                         :country              "us" ; Meta
+
                                                          :address              {:sha256_first_name (sha256< first-name)
                                                                                 :sha256_last_name  (sha256< last-name)
                                                                                 ;:first_name        first-name
